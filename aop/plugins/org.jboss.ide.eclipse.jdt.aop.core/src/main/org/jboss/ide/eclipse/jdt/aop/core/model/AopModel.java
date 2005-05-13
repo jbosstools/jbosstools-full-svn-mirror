537ee4f28f451b26761b486c6c210726113cc210
Rob Stryker <rob.stryker@jboss.com>
2005-05-13 16:17:03 +0000

    Moved the elementChangedListener methods to their own class to help keep the model class file more lean.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@151 a97e2381-89e5-4abb-bab3-167db6db766c

--- aop/plugins/org.jboss.ide.eclipse.jdt.aop.core/src/main/org/jboss/ide/eclipse/jdt/aop/core/model/AopModel.java
+++ aop/plugins/org.jboss.ide.eclipse.jdt.aop.core/src/main/org/jboss/ide/eclipse/jdt/aop/core/model/AopModel.java
@@ -4,32 +4,24 @@
 package org.jboss.ide.eclipse.jdt.aop.core.model;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Hashtable;
 import java.util.Iterator;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.jdt.core.ElementChangedEvent;
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IElementChangedListener;
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaElementDelta;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.IJavaSearchScope;
 import org.eclipse.jdt.core.search.SearchEngine;
-import org.eclipse.jdt.internal.core.CompilationUnit;
-import org.eclipse.jdt.internal.core.JavaElementDelta;
 import org.eclipse.jdt.internal.corext.util.AllTypesCache;
 import org.eclipse.jdt.internal.corext.util.TypeInfo;
 import org.jboss.aop.AspectManager;
@@ -64,8 +56,7 @@ import org.jboss.ide.eclipse.jdt.aop.core.pointcut.JDTTypedefExpression;
 /**
  * @author Marshall
  */
-public class AopModel implements IElementChangedListener
-{
+public class AopModel {
 	
 	private static AopModel instance;
 	
@@ -87,8 +78,7 @@ public class AopModel implements IElementChangedListener
 		projectAdvisors = new Hashtable();
 		globalListeners = new ArrayList();
 		registeredTypes = new ArrayList();
-		
-		JavaCore.addElementChangedListener(this);
+		new AopModelElementChangedListener();
 	}
 	
 	protected IAopAdvisor[] getPointcutAdvisors (IJavaProject project, JDTPointcutExpression expression)
@@ -457,8 +447,6 @@ public class AopModel implements IElementChangedListener
 	
 	public void updateModel (IJavaProject project, IProgressMonitor monitor)
 	{
-		// Rather than completely re-creating the entire internal model, we'll try to be "smart" about changed pointcuts,
-		// and added/removed advisors
 		
 		ProjectAdvisors advisors = getProjectAdvisors (project);
 		
@@ -469,6 +457,25 @@ public class AopModel implements IElementChangedListener
 		descriptor.update();
 		
 		Aop aop = descriptor.getAop();
+
+		// Init the typedefs
+		AspectManager.instance().getTypedefs().clear();
+		for (Iterator iter = AopModelUtils.getTypedefsFromAop(aop).iterator(); iter.hasNext(); )
+		{
+			Typedef jaxbTypedef = (Typedef) iter.next();
+			try {
+				TypedefExpression typedefExpression = new TypedefExpression(jaxbTypedef.getName(), jaxbTypedef.getExpr());
+				AspectManager.instance().addTypedef(new JDTTypedefExpression( typedefExpression ));
+			} catch( Exception typedefExec ) {
+				
+			}
+		}
+
+		
+		
+		
+		// Rather than completely re-creating the entire internal model, we'll try to be "smart" about changed pointcuts,
+		// and added/removed advisors
 		
 		for (Iterator iter = AopModelUtils.getPointcutsFromAop(aop).iterator(); iter.hasNext(); )
 		{
@@ -518,6 +525,8 @@ public class AopModel implements IElementChangedListener
 				}
 				
 				advisors.removeAspect(aspect);
+				AspectManager.instance().removeAspectDefinition(aspect.getType().getFullyQualifiedName());
+				
 			}
 		}
 		
@@ -527,6 +536,17 @@ public class AopModel implements IElementChangedListener
 			if (! advisors.hasAspect(xmlAspect.getClazz()))
 			{
 				advisors.addAspect(xmlAspect.getClazz());
+			}
+			if( AspectManager.instance().getAspectDefinition(xmlAspect.getClazz()) == null ) {
+				Scope sScope = AopModel.getScopeFromString(xmlAspect.getScope());
+				
+				AspectDefinition def = new AspectDefinition(xmlAspect.getClazz(), sScope, null);
+				try {
+					AspectManager.instance().addAspectDefinition(def);
+				} catch( Exception e ) {
+					
+				}
+
 			}
 		}
 		
@@ -556,6 +576,10 @@ public class AopModel implements IElementChangedListener
 				}
 				
 				advisors.removeInterceptor (interceptors[i]);
+				try {
+					AspectManager.instance().removeAspectDefinition(interceptors[i].getName());
+				} catch( Throwable thrw ) {
+				}
 			}
 		}
 		
@@ -897,7 +921,6 @@ public class AopModel implements IElementChangedListener
 				registerType((IType)iter.next());
 			}
 		} catch (JavaModelException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -1171,294 +1194,6 @@ public class AopModel implements IElementChangedListener
 	}
 	
 	
-	
-	
-	
-	
-	/**
-	 * This method is used when elementChanged's delta's element is not directly
-	 * associated with a project. Here, we search for its compilation unit
-	 * recursively through the deltas until we find it. 
-	 * 
-	 * @param event The element changed event sent to the elementChanged method
-	 * @return The compilation unit that's been changed
-	 */
-	public ICompilationUnit elementChangedGetCompilationUnit(ElementChangedEvent event) {
-		ArrayList list = elementChangedGetAllAffected(event.getDelta(), true);
-		Iterator i = list.iterator();
-		while( i.hasNext() ) {
-			JavaElementDelta delta = (JavaElementDelta)i.next();
-			IJavaElement element = delta.getElement();
-			if( element instanceof CompilationUnit ) {
-				return (CompilationUnit)element;
-			}
-		}
-		return null;
-	}
-	
-	public void elementChanged (ElementChangedEvent event)
-	{
-		ArrayList changed = new ArrayList();
-		IJavaProject project = event.getDelta().getElement().getJavaProject();
-		if( project == null ) {
-			ICompilationUnit unit = elementChangedGetCompilationUnit(event);
-			if( unit == null ) {
-				//System.out.println("Problem"); 
-				return;
-			}
-			project = unit.getJavaProject();
-			//System.out.println("Saved");
-		}
-		JDTPointcutExpression expressions[] = getProjectPointcuts( project );
-		boolean annotationsHaveChanged = false;
-		
-		if( event.getDelta().getElement() instanceof CompilationUnit ) {
-			annotationsHaveChanged = true;
-		}
-
-		
-		changed.addAll(elementChangedGetAllAffected(event.getDelta()));		
-		
-		
-		
-		// The removed elements should be at the top now.  The added elements at the bottom. 
-		Collections.sort(changed, new Comparator() {
-
-			public int compare(Object first, Object second) {
-				if( ((IJavaElementDelta)first).getKind() ==  ((IJavaElementDelta)second).getKind())
-					return 0;
-				
-				if(((IJavaElementDelta)first).getKind() == IJavaElementDelta.REMOVED ) {
-					return -1;
-				}
-				
-				return 1;
-			}
-			
-		});
-		
-		for( Iterator i = changed.iterator(); i.hasNext();) {
-			IJavaElementDelta t = ((IJavaElementDelta)(i.next()));
-
-			if( t.getKind() == IJavaElementDelta.ADDED ) 
-				handleElementAdded(t, expressions);
-			else 
-				handleElementRemoved(t, expressions);
-
-		}
-
-		
-		
-		
-		// Finally check the annotations
-		if( annotationsHaveChanged ) {
-			try {
-				IType types[] = ((CompilationUnit)event.getDelta().getElement()).getTypes();
-				for( int i = 0; i < types.length; i++ ) {
-					elementChangedCompilationUnit(types[i], expressions);
-				}
-			} catch( CoreException e ) {
-				//e.printStackTrace();
-			}
-		}
-	}
-
-	
-	/**
-	 * Convenience method
-	 */
-	private ArrayList elementChangedGetAllAffected(IJavaElementDelta delta ) {
-		return elementChangedGetAllAffected(delta, false);
-	}
-	
-	/**
-	 * This is a private method that will recursively get a list
-	 * of added, or deleted children to be used by the elementChanged 
-	 * method. 
-	 * It will not include anything except methods or fields. 
-	 * It will not include changed elements. Only added or removed.
-	 * @param delta The original event's delta
-	 * @param includeCompilationUnit  Do we include the compilation unit in the returned list?
-	 * @return tmp An arraylist 
-	 */
-	private ArrayList elementChangedGetAllAffected(IJavaElementDelta delta, boolean includeCompilationUnit) {
-		ArrayList changed = new ArrayList();
-		IJavaElementDelta changedChildren[] = delta.getAffectedChildren();
-		if( delta.getElement() instanceof IMethod || delta.getElement() instanceof IField 
-				|| (delta.getElement() instanceof CompilationUnit && includeCompilationUnit)) {
-			
-			// We only care about added or removed... not changed.
-			if( delta.getKind() != IJavaElementDelta.CHANGED)
-				changed.add(delta);
-
-			// unless we're including the compilation unit
-			if( includeCompilationUnit && delta.getKind() == IJavaElementDelta.CHANGED) {
-				changed.add(delta);				
-			}
-		}
-		for( int i = 0; i < changedChildren.length; i++ ) {
-			changed.addAll(elementChangedGetAllAffected(changedChildren[i], includeCompilationUnit));
-		}
-		return changed;
-	}
-	
-
-	
-	/**
-	 * A new element has been added through a delta to the elementChanged method. 
-	 * We must check if it matches anything, and if so, add the match to the model.
-	 * 
-	 * 
-	 * @param addedDelta
-	 * @param expressions
-	 */
-	private void handleElementAdded (IJavaElementDelta addedDelta, JDTPointcutExpression expressions[])
-	{
-		IJavaElement added = addedDelta.getElement();
-		
-		for (int j = 0; j < expressions.length; j++)
-		{
-			if (added.getElementType() == IJavaElement.METHOD)
-			{
-				if (expressions[j].matchesExecution((IMethod) added))
-				{
-					addAdvisedToPointcutAdvisors(added, IAopAdvised.TYPE_METHOD_EXECUTION, expressions[j]);
-				}
-			}
-			else if (added.getElementType() == IJavaElement.FIELD)
-			{
-				if (expressions[j].matchesGet((IField) added))
-				{
-					addAdvisedToPointcutAdvisors(added, IAopAdvised.TYPE_FIELD_GET, expressions[j]);
-				}
-				if (expressions[j].matchesSet((IField) added))
-				{
-					addAdvisedToPointcutAdvisors(added, IAopAdvised.TYPE_FIELD_SET, expressions[j]);
-				}
-			}
-		}
-	}
-	
-	/**
-	 * An element has been removed through a delta to the changeEvent method.
-	 * 
-	 * @param removedDelta
-	 * @param expressions
-	 */
-	private void handleElementRemoved (IJavaElementDelta removedDelta, JDTPointcutExpression expressions[])
-	{
-		IJavaElement removed = removedDelta.getElement();
-		if( removed instanceof IMethod ) elementChangedRemoveMethod(((IMethod)removed), expressions);
-		if( removed instanceof IField ) elementChangedRemoveField((IField)removed, expressions);
-	}
-	
-	private void elementChangedRemoveMethod(IMethod method, JDTPointcutExpression expressions[])
-	{
-		IJavaProject project = method.getCompilationUnit().getJavaProject();
-		for( int i = 0; i < expressions.length; i++ ) {
-			IAopAdvised advised = expressions[i].getAdvised(method);
-			if( advised != null ) removeAdvised(project, new IAopAdvised[] { advised });
-		}
-	}
-	
-
-	private void elementChangedRemoveField(IField field, JDTPointcutExpression expressions[]) {
-		IJavaProject project = field.getCompilationUnit().getJavaProject();
-		for( int i = 0; i < expressions.length; i++ ) {
-			IAopAdvised getVal = 
-				expressions[i].getAdvised(field, IAopAdvised.TYPE_FIELD_GET);
-			IAopAdvised setVal = 
-				expressions[i].getAdvised(field, IAopAdvised.TYPE_FIELD_SET);
-			if( getVal != null ) removeAdvised(project, new IAopAdvised[] { getVal });
-			if( setVal != null ) removeAdvised(project, new IAopAdvised[] { setVal });
-		}
-	}
-	
-	
-	/**
-	 * This method is only reached if the compilation unit has changed,
-	 * which implies the annotations have changed.
-	 * 
-	 * This will add or remove advisors based on the changed annotations.
-	 * Because the jdt model here only returns a compilation unit, 
-	 * we're not exactly sure what's changed, so we have to check
-	 * every method, field, and inner class for changes.
-	 * 
-	 * @param type
-	 * @param expressions
-	 */
-	private void elementChangedCompilationUnit(IType type, JDTPointcutExpression expressions[] ) {
-		try {
-			IMethod[] methods = type.getMethods();
-			IField[] fields = type.getFields();
-			IType[] innerTypes = type.getTypes();
-						
-
-			/*
-			 * Checking what fields are changed and update 
-			 * the model accordingly. 
-			 */
-			for( int i = 0; i < fields.length; i++ ) {
-				elementChangedVerifyField( fields[i], expressions);
-			}
-			
-			/*
-			 * Check what methods (and constructors) are changed 
-			 * and update the model accordingly. 
-			 */
-			for( int i = 0; i < methods.length; i++ ) {
-				elementChangedVerifyMethod(methods[i], expressions);
-			}
-			
-			/*
-			 * Finally, we should go recursively through subtypes.
-			 */
-			for( int i = 0; i < innerTypes.length; i++) {
-				elementChangedCompilationUnit(innerTypes[i], expressions);
-			}
-			
-			
-		} catch (JavaModelException e ) {
-		}
-	}
-	
-	private void elementChangedVerifyField( IField field, JDTPointcutExpression expressions[]) {
-		for( int j = 0; j < expressions.length; j++ ) {
-			boolean matchesGet = expressions[j].matchesGet(field);
-			boolean matchesSet = expressions[j].matchesSet(field);
-			IAopAdvised getVal = 
-				expressions[j].getAdvised(field, IAopAdvised.TYPE_FIELD_GET);
-			IAopAdvised setVal = 
-				expressions[j].getAdvised(field, IAopAdvised.TYPE_FIELD_SET);
-			
-			
-			if( matchesGet && getVal == null ) {  // add him
-				addAdvisedToPointcutAdvisors(field, IAopAdvised.TYPE_FIELD_GET, expressions[j]);
-			} else if( !matchesGet && getVal != null ) { // remove him
-				removeAdvisedFromPointcutAdvisors(getVal, expressions[j]);
-			}
-			
-			if( matchesSet && setVal == null ) {
-				addAdvisedToPointcutAdvisors(field, IAopAdvised.TYPE_FIELD_SET, expressions[j]);						
-			} else if( !matchesSet && setVal != null ) {
-				removeAdvisedFromPointcutAdvisors(setVal, expressions[j]);
-			}
-		}
-	}
-
-	private void elementChangedVerifyMethod(IMethod method, JDTPointcutExpression expressions[]) {
-		for( int j = 0; j < expressions.length; j++ ) {
-			boolean matches = expressions[j].matchesExecution(method);
-			IAopAdvised advisedObj = expressions[j].getAdvised(method);
-			
-			if( matches && advisedObj == null ) {
-				addAdvisedToPointcutAdvisors(method, 
-						IAopAdvised.TYPE_METHOD_EXECUTION, expressions[j]);
-			} else if( !matches && advisedObj != null ) {
-				removeAdvisedFromPointcutAdvisors(advisedObj, expressions[j]);
-			}
-		}
-	}
 	
 	/**
 	 * Adds an IJavaElement to every advisor for a given expression. 
