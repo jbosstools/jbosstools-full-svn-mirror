5f901f45972ea08807f113e08d612e3300608628
Marshall Culpepper <mculpepper@jboss.com>
2005-09-19 14:19:07 +0000

    a new ant script has been created that exports the binaries built from the product build (mainly the "ALL" build) into a working update site. the generated site uses templates from either the "development" or "stable" subdirectories of the "updateSIte" directory under the product builder.
    the easiest way to run the update site builder is to either run build-site.sh like so (from the builders/product directory):
    
      ./build-site.sh [site-type] [version] (build-type)
        site-type:  The update site type to build. [development|stable]
        version:    The version to build this update site from.
        build-type: The type of build this update site is for. [release|integration|nightly] (optional: default 'release')
    
    or run the ant script like so (from the builders/product directory):
    ant -lib ../../lib -Dproduct.versionTag=XX -Dproduct.buildType=[R|I|N] -f buildSite.xml [development|stable]
    
    it should be noted that for now the update site builder automatically publishes the site to download.jboss.org (using the scp properties from build customization)
    
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@483 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ releng/org.jboss.ide.eclipse.releng/builders/product/build-site.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+TARGET=$1
+shift
+PRODUCT_VERSION=$1
+shift
+BUILD_TYPE=$1
+shift
+ANT=$ANT_HOME/bin/ant
+
+function usage
+{
+	echo "Usage:"
+	echo ""
+	echo "  $0 [site-type] [version] (build-type)"
+	echo "    site-type:  The update site type to build. [development|stable]"
+	echo "    version:    The version to build this update site from."
+	echo "    build-type: The type of build this update site is for. [release|integration|nightly] (optional: default 'release')"
+	echo ""
+	exit
+}
+
+if [ "$TARGET" = "" ]; then
+	usage
+fi
+
+if [ "$PRODUCT_VERSION" = "" ]; then
+	usage
+fi
+
+if [ "$ANT_HOME" = "" ]; then
+	echo ANT_HOME is not set, please set ANT_HOME before calling this script
+	exit -1
+fi
+
+case "$BUILD_TYPE" in
+	[rR]*) buildtype="R";;
+	[iI]*) buildtype="I";;
+	[nN]*) buildType="N";;
+	*);;
+esac
+
+
+echo buildType=$buildtype,productVersion=$PRODUCT_VERSION,target=$TARGET
+
+$ANT $@ -lib ../../lib -Dproduct.versionTag=$PRODUCT_VERSION -Dproduct.buildType=$buildtype -f buildSite.xml $TARGET
