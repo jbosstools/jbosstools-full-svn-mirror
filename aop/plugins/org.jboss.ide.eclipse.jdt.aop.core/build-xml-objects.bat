cceb42f0b87dd5175555c9aea7b8716f7f0241d0
No Author <null@localhost>
2005-11-29 15:45:12 +0000

    This commit was manufactured by cvs2svn to create tag 'IDE_EJB3_TOOLS_1_0_RC2'.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/IDE_EJB3_TOOLS_1_0_RC2@600 a97e2381-89e5-4abb-bab3-167db6db766c

--- aop/plugins/org.jboss.ide.eclipse.jdt.aop.core/build-xml-objects.bat
+++
@@ -1,5 +0,0 @@
-@ echo ------ Building XML Objects.. ------
-@ echo -- JWSDP_HOME = %JWSDP_HOME%
-@ echo -- JBOSS_AOP_ECLIPSE = %JBOSS_AOP_ECLIPSE%
-@ call %JWSDP_HOME%\jaxb\bin\xjc.bat -extension -d %JBOSS_AOP_ECLIPSE%\org.jboss.ide.eclipse.jdt.aop.core\generated -p org.jboss.ide.eclipse.jdt.aop.core.jaxb %JBOSS_AOP_ECLIPSE%\org.jboss.ide.eclipse.jdt.aop.core\aop-report.xsd %JBOSS_AOP_ECLIPSE%\org.jboss.ide.eclipse.jdt.aop.core\jboss-aop.xsd
-@ echo ------ Done -------
