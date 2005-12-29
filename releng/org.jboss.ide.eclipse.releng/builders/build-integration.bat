d20c05f1011398e71e20e54c96f1cb2774a791d6
Marshall Culpepper <mculpepper@jboss.com>
2005-12-29 03:15:58 +0000

    the tags file was supposed to be optional for the integration scripts, but the script would choke if it didn't exist. you will now have to specify the tags file like so :
    ./build-integration.sh product -tags 1.5.tags
    
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@713 a97e2381-89e5-4abb-bab3-167db6db766c

--- releng/org.jboss.ide.eclipse.releng/builders/build-integration.bat
+++ releng/org.jboss.ide.eclipse.releng/builders/build-integration.bat
@@ -1,16 +1,58 @@
-@if "%ANT_HOME%"=="" goto noAnt
-call %ANT_HOME%\bin\ant %3 %4 %5 %6 -lib ..\lib -f common/buildRequirements.xml cleanRequirements
+@echo off
 
-@if "%1"=="product" goto productBuild
-call %ANT_HOME%\bin\ant %3 %4 %5 %6 -lib ..\lib -propertyfile %2 -f builder-wrap.xml integration -Dbuilder=%1
+REM
+REM Usage:
+REM
+REM         build-integration [builder-name] (-tags [tagfilename]) (ant properties)
+REM
+
+set target=%1
+shift
+
+set ant=%ANT_HOME%\bin\ant
+
+if "%1"=="-tags" goto settags
+goto notags
+
+:settags
+shift
+set cvstagproperties=%1
+shift
+
+:notags
+
+
+if "%ANT_HOME%"=="" goto noAnt
+
+call %ant %1 %2 %3 %4 %5 %6 -lib ..\lib -f common/buildRequirements.xml cleanRequirements
+
+if "%target"=="product" goto productBuild
+if "%cvstagproperties"=="" goto builderBuildNoTags
+goto builderBuildWithTags
+
+:builderBuildNoTags
+call %ant %1 %2 %3 %4 %5 %6 -lib ..\lib -f builder-wrap.xml integration -Dbuilder=%target
+goto end
+
+:builderBuildWithTags
+call %ant %1 %2 %3 %4 %5 %6 -lib ..\lib -propertyfile %cvstagsproperties -f builder-wrap.xml integration -Dbuilder=%target
 goto end
 
 :productBuild
-call %ANT_HOME%\bin\ant %3 %4 %5 %6 -lib ..\lib -propertyfile %2 -f product/productBuild.xml integration > build.log
-call %ANT_HOME%\bin\ant %3 %4 %5 %6 -lib ..\lib -propertyfile %2 -f product/buildResults.xml publish.log
+if "%cvstagproperties"=="" goto productBuildNoTags
+goto productBuildWithTags
+
+:productBuildNoTags
+call %ant %1 %2 %3 %4 %5 %6 -lib ..\lib -f product/productBuild.xml integration > build.log
+call %ant %1 %2 %3 %4 %5 %6 -lib ..\lib -f product/buildResults.xml publish.log
+goto end
+
+:productBuildWithTags
+call %ant %1 %2 %3 %4 %5 %6 -lib ..\lib -propertyfile %cvstagsproperties -f product/productBuild.xml integration > build.log
+call %ant %1 %2 %3 %4 %5 %6 -lib ..\lib -propertyfile %cvstagsproperties -f product/buildResults.xml publish.log
 goto end
 
 :noAnt
-@echo ANT_HOME is not set, please set ANT_HOME before calling this script
+echo ANT_HOME is not set, please set ANT_HOME before calling this script
 
 :end
--- releng/org.jboss.ide.eclipse.releng/builders/build-integration.sh
+++ releng/org.jboss.ide.eclipse.releng/builders/build-integration.sh
@@ -2,8 +2,13 @@
 
 TARGET=$1
 shift
-CVSTAGPROPERTIES=$1
-shift
+
+if [ "$1" = "-tags" ]; then
+	shift
+	CVSTAGPROPERTIES=$1
+	shift
+fi
+
 ANT=$ANT_HOME/bin/ant
 
 echo target=$TARGET
@@ -16,15 +21,28 @@ fi
 $ANT $@ -lib ../lib -f common/buildRequirements.xml cleanRequirements
 
 if [ "$TARGET" = "product" ]; then
-	$ANT $@ -lib ../lib -propertyfile $CVSTAGPROPERTIES -f product/productBuild.xml integration > build.log
+	if [ "$CVSTAGPROPERTIES" = "" ]; then
+		$ANT $@ -lib ../lib -f product/productBuild.xml integration > build.log
+	else
+		$ANT $@ -lib ../lib -propertyfile $CVSTAGPROPERTIES -f product/productBuild.xml integration > build.log
+	fi
+	
 	
 	if [ "$?" = "0" ]; then
-		$ANT $@ -lib ../lib -propertyfile $CVSTAGPROPERTIES -f product/buildResults.xml publish.log
+		if [ "$CVSTAGPROPERTIES" = "" ]; then
+			$ANT $@ -lib ../lib -f product/buildResults.xml publish.log
+		else
+			$ANT $@ -lib ../lib -propertyfile $CVSTAGPROPERTIES -f product/buildResults.xml publish.log
+		fi
 	else
 		echo "\t[JBossIDE-Build ERROR] There was an error running the build"
 		exit -1
 	fi
 	
 else
-	$ANT $@ -lib ../lib -propertyfile $CVSTAGPROPERTIES -f builder-wrap.xml integration -Dbuilder=$TARGET
+	if [ "$CVSTAGPROPERTIES" = "" ]; then
+		$ANT $@ -lib ../lib -f builder-wrap.xml integration -Dbuilder=$TARGET
+	else
+		$ANT $@ -lib ../lib -propertyfile $CVSTAGPROPERTIES -f builder-wrap.xml integration -Dbuilder=$TARGET
+	fi
 fi
