46cd4bf5bce2145e0e7308eb68335d745d3501dc
Marshall Culpepper <mculpepper@jboss.com>
2006-01-12 22:46:11 +0000

    [JBIDE-258] J2EE projects now automatically include the WST validation builder
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/core@745 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.ide.eclipse.jdt.j2ee.ui/src/main/org/jboss/ide/eclipse/jdt/j2ee/ui/wizards/projects/J2EE13ProjectCreationWizardPage.java
+++ plugins/org.jboss.ide.eclipse.jdt.j2ee.ui/src/main/org/jboss/ide/eclipse/jdt/j2ee/ui/wizards/projects/J2EE13ProjectCreationWizardPage.java
@@ -12,6 +12,7 @@ import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 import org.jboss.ide.eclipse.jdt.core.classpath.ClassPathContainerRepository;
 import org.jboss.ide.eclipse.jdt.core.util.JavaProjectUtil;
+import org.jboss.ide.eclipse.jdt.j2ee.core.JDTJ2EECorePlugin;
 import org.jboss.ide.eclipse.jdt.j2ee.core.classpath.J2EE13ClasspathContainer;
 import org.jboss.ide.eclipse.jdt.j2ee.ui.JDTJ2EEUIMessages;
 import org.jboss.ide.eclipse.jdt.ui.wizards.ProjectWizardPage;
@@ -22,34 +23,40 @@ import org.jboss.ide.eclipse.jdt.ui.wizards.ProjectWizardPage;
  * @author    Laurent Etiemble
  * @version   $Revision$
  */
-public class J2EE13ProjectCreationWizardPage extends ProjectWizardPage
-{
-   /**
-    *Constructor for the J2EE13ProjectCreationWizardPage object
-    *
-    * @param mainPage  Description of the Parameter
-    */
-   public J2EE13ProjectCreationWizardPage(WizardNewProjectCreationPage mainPage)
-   {
-      super(mainPage);
-      this.setTitle(JDTJ2EEUIMessages.getString("J2EE13ProjectCreationWizardPage.title"));//$NON-NLS-1$
-      this.setDescription(JDTJ2EEUIMessages.getString("J2EE13ProjectCreationWizardPage.description"));//$NON-NLS-1$
-   }
+public class J2EE13ProjectCreationWizardPage extends ProjectWizardPage {
+	/**
+	 *Constructor for the J2EE13ProjectCreationWizardPage object
+	 *
+	 * @param mainPage  Description of the Parameter
+	 */
+	public J2EE13ProjectCreationWizardPage(WizardNewProjectCreationPage mainPage) {
+		super(mainPage);
+		this.setTitle(JDTJ2EEUIMessages
+				.getString("J2EE13ProjectCreationWizardPage.title"));//$NON-NLS-1$
+		this.setDescription(JDTJ2EEUIMessages
+				.getString("J2EE13ProjectCreationWizardPage.description"));//$NON-NLS-1$
+	}
 
+	/**
+	 * Add necessary source and libraries
+	 *
+	 * @param entries
+	 * @return
+	 * @throws CoreException
+	 */
+	protected IClasspathEntry[] checkEntries(IClasspathEntry[] entries)
+			throws CoreException {
+		// entries = JavaProjectUtil.mergeClasspathEntry(entries, JavaProjectUtil.createSourceClasspathEntry(fCurrProject, "src"));
+		entries = JavaProjectUtil.mergeClasspathEntry(entries, JavaRuntime
+				.getDefaultJREContainerEntry());
+		entries = JavaProjectUtil.mergeClasspathEntry(entries,
+				ClassPathContainerRepository.getInstance().getEntry(
+						J2EE13ClasspathContainer.CLASSPATH_CONTAINER));
+		return entries;
+	}
 
-   /**
-    * Add necessary source and libraries
-    *
-    * @param entries
-    * @return
-    * @throws CoreException
-    */
-   protected IClasspathEntry[] checkEntries(IClasspathEntry[] entries)
-      throws CoreException
-   {
-      // entries = JavaProjectUtil.mergeClasspathEntry(entries, JavaProjectUtil.createSourceClasspathEntry(fCurrProject, "src"));
-      entries = JavaProjectUtil.mergeClasspathEntry(entries, JavaRuntime.getDefaultJREContainerEntry());
-      entries = JavaProjectUtil.mergeClasspathEntry(entries, ClassPathContainerRepository.getInstance().getEntry(J2EE13ClasspathContainer.CLASSPATH_CONTAINER));
-      return entries;
-   }
+	protected String[] getBuilders ()
+	{
+		return new String[] { JDTJ2EECorePlugin.WST_VALIDATION_BUILDER_ID };
+	}
 }
--- plugins/org.jboss.ide.eclipse.jdt.j2ee.ui/src/main/org/jboss/ide/eclipse/jdt/j2ee/ui/wizards/projects/J2EE14ProjectCreationWizardPage.java
+++ plugins/org.jboss.ide.eclipse.jdt.j2ee.ui/src/main/org/jboss/ide/eclipse/jdt/j2ee/ui/wizards/projects/J2EE14ProjectCreationWizardPage.java
@@ -12,6 +12,7 @@ import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 import org.jboss.ide.eclipse.jdt.core.classpath.ClassPathContainerRepository;
 import org.jboss.ide.eclipse.jdt.core.util.JavaProjectUtil;
+import org.jboss.ide.eclipse.jdt.j2ee.core.JDTJ2EECorePlugin;
 import org.jboss.ide.eclipse.jdt.j2ee.core.classpath.J2EE14ClasspathContainer;
 import org.jboss.ide.eclipse.jdt.j2ee.ui.JDTJ2EEUIMessages;
 import org.jboss.ide.eclipse.jdt.ui.wizards.ProjectWizardPage;
@@ -19,39 +20,53 @@ import org.jboss.ide.eclipse.jdt.ws.core.classpath.WSIBasicProfile10ClasspathCon
 
 /**
  * Description of the Class
- *
- * @author    Laurent Etiemble
- * @version   $Revision$
+ * 
+ * @author Laurent Etiemble
+ * @version $Revision$
  */
-public class J2EE14ProjectCreationWizardPage extends ProjectWizardPage
-{
-   /**
-    *Constructor for the J2EE14ProjectCreationWizardPage object
-    *
-    * @param mainPage  Description of the Parameter
-    */
-   public J2EE14ProjectCreationWizardPage(WizardNewProjectCreationPage mainPage)
-   {
-      super(mainPage);
-      this.setTitle(JDTJ2EEUIMessages.getString("J2EE14ProjectCreationWizardPage.title"));//$NON-NLS-1$
-      this.setDescription(JDTJ2EEUIMessages.getString("J2EE14ProjectCreationWizardPage.description"));//$NON-NLS-1$
-   }
+public class J2EE14ProjectCreationWizardPage extends ProjectWizardPage {
+	/**
+	 * Constructor for the J2EE14ProjectCreationWizardPage object
+	 * 
+	 * @param mainPage
+	 *            Description of the Parameter
+	 */
+	public J2EE14ProjectCreationWizardPage(WizardNewProjectCreationPage mainPage) {
+		super(mainPage);
+		this.setTitle(JDTJ2EEUIMessages
+				.getString("J2EE14ProjectCreationWizardPage.title"));//$NON-NLS-1$
+		this.setDescription(JDTJ2EEUIMessages
+				.getString("J2EE14ProjectCreationWizardPage.description"));//$NON-NLS-1$
+	}
 
+	/**
+	 * Add necessary source and libraries
+	 * 
+	 * @param entries
+	 * @return
+	 * @throws CoreException
+	 */
+	protected IClasspathEntry[] checkEntries(IClasspathEntry[] entries)
+			throws CoreException {
+		// entries = JavaProjectUtil.mergeClasspathEntry(entries,
+		// JavaProjectUtil.createSourceClasspathEntry(fCurrProject, "src"));
+		entries = JavaProjectUtil.mergeClasspathEntry(entries, JavaRuntime
+				.getDefaultJREContainerEntry());
+		entries = JavaProjectUtil.mergeClasspathEntry(entries,
+				ClassPathContainerRepository.getInstance().getEntry(
+						J2EE14ClasspathContainer.CLASSPATH_CONTAINER));
+		entries = JavaProjectUtil
+				.mergeClasspathEntry(
+						entries,
+						ClassPathContainerRepository
+								.getInstance()
+								.getEntry(
+										WSIBasicProfile10ClasspathContainer.CLASSPATH_CONTAINER));
+		return entries;
+	}
 
-   /**
-    * Add necessary source and libraries
-    *
-    * @param entries
-    * @return
-    * @throws CoreException
-    */
-   protected IClasspathEntry[] checkEntries(IClasspathEntry[] entries)
-      throws CoreException
-   {
-      // entries = JavaProjectUtil.mergeClasspathEntry(entries, JavaProjectUtil.createSourceClasspathEntry(fCurrProject, "src"));
-      entries = JavaProjectUtil.mergeClasspathEntry(entries, JavaRuntime.getDefaultJREContainerEntry());
-      entries = JavaProjectUtil.mergeClasspathEntry(entries, ClassPathContainerRepository.getInstance().getEntry(J2EE14ClasspathContainer.CLASSPATH_CONTAINER));
-      entries = JavaProjectUtil.mergeClasspathEntry(entries, ClassPathContainerRepository.getInstance().getEntry(WSIBasicProfile10ClasspathContainer.CLASSPATH_CONTAINER));
-      return entries;
-   }
+	protected String[] getBuilders()
+	{
+		return new String[] { JDTJ2EECorePlugin.WST_VALIDATION_BUILDER_ID };
+	}
 }
