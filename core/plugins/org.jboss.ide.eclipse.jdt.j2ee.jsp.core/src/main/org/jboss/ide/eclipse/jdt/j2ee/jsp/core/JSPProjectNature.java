0fcdaaa2c6872ff28657deb0894ea03108c9997f
Marshall Culpepper <mculpepper@jboss.com>
2006-01-12 22:48:25 +0000

    [JBIDE-258] enabling JSP compilation automatically adds the wst validation builder (changed code to use the new ProjectUtil.add/removeProjectNature)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@747 a97e2381-89e5-4abb-bab3-167db6db766c

--- core/plugins/org.jboss.ide.eclipse.jdt.j2ee.jsp.core/src/main/org/jboss/ide/eclipse/jdt/j2ee/jsp/core/JSPProjectNature.java
+++ core/plugins/org.jboss.ide.eclipse.jdt.j2ee.jsp.core/src/main/org/jboss/ide/eclipse/jdt/j2ee/jsp/core/JSPProjectNature.java
@@ -6,12 +6,11 @@
  */
 package org.jboss.ide.eclipse.jdt.j2ee.jsp.core;
 
-import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
+import org.jboss.ide.eclipse.core.util.ProjectUtil;
+import org.jboss.ide.eclipse.jdt.j2ee.core.JDTJ2EECorePlugin;
 
 /**
  * JSP Nature that installs the JSP builder.
@@ -22,7 +21,6 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 public class JSPProjectNature implements IProjectNature
 {
    private IProject project;
-
 
    /**Constructor for the JSPProjectNature object */
    public JSPProjectNature()
@@ -39,32 +37,15 @@ public class JSPProjectNature implements IProjectNature
    public void configure()
       throws CoreException
    {
-      // Add the JSP Builder right after the Java Builder
-      IProjectDescription desc = this.project.getDescription();
-      ICommand[] commands = desc.getBuildSpec();
-      boolean found = false;
-
-      for (int i = 0; i < commands.length; ++i)
-      {
-         if (commands[i].getBuilderName().equals(JDTJ2EEJSPCorePlugin.JSP_BUILDER_ID))
-         {
-            found = true;
-            break;
-         }
-      }
-      if (!found)
-      {
-         //add builder to project
-         ICommand command = desc.newCommand();
-         command.setBuilderName(JDTJ2EEJSPCorePlugin.JSP_BUILDER_ID);
-         ICommand[] newCommands = new ICommand[commands.length + 1];
-
-         // Add it after other builders.
-         System.arraycopy(commands, 0, newCommands, 0, commands.length);
-         newCommands[commands.length] = command;
-         desc.setBuildSpec(newCommands);
-         this.project.setDescription(desc, new NullProgressMonitor());
-      }
+	   if (!ProjectUtil.projectHasBuilder(this.project, JDTJ2EEJSPCorePlugin.JSP_BUILDER_ID))
+	   {
+		   ProjectUtil.addProjectBuilder(this.project, JDTJ2EEJSPCorePlugin.JSP_BUILDER_ID);
+	   }
+	   
+	   if (!ProjectUtil.projectHasBuilder(this.project, JDTJ2EECorePlugin.WST_VALIDATION_BUILDER_ID))
+	   {
+		   ProjectUtil.addProjectBuilder(this.project, JDTJ2EECorePlugin.WST_VALIDATION_BUILDER_ID);
+	   }
    }
 
 
@@ -76,43 +57,12 @@ public class JSPProjectNature implements IProjectNature
    public void deconfigure()
       throws CoreException
    {
-      // Remove the JSP Builder
-      IProjectDescription desc = this.project.getDescription();
-      ICommand[] commands = desc.getBuildSpec();
-      boolean found = false;
-      int index = -1;
-
-      for (int i = 0; i < commands.length; ++i)
-      {
-         if (commands[i].getBuilderName().equals(JDTJ2EEJSPCorePlugin.JSP_BUILDER_ID))
-         {
-            found = true;
-            index = i;
-            break;
-         }
-      }
-      if (found && index >= 0)
-      {
-         ICommand[] newCommands = new ICommand[commands.length - 1];
-
-         // Remove builder to project
-         if (index == 0)
-         {
-            System.arraycopy(commands, 1, newCommands, 0, newCommands.length);
-         }
-         else if (index == commands.length)
-         {
-            System.arraycopy(commands, 0, newCommands, 0, newCommands.length);
-         }
-         else
-         {
-            System.arraycopy(commands, 0, newCommands, 0, index);
-            System.arraycopy(commands, index + 1, newCommands, index, newCommands.length - index);
-         }
-
-         desc.setBuildSpec(newCommands);
-         this.project.setDescription(desc, new NullProgressMonitor());
-      }
+	   if (ProjectUtil.projectHasBuilder(this.project, JDTJ2EEJSPCorePlugin.JSP_BUILDER_ID))
+	   {
+		   ProjectUtil.removeProjectBuilder(this.project, JDTJ2EEJSPCorePlugin.JSP_BUILDER_ID);
+	   }
+	   
+	   // we won't actually remove the WST validation builder (some people may still want validation without compilation..)
    }
 
 
