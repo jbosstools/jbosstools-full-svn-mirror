f5c2f8e1845f5aa0038deddadb81a46526083020
Marshall Culpepper <mculpepper@jboss.com>
2006-01-22 19:59:13 +0000

    hopefully fixed all the win32 batch scripts
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@753 a97e2381-89e5-4abb-bab3-167db6db766c

--- releng/org.jboss.ide.eclipse.releng/builders/build-nightly.bat
+++ releng/org.jboss.ide.eclipse.releng/builders/build-nightly.bat
@@ -1,17 +1,29 @@
-@if "%ANT_HOME%"=="" goto noAnt
-call %ANT_HOME%\bin\ant %2 %3 %4 %5 -lib ..\lib -f common/buildRequirements.xml cleanRequirements
+@echo off
 
-@if "%1"=="product" goto productBuild
-call %ANT_HOME%\bin\ant %2 %3 %4 %5 -lib ..\lib -f builder-wrap.xml nightly -Dbuilder=%1
+set target=%1
+shift
+
+if "%target%"=="" goto usage
+
+set ant=%ANT_HOME%\bin\ant
+
+if "%ANT_HOME%"=="" goto noAnt
+call %ant% %1 %2 %3 %4 %5 %6 -lib ..\lib -f common/buildRequirements.xml cleanRequirements
+
+if "%target%"=="product" goto productBuild
+call %ant% %1 %2 %3 %4 %5 %6 -lib ..\lib -f builder-wrap.xml nightly -Dbuilder=%target%
 goto end
 
 :productBuild
-call %ANT_HOME%\bin\ant %2 %3 %4 %5 -lib ..\lib -f product/productBuild.xml nightly > build.log
-call %ANT_HOME%\bin\ant %2 %3 %4 %5 -lib ..\lib -f product/buildResults.xml publish.log
+call %ant% %1 %2 %3 %4 %5 %6 -lib ..\lib -f product/productBuild.xml nightly > build.log
+call %ant% %1 %2 %3 %4 %5 %6 -lib ..\lib -f product/buildResults.xml publish.log
 goto end
 
 :noAnt
-@echo ANT_HOME is not set, please set ANT_HOME before calling this script
+echo ANT_HOME is not set, please set ANT_HOME before calling this script
+
+:usage
+echo Usage:
+echo		build-nightly [builder-name] (ant options)
 
 :end
-
--- releng/org.jboss.ide.eclipse.releng/builders/build-release.bat
+++ releng/org.jboss.ide.eclipse.releng/builders/build-release.bat
@@ -1,16 +1,37 @@
-@if "%ANT_HOME%"=="" goto noAnt
-call %ANT_HOME%\bin\ant %3 %4 %5 %6 -lib ..\lib -f common/buildRequirements.xml cleanRequirements
+@echo off
 
-@if "%1"=="product" goto productBuild
-call %ANT_HOME%\bin\ant %3 %4 %5 %6 -lib ..\lib -f builder-wrap.xml release -DreleaseNumber=%2 -Dbuilder=%1
+set target=%1
+shift
+
+set releaseName=%2
+shift
+
+set releaseType=%3
+shift
+
+if "%target%"=="" goto usage
+if "%releaseName%"=="" goto usage
+if "%releaseType%"=="" goto usage
+
+set ant=%ANT_HOME%\bin\ant
+
+if "%ANT_HOME%"=="" goto noAnt
+call %ant% %1 %2 %3 %4 %5 %6 -lib ..\lib -f common/buildRequirements.xml cleanRequirements
+
+if "%target%"=="product" goto productBuild
+call %ant% %1 %2 %3 %4 %5 %6 -lib ..\lib -f builder-wrap.xml release -DreleaseNumber=%releaseName% -DreleaseType=%releaseType% -Dbuilder=%target%
 goto end
 
 :productBuild
-call %ANT_HOME%\bin\ant %3 %4 %5 %6 -lib ..\lib -f product/productBuild.xml release -DreleaseNumber=%2 > build.log
-call %ANT_HOME%\bin\ant %2 %3 %4 %5 -lib ..\lib -f product/buildResults.xml publish.log
+call %ant% %1 %2 %3 %4 %5 %6 -lib ..\lib -f product/productBuild.xml release -DreleaseNumber=%releaseName% -DreleaseType=%releaseType% > build.log
+call %ant% %1 %2 %3 %4 %5 %6 -lib ..\lib -f product/buildResults.xml publish.log
 goto end
 
 :noAnt
-@echo ANT_HOME is not set, please set ANT_HOME before calling this script
+echo ANT_HOME is not set, please set ANT_HOME before calling this script
+
+:usage
+echo Usage:
+echo         build-release [builder-name] [release name] [release type (stable or development)] (ant options)
 
 :end
