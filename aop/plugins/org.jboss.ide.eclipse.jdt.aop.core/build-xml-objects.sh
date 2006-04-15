6952e3db8b0cb81ea9773eab4f9b2d1ee429dc2b
No Author <null@localhost>
2006-04-15 02:03:32 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_IDE_TESTS_ECLIPSE31'.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/Root_IDE_TESTS_ECLIPSE31@905 a97e2381-89e5-4abb-bab3-167db6db766c

--- aop/plugins/org.jboss.ide.eclipse.jdt.aop.core/build-xml-objects.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "------ Building XML Objects.. ------"
-echo "-- JWSDP_HOME = $JWSDP_HOME"
-echo "-- JBOSS_AOP_ECLIPSE = $JBOSS_AOP_ECLIPSE"
-
-$JWSDP_HOME/jaxb/bin/xjc.sh -extension -d $JBOSS_AOP_ECLIPSE/org.jboss.ide.eclipse.jdt.aop.core/generated -p org.jboss.ide.eclipse.jdt.aop.core.jaxb $JBOSS_AOP_ECLIPSE/org.jboss.ide.eclipse.jdt.aop.core/aop-report.xsd $JBOSS_AOP_ECLIPSE/org.jboss.ide.eclipse.jdt.aop.core/jboss-aop.xsd
-
-echo "------ Done -------"
