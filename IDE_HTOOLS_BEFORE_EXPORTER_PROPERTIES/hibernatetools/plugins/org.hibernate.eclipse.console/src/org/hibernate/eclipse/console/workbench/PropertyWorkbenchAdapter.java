f1ffec0320fb15c611acc8bc0f69e591ba904a63
No Author <null@localhost>
2006-04-19 05:24:34 +0000

    This commit was manufactured by cvs2svn to create tag 'IDE_HTOOLS_BEFORE_EXPORTER_PROPERTIES'.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@910 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ IDE_HTOOLS_BEFORE_EXPORTER_PROPERTIES/hibernatetools/plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/console/workbench/PropertyWorkbenchAdapter.java
@@ -0,0 +1,115 @@
+package org.hibernate.eclipse.console.workbench;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.progress.IDeferredWorkbenchAdapter;
+import org.hibernate.mapping.Any;
+import org.hibernate.mapping.Array;
+import org.hibernate.mapping.Bag;
+import org.hibernate.mapping.Component;
+import org.hibernate.mapping.DependantValue;
+import org.hibernate.mapping.IdentifierBag;
+import org.hibernate.mapping.List;
+import org.hibernate.mapping.ManyToOne;
+import org.hibernate.mapping.Map;
+import org.hibernate.mapping.OneToMany;
+import org.hibernate.mapping.OneToOne;
+import org.hibernate.mapping.PrimitiveArray;
+import org.hibernate.mapping.Property;
+import org.hibernate.mapping.Set;
+import org.hibernate.mapping.SimpleValue;
+import org.hibernate.mapping.Value;
+import org.hibernate.mapping.ValueVisitor;
+
+public class PropertyWorkbenchAdapter extends BasicWorkbenchAdapter implements
+		IDeferredWorkbenchAdapter {
+
+	public Object[] getChildren(Object o) {
+		Property p = (Property) o;
+		
+		Object[] result = (Object[]) p.getValue().accept(new ValueVisitor() {
+		
+			public Object accept(OneToOne oto) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(ManyToOne mto) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(Component component) {
+				return toArray(component.getPropertyIterator(), Property.class);				
+			}
+		
+			public Object accept(DependantValue value) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(SimpleValue value) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(Any any) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(Set set) {
+				return NO_CHILDREN; // should it look up the target entity?
+			}
+					
+			public Object accept(OneToMany many) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(Map map) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(Array list) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(PrimitiveArray primitiveArray) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(List list) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(IdentifierBag bag) {
+				return NO_CHILDREN;
+			}
+		
+			public Object accept(Bag bag) {
+				return NO_CHILDREN;
+			}		
+		});
+		
+		return result;
+	}
+
+	public ImageDescriptor getImageDescriptor(Object object) {
+		Property property = ((Property)object);
+		
+		return HibernateWorkbenchHelper.getImageDescriptor(property);		 
+	}
+
+	public String getLabel(Object o) {
+		Property property = ((Property)o);
+		Value value = property.getValue();
+		String typeName = (String) value.accept(new TypeNameValueVisitor(true));
+		
+		if (typeName!=null) {
+			return property.getName() + " : " + typeName;
+		}
+		
+		return property.getName(); 
+	}
+
+	public Object getParent(Object o) {
+		Property p = (Property) o;
+		return p.getPersistentClass();
+	}
+
+	
+}
