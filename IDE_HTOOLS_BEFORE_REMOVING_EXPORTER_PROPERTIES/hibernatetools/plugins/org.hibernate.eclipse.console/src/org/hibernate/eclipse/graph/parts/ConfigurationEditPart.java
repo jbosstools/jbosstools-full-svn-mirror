29d7f66c9d94f5179757e39bba71238acca20cab
No Author <null@localhost>
2006-04-22 08:13:50 +0000

    This commit was manufactured by cvs2svn to create tag 'IDE_HTOOLS_BEFORE_REMOVING_EXPORTER_PROPERTIES'.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@925 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ IDE_HTOOLS_BEFORE_REMOVING_EXPORTER_PROPERTIES/hibernatetools/plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/graph/parts/ConfigurationEditPart.java
@@ -0,0 +1,154 @@
+package org.hibernate.eclipse.graph.parts;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.FreeformLayer;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.LineBorder;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.EditPolicy;
+import org.eclipse.gef.GraphicalEditPart;
+import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
+import org.hibernate.eclipse.graph.layout.DelegatingLayoutManager;
+import org.hibernate.eclipse.graph.layout.GraphXYLayout;
+import org.hibernate.eclipse.graph.model.ConfigurationViewAdapter;
+import org.hibernate.eclipse.graph.model.GraphNode;
+import org.hibernate.eclipse.graph.policy.ConfigurationLayoutEditPolicy;
+
+public class ConfigurationEditPart extends AbstractGraphicalEditPart implements Observer {
+
+	private DelegatingLayoutManager delegatingLayoutManager;
+	private boolean manualLayoutActive = true;
+
+	public void activate() {
+		super.activate();
+		getConfigurationViewAdapter().addObserver(this);
+	}
+	
+	public void deactivate() {
+		super.deactivate();
+		getConfigurationViewAdapter().deleteObserver(this);
+	}
+	
+	public ConfigurationEditPart(ConfigurationViewAdapter configuration) {
+		setModel( configuration );
+	}
+
+	protected List getModelChildren() {
+		if ( getConfigurationViewAdapter() == null )
+			return super.getModelChildren();
+		return getConfigurationViewAdapter().getPersistentClasses();
+	}
+
+	protected IFigure createFigure() {
+		FreeformLayer layer = new FreeformLayer();
+		delegatingLayoutManager = new DelegatingLayoutManager(this);
+		layer.setLayoutManager( delegatingLayoutManager );
+		layer.setBorder( new LineBorder( 1 ) );
+		
+		//		 Create the static router for the connection layer
+		//ConnectionLayer connLayer = (ConnectionLayer)getLayer(LayerConstants.CONNECTION_LAYER);
+		//connLayer.setConnectionRouter(new ManhattanConnectionRouter());
+		return layer;
+	}
+
+	public ConfigurationViewAdapter getConfigurationViewAdapter() {
+		return (ConfigurationViewAdapter) getModel();
+	}
+
+	protected void createEditPolicies() {
+		installEditPolicy( EditPolicy.LAYOUT_ROLE,	new ConfigurationLayoutEditPolicy() );
+	}
+
+	/**
+	 * Updates the bounds of the table figure (without invoking any event
+	 * handling), and sets layout constraint data
+	 * 
+	 * @return whether the procedure execute successfully without any
+	 *         omissions. The latter occurs if any Table objects have no
+	 *         bounds set or if no figure is available for the TablePart
+	 */
+	public boolean resetFigureBounds(boolean updateConstraint)
+	{
+		List tableParts = getChildren();
+		
+		for (Iterator iter = tableParts.iterator(); iter.hasNext();)
+		{
+			GraphicalEditPart nodePart = (GraphicalEditPart) iter.next();
+			
+			// now check whether we can find an entry in the tableToNodesMap
+			Rectangle bounds = nodePart.getFigure().getBounds();
+			if (bounds == null)
+			{
+				// TODO handle this better
+				return false;
+			}
+			else
+			{
+				Figure tableFigure = (Figure) nodePart.getFigure();
+				if (tableFigure == null)
+				{
+					return false;
+				}
+				else
+				{
+					if (updateConstraint)
+					{
+						// pass the constraint information to the xy layout
+						// setting the width and height so that the
+						// preferred size will be applied
+						delegatingLayoutManager.setXYLayoutConstraint(tableFigure, new Rectangle(bounds.x, bounds.y,
+								-1, -1));
+					}
+				}
+			}			
+		}
+		return true;
+		
+	}
+
+	public boolean resetModelBounds(GraphXYLayout layout) {
+
+			List tableParts = getChildren();
+			
+			for (Iterator iter = tableParts.iterator(); iter.hasNext();)
+			{
+				GraphNodeEditPart classPart = (GraphNodeEditPart) iter.next();
+				IFigure persistentClassFigure = classPart.getFigure();
+
+				//if we don't find a node for one of the children then we should
+				// continue
+				if (persistentClassFigure == null)
+					continue;
+
+				Rectangle bounds = persistentClassFigure.getBounds().getCopy();
+				GraphNode node = classPart.getGraphNode();
+				node.setBounds(bounds);
+				if(layout!=null) {
+					layout.setConstraint(persistentClassFigure, bounds);
+				}
+			}
+
+			return true;
+	}
+
+	public void update(Observable o, Object arg) {
+		getFigure().setLayoutManager(delegatingLayoutManager);
+		refresh();
+	}
+
+	public boolean isManualLayoutActive() {
+		return manualLayoutActive;
+	}
+	
+	public void setManualLayoutActive(boolean manualLayoutActive) {
+		this.manualLayoutActive = manualLayoutActive;
+		getFigure().setLayoutManager(delegatingLayoutManager);
+		refresh();
+	}
+	
+}
---
+++ IDE_HTOOLS_BEFORE_REMOVING_EXPORTER_PROPERTIES/hibernatetools/plugins/org.hibernate.eclipse.console/src/org/hibernate/eclipse/launch/CodeGenerationSettings.java
@@ -0,0 +1,441 @@
+package org.hibernate.eclipse.launch;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.jdt.core.JavaConventions;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.ComboDialogField;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.DialogField;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.IDialogFieldListener;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.IStringButtonAdapter;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.SelectionButtonDialogField;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.StringButtonDialogField;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.StringDialogField;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
+import org.hibernate.console.ConsoleConfiguration;
+import org.hibernate.console.ImageConstants;
+import org.hibernate.console.KnownConfigurations;
+import org.hibernate.eclipse.console.HibernateConsolePlugin;
+import org.hibernate.eclipse.console.utils.DialogSelectionHelper;
+import org.hibernate.eclipse.console.utils.EclipseImages;
+import org.hibernate.eclipse.console.wizards.NewReverseEngineeringFileWizard;
+
+public class CodeGenerationSettings extends	AbstractLaunchConfigurationTab {
+
+	private ComboDialogField consoleConfigurationName;
+
+	private IStructuredSelection selection;
+
+	private SelectionButtonDialogField reverseengineer;
+	
+	private StringButtonDialogField outputdir;
+	
+	private StringButtonDialogField reverseEngineeringSettings;
+	
+	private StringButtonDialogField reverseEngineeringStrategy;
+    
+    private StringDialogField packageName;
+
+    private SelectionButtonDialogField preferRawCompositeIds;
+
+    private SelectionButtonDialogField useOwnTemplates;
+    private StringButtonDialogField templatedir;
+    
+    private LaunchAttributes attributes;
+    
+	public CodeGenerationSettings() {
+		super();
+	}
+
+    public void createControl(Composite parent) {
+		
+		//initializeDialogUnits(parent);
+		
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		
+		container.setLayout(layout);
+		layout.numColumns = 3;
+		layout.verticalSpacing = 10;
+		
+		consoleConfigurationName = new ComboDialogField(SWT.READ_ONLY);
+		consoleConfigurationName.setLabelText("Console &configuration:");
+		ConsoleConfiguration[] cfg = KnownConfigurations.getInstance().getConfigurations();
+		String[] names = new String[cfg.length];
+		for (int i = 0; i < cfg.length; i++) {
+			ConsoleConfiguration configuration = cfg[i];
+			names[i] = configuration.getName();
+		}
+		consoleConfigurationName.setItems(names);
+		
+		IDialogFieldListener fieldlistener = new IDialogFieldListener() {
+			public void dialogFieldChanged(DialogField field) {
+				dialogChanged();
+			}
+		};
+        
+        consoleConfigurationName.setDialogFieldListener(fieldlistener);
+		
+		outputdir = new StringButtonDialogField(new IStringButtonAdapter() {
+			public void changeControlPressed(DialogField field) {
+				IPath[] paths = DialogSelectionHelper.chooseFileEntries(getShell(),  getOutputDirectory(), new IPath[0], "Select output directory", "Choose directory in which the generated files will be stored", new String[] {"cfg.xml"}, false, true, false);
+				if(paths!=null && paths.length==1) {
+					outputdir.setText( ( (paths[0]).toOSString() ) );
+				}					
+			}
+		});
+        outputdir.setDialogFieldListener(fieldlistener);
+		outputdir.setLabelText("Output &directory:");
+		outputdir.setButtonLabel("&Browse...");
+		
+        templatedir = new StringButtonDialogField(new IStringButtonAdapter() {
+            public void changeControlPressed(DialogField field) {
+                IPath[] paths = DialogSelectionHelper.chooseFileEntries(getShell(),  getTemplateDirectory(), new IPath[0], "Select template directory", "Choose directory containing custom templates", new String[0], false, true, false);
+                if(paths!=null && paths.length==1) {
+                    templatedir.setText( ( (paths[0]).toOSString() ) );
+                }                   
+            }
+        });
+        templatedir.setDialogFieldListener(fieldlistener);
+        templatedir.setLabelText("Template &directory:");
+        templatedir.setButtonLabel("&Browse...");
+        
+		packageName = new StringDialogField();
+        packageName.setDialogFieldListener(fieldlistener);
+        packageName.setLabelText("&Package:");
+        
+        reverseEngineeringStrategy = new StringButtonDialogField(new IStringButtonAdapter() {
+		
+			public void changeControlPressed(DialogField field) {
+				String string = DialogSelectionHelper.chooseImplementation(ReverseEngineeringStrategy.class.getName(), reverseEngineeringStrategy.getText(), "Choose a reverse engineering strategy", getShell());
+				if(string!=null) {
+					reverseEngineeringStrategy.setText(string);
+				}		
+			}	
+		});
+        reverseEngineeringStrategy.setDialogFieldListener(fieldlistener);
+        reverseEngineeringStrategy.setLabelText("reveng. s&trategy:");
+        reverseEngineeringStrategy.setButtonLabel("&Browse...");
+		
+		reverseEngineeringSettings= new StringButtonDialogField(new IStringButtonAdapter() {
+            public void changeControlPressed(DialogField field) {
+            	int defaultChoice = 0;
+            	IPath reverseEngineeringSettingsFile = getReverseEngineeringSettingsFile();
+            	
+				if(reverseEngineeringSettingsFile==null) {
+            		defaultChoice = 0;
+            	} else {
+            		defaultChoice = 1;
+            	}
+				MessageDialog dialog = new MessageDialog(getShell(), 
+						"Setup reverse engineering", 
+						null, 
+						"Do you want to create a new reveng.xml or use an existing file ?", 
+						MessageDialog.QUESTION, 
+						new String[] { "Create &new...", "Use &existing...", IDialogConstants.CANCEL_LABEL}, 
+						defaultChoice);
+				int answer = dialog.open();
+				if(answer==0) { // create new
+					NewReverseEngineeringFileWizard wizard = new NewReverseEngineeringFileWizard();
+					wizard.init(PlatformUI.getWorkbench(), selection );
+					wizard.setSelectConfiguration(getConfigurationName());
+					IWorkbenchWindow win = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+					
+					WizardDialog wdialog = new WizardDialog(win.getShell(), wizard);
+					wdialog.open(); // This opens a dialog
+					IPath createdFilePath = wizard.getCreatedFilePath();
+					if(createdFilePath!=null) {
+						reverseEngineeringSettings.setText(createdFilePath.toOSString());
+					}
+				} else if (answer==1) { // use existing
+					IPath[] paths = DialogSelectionHelper.chooseFileEntries(getShell(),  reverseEngineeringSettingsFile, new IPath[0], "Select reverse engineering settings file", "Choose file from which settings for the reverse engineering will be read", new String[] {"reveng.xml"}, false, false, true);
+					if(paths!=null && paths.length==1) {
+						reverseEngineeringSettings.setText( ( (paths[0]).toOSString() ) );
+					}		
+				}                                	
+            }
+        });
+		reverseEngineeringSettings.setDialogFieldListener(fieldlistener);
+        reverseEngineeringSettings.setLabelText("reveng.&xml:");
+        reverseEngineeringSettings.setButtonLabel("&Setup...");
+		        
+		reverseengineer = new SelectionButtonDialogField(SWT.CHECK);
+		reverseengineer.setLabelText("Reverse engineer from JDBC Connection");
+		reverseengineer.setDialogFieldListener(fieldlistener);
+          
+        useOwnTemplates = new SelectionButtonDialogField(SWT.CHECK);
+        useOwnTemplates.setDialogFieldListener(fieldlistener);
+        useOwnTemplates.setLabelText("Use custom templates");
+       
+        preferRawCompositeIds = new SelectionButtonDialogField(SWT.CHECK);
+        preferRawCompositeIds.setLabelText("Generate basic typed composite ids");
+        preferRawCompositeIds.setSelection(true);
+        preferRawCompositeIds.setDialogFieldListener(fieldlistener);
+        
+		useOwnTemplates.attachDialogField(templatedir);
+        reverseengineer.attachDialogFields(new DialogField[] { packageName, preferRawCompositeIds, reverseEngineeringSettings, reverseEngineeringStrategy });
+       
+		consoleConfigurationName.doFillIntoGrid(container, 3);
+		Control[] controls = outputdir.doFillIntoGrid(container, 3);
+		// Hack to tell the text field to stretch!
+		( (GridData)controls[1].getLayoutData() ).grabExcessHorizontalSpace=true;
+		reverseengineer.doFillIntoGrid(container, 3);
+        packageName.doFillIntoGrid(container, 3);        
+		reverseEngineeringSettings.doFillIntoGrid(container, 3);
+		reverseEngineeringStrategy.doFillIntoGrid(container, 3);
+		
+        fillLabel(container);
+        preferRawCompositeIds.doFillIntoGrid(container, 2);
+		fillLabel(container);
+        useOwnTemplates.doFillIntoGrid(container, 3);
+        controls = templatedir.doFillIntoGrid(container, 3);
+        // Hack to tell the text field to stretch!
+        ( (GridData)controls[1].getLayoutData() ).grabExcessHorizontalSpace=true;
+        
+		dialogChanged();
+		setControl(container);
+	}
+
+    private void fillLabel(Composite container) {
+        new Label(container, SWT.NULL);
+    }
+
+
+	private void dialogChanged() {
+		boolean configSelected = getConfigurationName().length()==0;
+		outputdir.setEnabled(!configSelected);
+		reverseengineer.setEnabled(!configSelected);
+		useOwnTemplates.setEnabled(!configSelected);
+		
+		if (configSelected) {
+			updateStatus("Console configuration must be specified");
+			return;
+		}
+        
+        String msg = checkDirectory(getOutputDirectory(), "output directory");
+        
+        if (msg!=null) {
+            updateStatus(msg);
+            return;
+        } 
+
+        if(packageName.isEnabled() && getOutputPackage().length()>0) {
+        	IStatus val= JavaConventions.validatePackageName(getOutputPackage() );
+            if (val.getSeverity() == IStatus.ERROR || val.getSeverity() == IStatus.WARNING) {
+                updateStatus(val.getMessage() );
+                return;
+            } 
+        	
+        }
+        
+        if(reverseEngineeringSettings.getText().trim().length()>0) {
+        	msg = checkFile(getReverseEngineeringSettingsFile(), "reveng.xml");
+        	if(msg!=null) {
+        		updateStatus(msg);
+        		return;
+        	}
+        }
+
+        if(useOwnTemplates.isSelected() ) {
+            msg = checkDirectory(getTemplateDirectory(), "template directory");
+            if (msg!=null) {
+                updateStatus(msg);
+                return;
+            } else {
+            	// imprecise and inefficient to check recursively all for .vm
+                /*IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(getTemplateDirectory() );
+                IResource[] files = new IFile[0];
+                boolean found = false;
+                
+                if(resource.getType() == IResource.FOLDER) {
+                    try {
+                        found = ( (IFolder)resource).accept(new IResourceProxyVisitor() {
+						
+							public boolean visit(IResourceProxy proxy) throws CoreException {								
+								return false;
+							}
+						
+						});
+                    } catch (CoreException e) {
+                        // noop
+                    }
+                }
+                
+                if(!found) {
+                    setMessage("No templates (*.vm) found in template directory", IMessageProvider.WARNING);
+                } else {
+                    setMessage(null);
+                }*/
+            }
+        } else {
+            setMessage(null);
+        }
+        
+		updateStatus(null);
+	}
+
+
+
+    protected String checkDirectory(IPath path, String name) {
+        IResource res= ResourcesPlugin.getWorkspace().getRoot().findMember(path);
+        if (res != null) {
+            int resType= res.getType();
+            if (resType == IResource.PROJECT || resType == IResource.FOLDER) {
+                IProject proj= res.getProject();
+                if (!proj.isOpen() ) {
+                    return "Project for " + name + " is closed";                    
+                }                               
+            } else {
+                return name + " has to be a folder or project";
+            }
+        } else {
+            return name + " does not exist";
+        }
+        return null;
+    }
+    
+    protected String checkFile(IPath path, String name) {
+        IResource res= ResourcesPlugin.getWorkspace().getRoot().findMember(path);
+        if (res != null) {
+            int resType= res.getType();
+            if (resType == IResource.FILE) {
+                return null;
+            } else {
+            	return name + " must be a file";
+            }
+        } else {
+            return name + " does not exist";
+        }
+    }
+    
+    private void updateStatus(String message) {
+        setErrorMessage(message);
+        updateLaunchConfigurationDialog();
+    }
+    
+	public String getConfigurationName() {
+		return consoleConfigurationName.getText();
+	}
+
+	private Path pathOrNull(String p) {
+		if(p==null || p.trim().length()==0) {
+			return null;
+		} else {
+			return new Path(p);
+		}
+	}
+
+
+	/**
+	 * @return
+	 */
+	public boolean isReverseEngineerEnabled() {
+		return reverseengineer.isSelected();
+	}
+
+	public IPath getOutputDirectory() {
+		return pathOrNull(outputdir.getText() );
+	}
+    
+    public IPath getTemplateDirectory() {
+        return pathOrNull(templatedir.getText() );
+    }
+
+    public String getOutputPackage() {
+          return packageName.getText();
+    }
+
+    public boolean isPreferBasicCompositeIds() {
+        return preferRawCompositeIds.isSelected();
+    }
+
+	private IPath getReverseEngineeringSettingsFile() {
+		return pathOrNull(reverseEngineeringSettings.getText() );
+	}    
+    
+	private String getReverseEngineeringStrategy() {
+		return reverseEngineeringStrategy.getText();
+	}   
+	
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+//	   try {
+//	      attributes = new ExporterAttributes(configuration);
+//       } catch (CoreException ce) {
+//          HibernateConsolePlugin.getDefault().logErrorMessage("Problem when setting up defaults for launch configuration", ce);
+//       }
+	}
+
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		try {
+           attributes = new LaunchAttributes(configuration);
+           consoleConfigurationName.setText(attributes.getConsoleConfigurationName());
+           preferRawCompositeIds.setSelection(attributes.isPreferBasicCompositeIds());
+           outputdir.setText(safeText(attributes.getOutputPath()));
+           reverseengineer.setSelection(attributes.isReverseEngineer());
+           reverseEngineeringSettings.setText(safeText(attributes.getRevengSettings()));
+           reverseEngineeringStrategy.setText(safeText(attributes.getRevengStrategy()));
+           useOwnTemplates.setSelection(attributes.isUseOwnTemplates());
+           packageName.setText(safeText(attributes.getPackageName()));
+           templatedir.setText(safeText(attributes.getTemplatePath()));
+		} catch (CoreException ce) {
+			HibernateConsolePlugin.getDefault().logErrorMessage("Problem when reading hibernate tools launch configuration", ce);
+		} 		
+	}
+
+	private String safeText(String text) {
+		return text==null?"":text;
+	}
+
+	private String strOrNull(String text) {
+		if(text==null || text.trim().length()==0) {
+			return null;
+		} else {
+			return text;
+		}
+	}
+	
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_OUTPUT_DIR, strOrNull(outputdir.getText()));
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_PREFER_BASIC_COMPOSITE_IDS, preferRawCompositeIds.isSelected());
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_REVERSE_ENGINEER, isReverseEngineerEnabled());
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_REVERSE_ENGINEER_STRATEGY, strOrNull(reverseEngineeringStrategy.getText()));
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_REVERSE_ENGINEER_SETTINGS, strOrNull(reverseEngineeringSettings.getText()));
+		
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_USE_OWN_TEMPLATES, useOwnTemplates.isSelected());
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_TEMPLATE_DIR, strOrNull(templatedir.getText()));
+		
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_CONSOLE_CONFIGURATION_NAME, getConfigurationName());        
+		configuration.setAttribute(HibernateLaunchConstants.ATTR_PACKAGE_NAME, getOutputPackage());
+        
+	}
+
+	public String getName() {
+		return "Main";
+	}
+	
+	public Image getImage() {
+		return EclipseImages.getImage(ImageConstants.MINI_HIBERNATE);
+	}
+
+	
+}
