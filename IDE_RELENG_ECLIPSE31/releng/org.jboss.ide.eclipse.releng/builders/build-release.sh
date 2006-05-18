bfad0ecc7a31f099a7b61afb87ff3d6b32540c4d
No Author <null@localhost>
2006-05-18 21:15:43 +0000

    This commit was manufactured by cvs2svn to create branch 'IDE_RELENG_ECLIPSE31'.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches@986 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ IDE_RELENG_ECLIPSE31/releng/org.jboss.ide.eclipse.releng/builders/build-release.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+TARGET=$1
+RELEASETYPE=$2
+RELEASE=$3
+shift
+shift
+shift
+
+ANT=$ANT_HOME/bin/ant
+
+if [ "$ANT_HOME" = "" ]; then
+	echo ANT_HOME is not set, please set ANT_HOME before calling this script
+	exit
+fi
+
+$ANT $@ -lib ../lib -f common/buildRequirements.xml cleanRequirements
+$ANT $@ -lib ../lib -f common/buildRequirements.xml getBaseRequirements
+
+if [ "$TARGET" = "product" ]; then
+	$ANT $@ -lib ../lib -f product/productBuild.xml release -DreleaseType=$RELEASETYPE -DreleaseNumber=$RELEASE | tee build.log
+
+	if [ "$?" = "0" ]; then
+		$ANT $@ -lib ../lib -f product/buildResults.xml publish.log
+	else
+		echo "\t[JBossIDE-Build ERROR] There was an error running the build"
+		exit -1
+	fi
+
+else
+	$ANT $@ -lib ../lib -f builder-wrap.xml release -Dbuilder=$TARGET -DreleaseType=$RELEASETYPE -DreleaseNumber=$RELEASE
+fi
