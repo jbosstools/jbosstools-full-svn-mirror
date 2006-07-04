ce0473c800fdc7342faf5101b14a8dcb1ea2e11b
No Author <null@localhost>
2006-07-04 09:58:35 +0000

    This commit was manufactured by cvs2svn to create branch 'v_0_5_alpha1'.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches@1139 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ v_0_5_alpha1/netbeans/hibernate-console/src/org/hibernate/netbeans/console/util/HibernateExecutor.java
@@ -0,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+
+package org.hibernate.netbeans.console.util;
+
+import org.openide.util.RequestProcessor;
+
+/**
+ * @author leon
+ */
+public class HibernateExecutor {
+    
+    private static RequestProcessor hibernateProcessor = new RequestProcessor("Hibernate", 1);
+    
+    private HibernateExecutor() {
+    }
+    
+    public static void execute(Runnable runnable) {
+        hibernateProcessor.post(runnable);
+    }
+    
+}
