d3e52888640eb73560adf2d54c87a40c0cbbb264
No Author <null@localhost>
2006-07-13 19:24:52 +0000

    This commit was manufactured by cvs2svn to create tag 'IDE_AOP_DEVELOPER_1_2_BETA1'.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@1218 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ IDE_AOP_DEVELOPER_1_2_BETA1/aop/plugins/org.jboss.ide.eclipse.jdt.aop.core/src/main/org/jboss/ide/eclipse/jdt/aop/core/classpath/AopJdk15ClasspathContainer.java
@@ -0,0 +1,60 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.ide.eclipse.jdt.aop.core.classpath;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+/**
+ * @author Marshall
+ */
+public class AopJdk15ClasspathContainer extends AopClasspathContainer
+{
+   public static final String CONTAINER_ID = "org.jboss.ide.eclipse.jdt.aop.core.classpath.AOP_15_CONTAINER";
+
+   public static final String DESCRIPTION = "JBossAOP 1.3 Libraries (jdk 1.5)";
+
+   protected final static IPath aopJars[] = new IPath[]
+   {new Path("jboss-aop-jdk50.jar"), new Path("jboss-common.jar"), new Path("qdox.jar"), new Path("concurrent.jar"),
+         new Path("trove.jar"), new Path("javassist.jar"), new Path("jboss-aspect-library-jdk50.jar"),
+         new Path("pluggable-instrumentor.jar")};
+
+   public AopJdk15ClasspathContainer(IPath path)
+   {
+      super(path);
+   }
+
+   protected IPath[] getAopJarRelativePaths()
+   {
+      return aopJars;
+   }
+
+   public String getContainerId()
+   {
+      return CONTAINER_ID;
+   }
+
+   public String getDescription()
+   {
+      return DESCRIPTION;
+   }
+}
