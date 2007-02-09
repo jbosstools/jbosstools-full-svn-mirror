ebc7470657132a836dd6d0e8b120fb58c5f5078c
Marshall Culpepper <mculpepper@jboss.com>
2007-02-09 19:02:01 +0000

    a stub build listener implementation for convenience
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1676 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ core/plugins/org.jboss.ide.eclipse.packages.core/src/main/org/jboss/ide/eclipse/packages/core/model/AbstractPackagesBuildListener.java
@@ -0,0 +1,43 @@
+package org.jboss.ide.eclipse.packages.core.model;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IStatus;
+
+public class AbstractPackagesBuildListener implements IPackagesBuildListener {
+
+	public void buildFailed(IPackage pkg, IStatus status) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void finishedBuild(IProject project) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void finishedBuildingPackage(IPackage pkg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void finishedCollectingFileSet(IPackageFileSet fileset) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void startedBuild(IProject project) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void startedBuildingPackage(IPackage pkg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void startedCollectingFileSet(IPackageFileSet fileset) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
