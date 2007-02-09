20aef8fb1dc4bc7cf8c5751cdbaf71ecf3cf282c
Rob Stryker <rob.stryker@jboss.com>
2007-02-09 18:54:33 +0000

    made it work
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1675 a97e2381-89e5-4abb-bab3-167db6db766c

--- ejb3/plugins/org.jboss.ide.eclipse.ejb3.wizards.core/src/main/org/jboss/ide/eclipse/ejb3/core/module/Ejb30PackageType.java
+++ ejb3/plugins/org.jboss.ide.eclipse.ejb3.wizards.core/src/main/org/jboss/ide/eclipse/ejb3/core/module/Ejb30PackageType.java
@@ -3,6 +3,7 @@ package org.jboss.ide.eclipse.ejb3.core.module;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jst.j2ee.internal.project.J2EEProjectUtilities;
 import org.eclipse.jst.server.core.IWebModule;
 import org.eclipse.wst.server.core.IModule;
 import org.jboss.ide.eclipse.as.core.packages.ObscurelyNamedPackageTypeSuperclass;
@@ -15,41 +16,22 @@ public class Ejb30PackageType extends ObscurelyNamedPackageTypeSuperclass {
 
 	public static final String ID = "org.jboss.ide.eclipse.ejb3.wizards.core.ejbPackageType";
 	public String getAssociatedModuleType() {
-		return "jbide.jboss30";
+		return "jbide.ejb30";
 	}
 
 	public IPackage createDefaultConfiguration(IProject project, IProgressMonitor monitor) {
-		IModule mod = getModule(project);
-		if( mod == null ) 
-			return createDefaultConfiguration2(project, monitor);
-		else
-			return createDefaultConfigFromModule(mod, monitor);
+		boolean facetFound = J2EEProjectUtilities.isProjectOfType(project, getAssociatedModuleType()); 
+		return createDefaultConfiguration(project, facetFound, monitor);
 	}
 	
-	protected IPackage createDefaultConfiguration2(IProject project, IProgressMonitor monitor) {
+	public IPackage createDefaultConfiguration(IProject project, boolean facetFound, IProgressMonitor monitor) {
+		String metaInfDir = (facetFound ? EJBMODULE + Path.SEPARATOR : "") + METAINF;
+		
 		IPackage topLevel = createGenericIPackage(project, null, project.getName() + ".jar");
 		topLevel.setDestinationContainer(project);
 		IPackageFolder metainf = addFolder(project, topLevel, METAINF);
 		IPackageFolder lib = addFolder(project, metainf, LIB);
-		addFileset(project, metainf, METAINF, null);
-		return topLevel;
+		addFileset(project, metainf, metaInfDir, null);
+		return topLevel;		
 	}
-
-	protected IPackage createDefaultConfigFromModule(IModule mod, IProgressMonitor monitor) {
-		try {
-			IProject project = mod.getProject();
-
-			IPackage topLevel = createGenericIPackage(project, null, project.getName() + ".jar");
-			topLevel.setDestinationContainer(project);
-			IPackageFolder metainf = addFolder(project, topLevel, METAINF);
-			IPackageFolder lib = addFolder(project, metainf, LIB);
-			addFileset(project, metainf, EJBMODULE + Path.SEPARATOR + METAINF, null);
-
-			return topLevel;
-		} catch( Exception e ) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
 }
