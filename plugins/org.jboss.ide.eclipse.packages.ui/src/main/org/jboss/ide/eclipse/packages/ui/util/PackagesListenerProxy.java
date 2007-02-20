e0cd1308907e369989f098af9f1f73a37c5fec80
Marshall Culpepper <mculpepper@jboss.com>
2007-02-20 07:27:21 +0000

    added projectRegistered callback for ProjectModelListener, added project name to the project packages view when no packages exist yet [JBIDE-451]
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/core@1745 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.ide.eclipse.packages.ui/src/main/org/jboss/ide/eclipse/packages/ui/util/PackagesListenerProxy.java
+++ plugins/org.jboss.ide.eclipse.packages.ui/src/main/org/jboss/ide/eclipse/packages/ui/util/PackagesListenerProxy.java
@@ -1,5 +1,6 @@
 package org.jboss.ide.eclipse.packages.ui.util;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.swt.widgets.Display;
 import org.jboss.ide.eclipse.packages.core.model.IPackageNode;
 import org.jboss.ide.eclipse.packages.core.model.IPackagesModelListener;
@@ -14,22 +15,26 @@ public class PackagesListenerProxy implements IPackagesModelListener
 		public static final int REMOVED = 1;
 		public static final int CHANGED = 2;
 		public static final int ATTACHED = 3;
+		public static final int PROJECT_REGISTERED = 4;
 		
 		public IPackageNode node;
+		public IProject project;
 		public int event;
 		public void run ()
 		{
-			if (event > -1 && node != null)
+			if (event > -1)
 			{
 				switch (event) {
 				case ADDED: listener.packageNodeAdded(node); break;
 				case REMOVED: listener.packageNodeRemoved(node); break;
 				case CHANGED: listener.packageNodeChanged(node); break;
 				case ATTACHED: listener.packageNodeAttached(node); break;
+				case PROJECT_REGISTERED: listener.projectRegistered(project); break;
 				}
 			}
 			
 			node = null;
+			project = null;
 			event = -1;
 		}
 	}
@@ -37,6 +42,12 @@ public class PackagesListenerProxy implements IPackagesModelListener
 	public PackagesListenerProxy (IPackagesModelListener listener)
 	{
 		this.listener = listener;
+	}
+	
+	public void projectRegistered(IProject project) {
+		internalListener.project = project;
+		internalListener.event = Listener.PROJECT_REGISTERED;
+		Display.getDefault().syncExec(internalListener);
 	}
 	
 	public void packageNodeAdded(IPackageNode added) {
