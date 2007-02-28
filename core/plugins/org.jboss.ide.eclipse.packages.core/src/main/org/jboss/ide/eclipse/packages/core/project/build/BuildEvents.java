e3fe2db3230f0203a7b6210a6502283c56397783
Marshall Culpepper <mculpepper@jboss.com>
2007-02-28 16:36:43 +0000

    moved the scanner cache and changePackage/changeFileset methods over to BuildFileOperations (analagous w/ removePackage/Fileset updatePackage/Fileset).
    lots of bug fixes in build file operations, all unit tests should now be 100% again, including a new test for overlapping filesets.
    
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1805 a97e2381-89e5-4abb-bab3-167db6db766c

--- core/plugins/org.jboss.ide.eclipse.packages.core/src/main/org/jboss/ide/eclipse/packages/core/project/build/BuildEvents.java
+++ core/plugins/org.jboss.ide.eclipse.packages.core/src/main/org/jboss/ide/eclipse/packages/core/project/build/BuildEvents.java
@@ -1,11 +1,8 @@
 package org.jboss.ide.eclipse.packages.core.project.build;
 
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.tools.ant.DirectoryScanner;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
@@ -14,25 +11,18 @@ import org.jboss.ide.eclipse.packages.core.Trace;
 import org.jboss.ide.eclipse.packages.core.model.IPackage;
 import org.jboss.ide.eclipse.packages.core.model.IPackageFileSet;
 import org.jboss.ide.eclipse.packages.core.model.IPackageNode;
-import org.jboss.ide.eclipse.packages.core.model.IPackageNodeVisitor;
 import org.jboss.ide.eclipse.packages.core.model.IPackagesBuildListener;
 import org.jboss.ide.eclipse.packages.core.model.IPackagesModelListener;
-import org.jboss.ide.eclipse.packages.core.model.internal.PackageFileSetImpl;
 import org.jboss.ide.eclipse.packages.core.model.internal.PackagesModel;
-
-import de.schlichtherle.io.ArchiveDetector;
-import de.schlichtherle.io.File;
 
 public class BuildEvents implements IPackagesModelListener {
 
 	private PackageBuildDelegate builder;
 	private NullProgressMonitor nullMonitor = new NullProgressMonitor();
-	private Hashtable scannerCache;
 	
 	public BuildEvents (PackageBuildDelegate builder)
 	{
 		this.builder = builder;
-		this.scannerCache = new Hashtable();
 		
 		PackagesModel.instance().addPackagesModelListener(this);
 	}
@@ -142,32 +132,11 @@ public class BuildEvents implements IPackagesModelListener {
 		
 		if (added.getNodeType() == IPackageNode.TYPE_PACKAGE)
 		{
-			updateScannerCache ((IPackage) added);
+			builder.getFileOperations().updateScannerCache ((IPackage) added);
 		}
 		else if (added.getNodeType() == IPackageNode.TYPE_PACKAGE_FILESET)
 		{
-			updateScannerCache ((IPackageFileSet)added);
-		}
-	}
-
-	public void updateScannerCache (IPackageFileSet fileset)
-	{
-		scannerCache.put(fileset, ((PackageFileSetImpl)fileset).createDirectoryScanner(true));
-	}
-	
-	public void updateScannerCache (IPackage pkg)
-	{
-		if (pkg.isTopLevel())
-		{
-			pkg.accept(new IPackageNodeVisitor () {
-				public boolean visit(IPackageNode node) {
-					if (node.getNodeType() == IPackageNode.TYPE_PACKAGE_FILESET)
-					{
-						updateScannerCache((IPackageFileSet)node);
-					}
-					return true;
-				}
-			});
+			builder.getFileOperations().updateScannerCache ((IPackageFileSet)added);
 		}
 	}
 	
@@ -179,60 +148,11 @@ public class BuildEvents implements IPackagesModelListener {
 		
 		if (changed.getNodeType() == IPackageNode.TYPE_PACKAGE_FILESET)
 		{
-			filesetChanged ((IPackageFileSet)changed);
+			builder.getFileOperations().changeFileset ((IPackageFileSet)changed);
 		}
 		else if (changed.getNodeType() == IPackageNode.TYPE_PACKAGE)
 		{
-			packageChanged ((IPackage)changed);
-		}
-	}
-
-	private void filesetChanged (IPackageFileSet fileset)
-	{
-		builder.getFileOperations().updateNode(fileset);
-		IPackageFileSet filesets[] = new IPackageFileSet[] { fileset };
-		PackageFileSetImpl filesetImpl = (PackageFileSetImpl) fileset;
-		
-		DirectoryScanner oldScanner = (DirectoryScanner) scannerCache.get(fileset);
-		
-		if (oldScanner != null)
-		{
-			if (fileset.isInWorkspace())
-			{
-				IFile oldFiles[] = filesetImpl.findMatchingFiles(oldScanner);
-				for (int i = 0; i < oldFiles.length; i++)
-				{
-					builder.getFileOperations().removeFileFromFilesets(oldFiles[i], filesets);
-				}
-			} else {
-				IPath oldPaths[] = filesetImpl.findMatchingPaths(oldScanner);
-				for (int i = 0; i < oldPaths.length; i++)
-				{
-					builder.getFileOperations().removePathFromFilesets(oldPaths[i], filesets);
-				}
-			}
-		}
-	}
-	
-	private void packageChanged (IPackage pkg)
-	{
-		File packageFile = TruezipUtil.getPackageFile(pkg);
-		
-		if (! packageFile.getName().equals(pkg.getName()))
-		{
-			// File name was changed, rename
-			File newPackageFile = new File(packageFile.getParent(), pkg.getName());
-			packageFile.renameTo(newPackageFile, packageFile.getArchiveDetector());
-		}
-		else if (packageFile.getDelegate().isFile() && pkg.isExploded())
-		{
-			// Changed to exploded from compressed
-			packageFile.renameTo(packageFile, ArchiveDetector.DEFAULT);
-		}
-		else if (packageFile.getDelegate().isDirectory() && !pkg.isExploded())
-		{
-			//	Changed to compressed from exploded
-			packageFile.renameTo(packageFile, ArchiveDetector.NULL);
+			builder.getFileOperations().changePackage((IPackage)changed);
 		}
 	}
 	
@@ -251,5 +171,4 @@ public class BuildEvents implements IPackagesModelListener {
 			}
 		}
 	}
-
 }
