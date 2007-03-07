5173435293bd7c72c743c7d45148348d25c0f8de
Marshall Culpepper <mculpepper@jboss.com>
2007-03-07 22:33:41 +0000

    removed custom manifest UI (removing from API eventually)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1855 a97e2381-89e5-4abb-bab3-167db6db766c

--- core/plugins/org.jboss.ide.eclipse.packages.ui/src/main/org/jboss/ide/eclipse/packages/ui/wizards/pages/PackageInfoWizardPage.java
+++ core/plugins/org.jboss.ide.eclipse.packages.ui/src/main/org/jboss/ide/eclipse/packages/ui/wizards/pages/PackageInfoWizardPage.java
@@ -24,7 +24,6 @@ import org.jboss.ide.eclipse.packages.core.Trace;
 import org.jboss.ide.eclipse.packages.core.model.IPackage;
 import org.jboss.ide.eclipse.packages.core.model.IPackageNode;
 import org.jboss.ide.eclipse.packages.core.model.PackagesCore;
-import org.jboss.ide.eclipse.packages.core.model.internal.PackageImpl;
 import org.jboss.ide.eclipse.packages.core.model.internal.PackagesModel;
 import org.jboss.ide.eclipse.packages.ui.PackagesUIMessages;
 import org.jboss.ide.eclipse.packages.ui.PackagesUIPlugin;
@@ -40,12 +39,8 @@ public class PackageInfoWizardPage extends WizardPageWithNotification {
 	private Text packageNameText;
 	private Button compressedButton;
 	private Button explodedButton;
-	private Button customManifestCheck;
-	private Text manifestText;
-	private Button manifestBrowseButton;
 	private String packageName;
 	private boolean packageExploded;
-	private boolean manifestEnabled;
 	private IFile manifestFile;
 	private PackageDestinationComposite destinationComposite;
 	private IPackage pkg;
@@ -79,9 +74,9 @@ public class PackageInfoWizardPage extends WizardPageWithNotification {
 		new Label(pkgNameComposite, SWT.NONE).setImage(PackagesUIPlugin.getImage(PackagesUIPlugin.IMG_PACKAGE));
 		
 		packageNameText = new Text(pkgNameComposite, SWT.BORDER);
-		packageName = "Untitled." + wizard.getPackageExtension();
+		packageName = wizard.getProject().getName() + "." + wizard.getPackageExtension();
 		packageNameText.setText(packageName);
-		packageNameText.setSelection(0, "Untitled.".length()-1);
+		packageNameText.setSelection(0, wizard.getProject().getName().length());
 		expand(packageNameText);
 		
 		GridData pkgNameData = new GridData(GridData.FILL_HORIZONTAL);
@@ -114,33 +109,6 @@ public class PackageInfoWizardPage extends WizardPageWithNotification {
 			}
 		});
 		destinationComposite.setLayoutData(destinationTextData);
-		
-		customManifestCheck = new Button(infoGroup , SWT.CHECK);
-		customManifestCheck.setText(PackagesUIMessages.PackageInfoWizardPage_customManifest_label);
-		customManifestCheck.addSelectionListener(new SelectionAdapter () {
-			public void widgetSelected(SelectionEvent e) {
-				boolean checked = customManifestCheck.getSelection();
-				manifestBrowseButton.setEnabled(checked);
-				manifestEnabled = checked;
-				validate();
-			}
-		});
-		
-		manifestText = new Text(infoGroup, SWT.BORDER);
-		expand(manifestText);
-		manifestText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				if (validate())
-				{
-					manifestFile = wizard.getProject().getFile(manifestText.getText()); 
-				}
-			}
-		});
-		
-		manifestBrowseButton = new Button(infoGroup, SWT.PUSH);
-		manifestBrowseButton.setText(PackagesUIMessages.PackageInfoWizardPage_manifestBrowseButton_label);
-		manifestText.setEnabled(false);
-		manifestBrowseButton.setEnabled(false);
 		
 		Group packageTypeGroup = new Group(main, SWT.NONE);
 		packageTypeGroup.setLayout(new GridLayout(1, false));
@@ -189,18 +157,6 @@ public class PackageInfoWizardPage extends WizardPageWithNotification {
 				destinationComposite.setPackageNodeDestination(pkg.getParent());
 			}
 			
-			if (pkg.hasManifest())
-			{
-				PackageImpl pkgImpl = (PackageImpl) pkg;
-				
-				manifestEnabled = true;
-				manifestFile = pkgImpl.getManifestFile();
-				manifestText.setText(manifestFile.getLocation().toString());
-				manifestText.setEnabled(true);
-				manifestBrowseButton.setEnabled(true);
-				customManifestCheck.setSelection(true);
-			}
-			
 			if (pkg.isExploded())
 			{
 				explodedButton.setEnabled(true);
@@ -220,19 +176,6 @@ public class PackageInfoWizardPage extends WizardPageWithNotification {
 		}
 		else {
 			setErrorMessage(null);
-		}
-		
-		if (customManifestCheck.getSelection())
-		{
-			if (manifestText.getText() == null || manifestText.getText().length() == 0)
-			{
-				setErrorMessage(PackagesUIMessages.PackageInfoWizardPage_error_noManifest);
-				setPageComplete(false);
-				return false;
-			}
-			else {
-				setErrorMessage(null);
-			}
 		}
 		
 		Object destination = getPackageDestination();
@@ -341,10 +284,6 @@ public class PackageInfoWizardPage extends WizardPageWithNotification {
 		
 		pkg.setName(getPackageName());
 		pkg.setExploded(isPackageExploded());
-		if (isManifestEnabled())
-		{
-			pkg.setManifest(getManifestFile());
-		}
 		
 		if (!destContainer.equals(wizard.getProject()) && destContainer instanceof IContainer) {
 			pkg.setDestinationContainer((IContainer)destContainer);
@@ -366,14 +305,6 @@ public class PackageInfoWizardPage extends WizardPageWithNotification {
 
 	public boolean isPackageExploded() {
 		return packageExploded;
-	}
-
-	public boolean isManifestEnabled() {
-		return manifestEnabled;
-	}
-
-	public IFile getManifestFile() {
-		return manifestFile;
 	}
 
 	public Object getPackageDestination() {
