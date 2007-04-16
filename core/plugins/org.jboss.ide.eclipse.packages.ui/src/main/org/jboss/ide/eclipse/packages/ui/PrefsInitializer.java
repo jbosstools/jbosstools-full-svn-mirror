8c97db9a558876c84981625b077dc7e194d0b0a2
Rob Stryker <rob.stryker@jboss.com>
2007-04-16 17:56:54 +0000

    Complete rewrite of the the two plugins leading to a cleaner, leaner project.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1895 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ core/plugins/org.jboss.ide.eclipse.packages.ui/src/main/org/jboss/ide/eclipse/packages/ui/PrefsInitializer.java
@@ -0,0 +1,25 @@
+package org.jboss.ide.eclipse.packages.ui;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+public class PrefsInitializer extends AbstractPreferenceInitializer {
+
+
+	// preference keys
+	public static final String PREF_SHOW_PACKAGE_OUTPUT_PATH = "showPackageOutputPath";
+	public static final String PREF_SHOW_FULL_FILESET_ROOT_DIR = "showFullFilesetRootDir";
+	public static final String PREF_SHOW_PROJECT_ROOT = "showProjectRoot";
+	public static final String PREF_SHOW_ALL_PROJECTS = "showAllProjects";
+	
+	public void initializeDefaultPreferences() {
+		IEclipsePreferences prefs = new DefaultScope().getNode(PackagesUIPlugin.PLUGIN_ID);
+		
+		prefs.put(PREF_SHOW_FULL_FILESET_ROOT_DIR, "true");
+		prefs.put(PREF_SHOW_PACKAGE_OUTPUT_PATH, "true");
+		prefs.put(PREF_SHOW_PROJECT_ROOT, "true");
+		prefs.put(PREF_SHOW_ALL_PROJECTS, "false");
+	}
+
+}
