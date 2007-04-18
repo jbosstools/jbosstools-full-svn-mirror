e1c953fb32c6e858842518f21cbe0b7b055e4de8
Rob Stryker <rob.stryker@jboss.com>
2007-04-18 21:24:00 +0000

    Given it's own module under the new name
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1904 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ core/plugins/org.jboss.ide.eclipse.archives.ui/src/main/org/jboss/ide/eclipse/archives/ui/wizards/AbstractPackageWizard.java
@@ -0,0 +1,158 @@
+package org.jboss.ide.eclipse.archives.ui.wizards;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.jboss.ide.eclipse.archives.core.Trace;
+import org.jboss.ide.eclipse.archives.core.model.IArchive;
+import org.jboss.ide.eclipse.archives.core.model.IArchiveNode;
+import org.jboss.ide.eclipse.archives.core.model.internal.ArchivesModel;
+import org.jboss.ide.eclipse.archives.ui.views.ProjectArchivesView;
+import org.jboss.ide.eclipse.archives.ui.wizards.pages.PackageInfoWizardPage;
+
+public abstract class AbstractPackageWizard extends WizardWithNotification implements INewWizard {
+	private PackageInfoWizardPage firstPage;
+	private WizardPage pages[];
+	protected IProject project;
+	protected Object selectedDestination;
+	protected IArchive existingPackage;
+	
+	public AbstractPackageWizard () {	
+		this.project = ProjectArchivesView.getInstance().getCurrentProject();
+	}
+	
+	public AbstractPackageWizard (IArchive existingPackage) {
+		this.existingPackage = existingPackage;
+		this.project = existingPackage.getProject();
+	}
+	
+	public void addPages() {
+		firstPage = new PackageInfoWizardPage(this, existingPackage);
+		addPage(firstPage);
+		
+		pages = createWizardPages();
+		for (int i = 0; i < pages.length; i++) {
+			addPage(pages[i]);
+		}
+	}
+	
+	public boolean canFinish() {
+		if (firstPage.isPageComplete()) {
+			for (int i = 0; i < pages.length; i++) {
+				if (!pages[i].isPageComplete()) return false;
+			}
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean performFinish() {
+		IWizardPage currentPage = getContainer().getCurrentPage();
+		
+		if (currentPage instanceof WizardPageWithNotification) {
+			((WizardPageWithNotification)currentPage).pageExited(WizardWithNotification.FINISH);
+		}
+		
+		final boolean create = this.existingPackage == null;
+		final IArchive pkg = firstPage.getPackage();
+		final Object destination = firstPage.getPackageDestination();
+		
+		boolean performed = performFinish(pkg);
+		
+		if (performed) {
+			try {
+				getContainer().run(false, false, new IRunnableWithProgress () {
+					public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+						IArchiveNode parent = null;
+						
+						if (destination instanceof IArchiveNode) {
+							// if we're modifying an existing package, remove old parentage
+							if (!create && !destination.equals(pkg.getParent())) {
+								if (pkg.getParent() != null) {
+									pkg.getParent().removeChild(pkg);
+								}
+							}
+							parent = (IArchiveNode)destination;
+						} else {
+							parent = ArchivesModel.instance().getRoot(project, true, monitor);
+						}
+						
+						if( create ) 
+							ArchivesModel.instance().attach(parent, pkg, monitor);
+						else
+							ArchivesModel.instance().saveModel(project, monitor);
+					}
+				});
+			} catch (InvocationTargetException e) {
+				Trace.trace(getClass(), e);
+			} catch (InterruptedException e) {
+				Trace.trace(getClass(), e);
+			}
+		}
+		return performed;
+	}
+
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		if (selection == null) return;
+		project = ProjectArchivesView.getInstance().getCurrentProject();
+		Object selected;
+		
+		if( selection.isEmpty() ) {
+			selected = project;
+		} else {
+			selected = selection.getFirstElement();
+		}
+
+		if (selected instanceof IArchiveNode)
+		{
+			IArchiveNode node = (IArchiveNode) selected;
+			if (node.getNodeType() == IArchiveNode.TYPE_ARCHIVE || node.getNodeType() == IArchiveNode.TYPE_ARCHIVE_FOLDER)
+			{
+				selectedDestination = selected;
+			}
+			project = node.getProject();
+		}
+		else if (selected instanceof IContainer)
+		{
+			selectedDestination = selected;
+		}
+		else {
+			selectedDestination = project;
+		}
+		
+		setNeedsProgressMonitor(true);
+	}
+	
+	public Object getSelectedDestination ()
+	{
+		return selectedDestination;
+	}
+	
+	public abstract boolean performFinish(IArchive pkg);
+	public abstract WizardPage[] createWizardPages();
+	public abstract ImageDescriptor getImageDescriptor();
+	public abstract String getPackageExtension();
+	
+	public IProject getProject() {
+		return project;
+	}
+	
+	/**
+	 * Returns the package created by this wizard.
+	 * Note: This should only be called after the first page has been completed
+	 * @return The package
+	 */
+	public IArchive getPackage () {
+		return firstPage.getPackage();
+	}
+}
