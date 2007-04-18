3a1b0dfd7b4e87daf3148d63652793e7dd853761
Rob Stryker <rob.stryker@jboss.com>
2007-04-18 21:24:00 +0000

    Given it's own module under the new name
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/core@1904 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.ide.eclipse.archives.ui/src/main/org/jboss/ide/eclipse/archives/ui/PackageNodeFactory.java
@@ -0,0 +1,22 @@
+package org.jboss.ide.eclipse.archives.ui;
+
+import org.jboss.ide.eclipse.archives.core.model.IArchive;
+import org.jboss.ide.eclipse.archives.core.model.IArchiveFileSet;
+import org.jboss.ide.eclipse.archives.core.model.IArchiveFolder;
+import org.jboss.ide.eclipse.archives.core.model.internal.ArchiveFileSetImpl;
+import org.jboss.ide.eclipse.archives.core.model.internal.ArchiveFolderImpl;
+import org.jboss.ide.eclipse.archives.core.model.internal.ArchiveImpl;
+
+public class PackageNodeFactory {
+	public static IArchive createPackage() {
+		return new ArchiveImpl();
+	}
+	
+	public static IArchiveFileSet createFileset() {
+		return new ArchiveFileSetImpl();
+	}
+	
+	public static IArchiveFolder createFolder() {
+		return new ArchiveFolderImpl();
+	}
+}
