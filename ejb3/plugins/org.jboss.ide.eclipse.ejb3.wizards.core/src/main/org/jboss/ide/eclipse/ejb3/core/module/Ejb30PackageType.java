b7f6efe16ddbcb1a1e23a522eaf2ec5b29642af8
Marshall Culpepper <mculpepper@jboss.com>
2007-04-27 16:15:14 +0000

    fixed to work with new archives plugins
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@1956 a97e2381-89e5-4abb-bab3-167db6db766c

--- ejb3/plugins/org.jboss.ide.eclipse.ejb3.wizards.core/src/main/org/jboss/ide/eclipse/ejb3/core/module/Ejb30PackageType.java
+++ ejb3/plugins/org.jboss.ide.eclipse.ejb3.wizards.core/src/main/org/jboss/ide/eclipse/ejb3/core/module/Ejb30PackageType.java
@@ -4,33 +4,37 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jst.j2ee.internal.project.J2EEProjectUtilities;
-import org.eclipse.jst.server.core.IWebModule;
-import org.eclipse.wst.server.core.IModule;
-import org.jboss.ide.eclipse.as.core.packages.ObscurelyNamedPackageTypeSuperclass;
-import org.jboss.ide.eclipse.as.core.packages.WarPackageType;
-import org.jboss.ide.eclipse.packages.core.model.IPackage;
-import org.jboss.ide.eclipse.packages.core.model.IPackageFolder;
-import org.jboss.ide.eclipse.packages.core.model.PackagesCore;
+import org.jboss.ide.eclipse.archives.core.model.IArchive;
+import org.jboss.ide.eclipse.archives.core.model.IArchiveFolder;
+import org.jboss.ide.eclipse.as.core.packages.types.J2EEArchiveType;
 
-public class Ejb30PackageType extends ObscurelyNamedPackageTypeSuperclass {
+public class Ejb30PackageType extends J2EEArchiveType {
 
 	public static final String ID = "org.jboss.ide.eclipse.ejb3.wizards.core.ejbPackageType";
 	public String getAssociatedModuleType() {
 		return "jbide.ejb30";
 	}
 
-	public IPackage createDefaultConfiguration(IProject project, IProgressMonitor monitor) {
+	public String getId() {
+		return ID;
+	}
+	
+	public String getLabel() {
+		return "EJB 3.0 JAR";
+	}
+	
+	public IArchive createDefaultConfiguration(IProject project, IProgressMonitor monitor) {
 		boolean facetFound = J2EEProjectUtilities.isProjectOfType(project, getAssociatedModuleType()); 
 		return createDefaultConfiguration(project, facetFound, monitor);
 	}
 	
-	public IPackage createDefaultConfiguration(IProject project, boolean facetFound, IProgressMonitor monitor) {
+	public IArchive createDefaultConfiguration(IProject project, boolean facetFound, IProgressMonitor monitor) {
 		String metaInfDir = (facetFound ? EJBMODULE + Path.SEPARATOR : "") + METAINF;
 		
-		IPackage topLevel = createGenericIPackage(project, null, project.getName() + ".jar");
-		topLevel.setDestinationContainer(project);
-		IPackageFolder metainf = addFolder(project, topLevel, METAINF);
-		IPackageFolder lib = addFolder(project, metainf, LIB);
+		IArchive topLevel = createGenericIArchive(project, null, project.getName() + ".jar");
+		topLevel.setDestinationPath(project.getLocation());
+		IArchiveFolder metainf = addFolder(project, topLevel, METAINF);
+		IArchiveFolder lib = addFolder(project, metainf, LIB);
 		addFileset(project, metainf, metaInfDir, null);
 		return topLevel;		
 	}
