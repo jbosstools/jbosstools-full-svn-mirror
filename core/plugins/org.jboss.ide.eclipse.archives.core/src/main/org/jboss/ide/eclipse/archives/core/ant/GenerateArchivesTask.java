d8a21bef2beddd305e125141e4b859aa5bcf87e9
Marshall Culpepper <mculpepper@jboss.com>
2007-05-21 23:47:25 +0000

    first stab at a custom ant task (hasn't been tested yet)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2041 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ core/plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/ant/GenerateArchivesTask.java
@@ -0,0 +1,28 @@
+package org.jboss.ide.eclipse.archives.core.ant;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.jboss.ide.eclipse.archives.core.ArchivesCore;
+import org.jboss.ide.eclipse.archives.core.StandaloneArchivesCore;
+import org.jboss.ide.eclipse.archives.core.model.ArchivesModel;
+import org.jboss.ide.eclipse.archives.core.model.ArchivesModelCore;
+
+public class GenerateArchivesTask extends Task {
+
+	private String projectPath;
+	
+	public void execute() throws BuildException {
+		// Force standalone mode
+		ArchivesCore standalone = new StandaloneArchivesCore();
+		
+		IPath projectPath = new Path(this.projectPath);
+		IProgressMonitor monitor = new NullProgressMonitor();
+		
+		ArchivesModel.instance().registerProject(projectPath, monitor);
+		ArchivesModelCore.buildProject(projectPath, monitor);
+	}
+}
