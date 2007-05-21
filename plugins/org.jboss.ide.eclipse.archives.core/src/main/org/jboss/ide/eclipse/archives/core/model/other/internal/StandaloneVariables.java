e745299612eacc936e19ef00f126a1fbb7ec6bdc
Marshall Culpepper <mculpepper@jboss.com>
2007-05-21 23:55:12 +0000

    "standalone" implementations of archives core specific interfaces
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/core@2048 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/model/other/internal/StandaloneVariables.java
+++ plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/model/other/internal/StandaloneVariables.java
@@ -3,22 +3,26 @@ package org.jboss.ide.eclipse.archives.core.model.other.internal;
 import java.net.URL;
 
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.jboss.ide.eclipse.archives.core.model.other.IRuntimeVariables;
 
 public class StandaloneVariables implements IRuntimeVariables {
 
 	public URL getBindingLog4j() {
-		// TODO Auto-generated method stub
-		return null;
+		return getClass().getClassLoader().getResource("log4j.xml");
 	}
 
 	public URL getBindingSchema() {
-		// TODO Auto-generated method stub
-		return null;
+		return getClass().getClassLoader().getResource("packages.xsd");
 	}
 
-	public IPath getWorkspacePath() {
-		// TODO Auto-generated method stub
+	public IPath getProjectPath(String projectName) {
+		projectName = projectName.replace(' ', '_');
+		
+		String projectPath = System.getProperty(projectName + ".dir");
+		if (projectPath != null)
+			return new Path(projectPath);
+		
 		return null;
 	}
 
