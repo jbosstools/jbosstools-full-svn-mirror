a6f70545a2ff9f92184eca0d294ccd0ceb791419
Sergey Vasilyev <sergey.vasilyev@exadel.com>
2007-07-23 15:25:04 +0000

    http://jira.jboss.org/jira/browse/JBIDE-531. javadoc fixed
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@2608 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/template/VpeTemplate.java
+++ vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/template/VpeTemplate.java
@@ -304,17 +304,29 @@ public interface VpeTemplate {
 	boolean isCaseSensitive();
 
 	/**
-	 * Return <code>true</code> if the element has visual preview on tab 'Preview', otherwise <code>false</code>. 
-	 * This value initialized in attribute <b>haveVisualPreview</b> for tag <b>&lt;vpe:template&gt</b>.
-	 * Default value <code>true</code> if this attribute not present. <br/>For example tag <code>&lt;h:form&gt;</code>:<br/>
-	 * In visual editor (see tab 'Visual') have a red border, but it not visible on tab 'Preview'. See part code for this template:<br>
-	 * <code>&lt;vpe:template children="yes" modify="yes" haveVisualPreview="no"&gt;</code><br><br>  
-	 *This element have visible on tab 'Preview:'<br/>
-	 *<code>&lt;vpe:template children="yes" modify="yes" haveVisualPreview="yes"&gt;</code> <br/>
-	 *or<br/>
-	 *<code>&lt;vpe:template children="yes" modify="yes" &gt;</code><br/>
-	 * <br/><br/>This element have not visible on tab 'Preview':<br/>
-	 * <code>&lt;vpe:template children="yes" modify="yes" haveVisualPreview="no"&gt;</code>
+	 * Return <code>true</code> if the element has visual representation on tab
+	 * 'Preview', otherwise <code>false</code>.<br/> 
+	 * This value is initialized by attribute <b>haveVisualPreview</b> of tag
+	 * <b>&lt;vpe:template&gt</b>. If the attribute is not presented default
+	 * value is <code>true</code>.<br/>
+	 * <br/>
+	 * Example:<br/>
+	 * JSF tag <code>&lt;h:form&gt;</code>:<br/>
+	 * <code>
+	 * &lt;vpe:tag name="h:form" case-sensitive="yes"&gt;<br/>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&lt;vpe:template children="yes" modify="yes" haveVisualPreview="no"&gt;<br/>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div style="border:1px dotted #FF6600;padding: 5px;" title="{tagstring()}"/&gt;<br/>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;vpe:dnd&gt;<br/>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;vpe:drag start-enable="yes"/&gt;<br/>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;vpe:drop container="yes"/&gt;<br/>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/vpe:dnd&gt;<br/>
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&lt;/vpe:template&gt;<br/>
+	 * &lt;/vpe:tag&gt;<br/>
+	 * </code><br/>
+	 * In visual editor part (see tab 'Visual') the tag have a red border for
+	 * visualization reason, but it is not visual element and it shouldn't have
+	 * visual representation on tab 'Preview'.<br/>
+	 * <br/>
 	 * @return <code>true</code> if the element is have visual preview
 	 */
 	boolean isHaveVisualPreview();
