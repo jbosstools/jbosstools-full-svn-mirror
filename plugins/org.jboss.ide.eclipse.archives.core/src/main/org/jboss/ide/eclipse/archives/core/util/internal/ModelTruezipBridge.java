a20c99ca8a741b633d618f89de7df55f34e4f408
Rob Stryker <rob.stryker@jboss.com>
2007-07-27 17:15:28 +0000

    small change to ensure its getting proper archive detector if its an archive
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/core@2704 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/util/internal/ModelTruezipBridge.java
+++ plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/util/internal/ModelTruezipBridge.java
@@ -234,7 +234,7 @@ public class ModelTruezipBridge {
 		if( node.getNodeType() == IArchiveNode.TYPE_ARCHIVE ) {
 			IArchive node2 = ((IArchive)node);
 			boolean exploded = ((IArchive)node).isExploded();
-			ArchiveDetector detector = exploded ? ArchiveDetector.NULL : ArchiveDetector.DEFAULT;
+			ArchiveDetector detector = exploded ? ArchiveDetector.NULL : TrueZipUtil.getJarArchiveDetector();
 			if( parentFile == null ) 
 				return new File(node2.getGlobalDestinationPath().append(node2.getName()).toOSString(), detector);
 			return new File(parentFile, node2.getName(), detector);
