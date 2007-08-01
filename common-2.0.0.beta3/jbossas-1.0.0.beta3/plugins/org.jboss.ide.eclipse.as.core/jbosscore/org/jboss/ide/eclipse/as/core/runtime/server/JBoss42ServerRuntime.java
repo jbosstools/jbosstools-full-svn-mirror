1d71cf81aeb2e15fb8621019a1ae57d76b3518c7
Denis Golovin <dgolovin@exadel.com>
2007-08-01 17:01:46 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@2811 a97e2381-89e5-4abb-bab3-167db6db766c

--- common-2.0.0.beta3/jbossas-1.0.0.beta3/plugins/org.jboss.ide.eclipse.as.core/jbosscore/org/jboss/ide/eclipse/as/core/runtime/server/JBoss42ServerRuntime.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.ide.eclipse.as.core.runtime.server;
-
-public class JBoss42ServerRuntime extends AbstractJBossServerRuntime {
-	public JBoss42ServerRuntime() {
-	}
-	public String getId() {
-		return "4.2";
-	}
-}
