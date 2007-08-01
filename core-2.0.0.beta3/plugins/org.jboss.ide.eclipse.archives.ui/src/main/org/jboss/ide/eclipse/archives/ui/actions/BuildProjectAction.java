14fac92ec5a076112eb9ed16a33c23b069fa8f6d
Marshall Culpepper <mculpepper@jboss.com>
2007-08-01 15:28:59 +0000

    2.0.0.beta3 tag for core component
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@2806 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ core-2.0.0.beta3/plugins/org.jboss.ide.eclipse.archives.ui/src/main/org/jboss/ide/eclipse/archives/ui/actions/BuildProjectAction.java
@@ -0,0 +1,40 @@
+package org.jboss.ide.eclipse.archives.ui.actions;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.jboss.ide.eclipse.archives.core.build.ArchiveBuildDelegate;
+
+public class BuildProjectAction implements IWorkbenchWindowActionDelegate {
+	private IProject selectedProject;
+	public void dispose() {
+	}
+
+	public void init(IWorkbenchWindow window) {
+	}
+
+	public void run(IAction action) {
+		if( selectedProject != null ) 
+			new ArchiveBuildDelegate().fullProjectBuild(selectedProject.getLocation());
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		if( !selection.isEmpty() && selection instanceof IStructuredSelection ) {
+			Object o = ((IStructuredSelection)selection).getFirstElement();
+			if( o instanceof IAdaptable ) {
+				IResource res = (IResource)  ((IAdaptable)o).getAdapter(IResource.class);
+				if( res != null ) {
+					selectedProject = res.getProject();				
+					return;
+				}
+			}
+		}
+		selectedProject = null;
+	}
+	
+}
---
+++ core-2.0.0.beta3/plugins/org.jboss.ide.eclipse.archives.ui/src/main/org/jboss/ide/eclipse/archives/ui/providers/ArchivesContentProvider.java
@@ -0,0 +1,86 @@
+package org.jboss.ide.eclipse.archives.ui.providers;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.jboss.ide.eclipse.archives.core.model.ArchivesModel;
+import org.jboss.ide.eclipse.archives.core.model.IArchiveModelNode;
+import org.jboss.ide.eclipse.archives.core.model.IArchiveNode;
+import org.jboss.ide.eclipse.archives.ui.PrefsInitializer;
+import org.jboss.ide.eclipse.archives.ui.views.ProjectArchivesView;
+
+public class ArchivesContentProvider implements ITreeContentProvider {
+	
+	public ArchivesContentProvider () {
+	}
+	
+	public static class WrappedProject  {
+		private IProject project;
+		public WrappedProject (IProject proj) { project = proj; }
+		public IProject getProject() { return project; }
+	}
+
+	private boolean showProjectRoot () {
+		return PrefsInitializer.getBoolean(PrefsInitializer.PREF_SHOW_PROJECT_ROOT);
+	}
+		
+	private Object[] wrapProjects(IProject[] project) {
+		Object[] ret = new Object[project.length];
+		for( int i = 0; i < project.length; i++ )
+			ret[i] = new WrappedProject(project[i]);
+		return ret;
+	}
+	public Object[] getChildren(Object parentElement) {
+		
+		if( parentElement instanceof Object[] ) return (Object[])parentElement;
+		
+		
+		if( parentElement instanceof IArchiveModelNode && showProjectRoot())  {
+			IProject[] projects;
+			if( PrefsInitializer.getBoolean(PrefsInitializer.PREF_SHOW_ALL_PROJECTS)) {
+				projects = ProjectArchivesView.getInstance().getAllProjectsWithPackages();
+			} else {
+				String projName = ((IArchiveModelNode)parentElement).getProjectPath().lastSegment();
+				projects = new IProject[] { ResourcesPlugin.getWorkspace().getRoot().getProject(projName) };
+			}
+			return wrapProjects(projects);
+		}
+		
+		if( parentElement instanceof IArchiveModelNode)
+			return ((IArchiveModelNode)parentElement).getAllChildren();
+		
+		if( parentElement instanceof ArchivesModel ) {
+			// return all that's there
+		}
+		if( parentElement instanceof WrappedProject ) {
+			return ArchivesModel.instance().getProjectArchives(((WrappedProject)parentElement).getProject().getLocation());
+		}
+		if( parentElement instanceof IArchiveNode ) {
+			return ((IArchiveNode)parentElement).getAllChildren();
+		}
+		return new Object[] {};
+	}
+
+	public Object getParent(Object element) {
+		if( element instanceof IArchiveNode ) 
+			return ((IArchiveNode)element).getParent();
+		if( element instanceof WrappedProject ) 
+			return ArchivesModel.instance().getRoot(((WrappedProject)element).getProject().getLocation());
+		return null;
+	}
+
+	public boolean hasChildren(Object element) {
+		return getChildren(element).length > 0;
+	}
+
+	public Object[] getElements(Object inputElement) {
+		return getChildren(inputElement);
+	}
+
+	public void dispose() {}
+
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+	}
+
+}
