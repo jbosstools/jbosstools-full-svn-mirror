fdb7104a011771ff91e136758637b3b9e0fc5134
Marshall Culpepper <mculpepper@jboss.com>
2007-08-10 08:23:20 +0000

    cleaning out / redoing tags yet again
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/seam-2.0.0.beta3@3052 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ seam/plugins/org.jboss.tools.seam.ui/src/org/jboss/tools/seam/ui/wizard/IParameter.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/ 
+
+package org.jboss.tools.seam.ui.wizard;
+
+/**
+ * @author eskimo
+ *
+ */
+public interface IParameter {
+	
+	public static String SEAM_PROJECT_NAME = "seam.project.name";
+	public static String SEAM_COMPONENT_NAME = "component.name";
+	public static String SEAM_LOCAL_INTERFACE_NAME = "interface.name";
+	public static String SEAM_BEAN_NAME = "bean.name";
+	public static String SEAM_METHOD_NAME = "method.name";
+	public static String SEAM_PAGE_NAME = "page.name";
+	public static String SEAM_MASTER_PAGE_NAME = "masterPage.name";
+	public static String SEAM_ENTITY_CLASS_NAME = "entity.name";
+	public static String SEAM_PROJECT_LOCATION_PATH = "seam.project.location";
+	public static String SEAM_PROJECT_WEBCONTENT_PATH = "seam.project.webcontent";
+	
+	public static String SEAM_EAR_PROJECT_LOCATION_PATH = "seam.ear.project.location";
+
+}
