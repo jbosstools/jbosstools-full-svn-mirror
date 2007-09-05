9a8d4c2b3856e18bbdd69fe353fb80c2da6689ad
Maxim Areshkau <mareshkau@exadel.com>
2007-09-05 12:47:03 +0000

    http://jira.jboss.com/jira/browse/JBIDE-744, scrolling for source selection event was added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches@3453 a97e2381-89e5-4abb-bab3-167db6db766c

--- jbosstools_xulrunner/vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/mozilla/MozillaBaseEventListener.java
+++ jbosstools_xulrunner/vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/mozilla/MozillaBaseEventListener.java
@@ -29,15 +29,6 @@ public class MozillaBaseEventListener implements nsIClipboardDragDropHooks {
 		// TODO Max Areshkau delete this reference afrer DnD had been added
 //		createCOMInterfaces();
 	}
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	VpeDnD getLocalDnD() {
-//		return dnd;
-//	}
-
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	XPCOMObject getDropListener() {
-//		return dropListener;
-//	}
 
 	void createCOMInterfaces() {
 		// TODO Max Areshkau delete this reference afrer DnD had been added
@@ -56,110 +47,6 @@ public class MozillaBaseEventListener implements nsIClipboardDragDropHooks {
 		// TODO Max Areshkau delete this reference afrer DnD had been added
 //		dnd = getDnD();
 	}
-
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	void disposeCOMInterfaces() {
-//		if (dropListener != null) {
-//			dropListener.dispose();
-//			dropListener = null;
-//		}
-//	}
-
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-	// nsISupports implementation
-//	int QueryInterface(int riid, int ppvObject) {
-//		if (riid == 0 || ppvObject == 0) {
-//			return XPCOM.NS_ERROR_NO_INTERFACE;
-//		}
-//		nsID guid = new nsID();
-//		XPCOM.memmove(guid, riid, nsID.sizeof);
-//		
-//		if (MozillaDebug.printVisualNoInterface) {
-//			System.out.println("DomEventListener - NO INTERFACE: " + guid.ToString());
-//		}
-//		XPCOM.memmove(ppvObject, new int[] {0}, 4);
-//		return XPCOM.NS_ERROR_NO_INTERFACE;
-//	}
-
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	int AddRef() {
-//		refCount++;
-//		return refCount;
-//	}
-	        	
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	int Release() {
-//		refCount--;
-//		if (refCount == 0) {
-//			disposeCOMInterfaces();
-//		}
-//		return refCount;
-//	}
-	
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	VpeDnD getDnD() {
-//		int[] result = new int[] {0};
-//		int rc = XPCOM.NS_GetComponentManager(result);
-//		if (rc != XPCOM.NS_OK) {
-//			VpePlugin.getPluginLog().logError("(DND) GetComponentManager error " + rc);
-//		}
-//		if (result[0] == 0) {
-//			VpePlugin.getPluginLog().logError("(DND) GetComponentManager error NO_INTERFACE");
-//		}
-//		
-//		nsIComponentManager componentManager = new nsIComponentManager(result[0]);
-//		result[0] = 0;
-//		rc = componentManager.CreateInstance(XPCOM.VPE_DND_CID, 0, VpeDnD.VPE_DND_IID, result);
-//		if (rc != XPCOM.NS_OK) {
-//			VpePlugin.getPluginLog().logError("(DND) Create instance error " + rc);
-//		}
-//		if (result[0] == 0) {
-//			VpePlugin.getPluginLog().logError("(DND) Create instance error NO_INTERFACE");
-//		}
-//		componentManager.Release();
-//
-//		return new VpeDnD(result[0]);
-//	}
-	
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-	// IDropListener
-//	int CanDrag(int aDragEvent, int _retval, int aNode, int offset) {
-//		XPCOM.memmove(_retval, new int[] {0}, 4);
-//		XPCOM.memmove(aNode, new int[] {0}, 4);
-//		XPCOM.memmove(offset, new int[] {0}, 4);
-//		return XPCOM.NS_OK;
-//	}
-
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	int CanDrop(int aDropEvent, int _retval, int aNode, int offset) {
-//		XPCOM.memmove(_retval, new int[] {0}, 4);
-//		XPCOM.memmove(aNode, new int[] {0}, 4);
-//		XPCOM.memmove(offset, new int[] {0}, 4);
-//		return XPCOM.NS_OK;
-//	}
-	
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	int Drop(int aDropEvent, int aNode, int offset) {
-//		XPCOM.memmove(aNode, new int[] {0}, 4);
-//		XPCOM.memmove(offset, new int[] {0}, 4);
-//		return XPCOM.NS_OK;
-//	}
-	
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	int CanDropExternal(int aDropEvent, int aFlavor, int aTransData, int _retval, int aNode, int offset) {
-//		XPCOM.memmove(_retval, new int[] {0}, 4);
-//		XPCOM.memmove(aNode, new int[] {0}, 4);
-//		XPCOM.memmove(offset, new int[] {0}, 4);
-//		return XPCOM.NS_OK;
-//	}
-	
-	// TODO Max Areshkau delete this reference afrer DnD had been added
-//	int DropExternal(int aDropEvent, int aFlavor, int aTransData, int aNode, int offset) {
-//		XPCOM.memmove(aNode, new int[] {0}, 4);
-//		XPCOM.memmove(offset, new int[] {0}, 4);
-////		return XPCOM.NS_OK;
-//		return XPCOM.NS_ERROR_ABORT;
-//	}
 	
 	/* (non-Javadoc)
 	 * @see org.mozilla.interfaces.nsIClipboardDragDropHooks#allowDrop(org.mozilla.interfaces.nsIDOMEvent, org.mozilla.interfaces.nsIDragSession)
