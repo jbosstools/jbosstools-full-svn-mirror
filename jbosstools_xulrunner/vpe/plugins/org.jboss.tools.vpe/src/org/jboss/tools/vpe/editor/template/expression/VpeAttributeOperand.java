df9176f8277ced154ef4feb9d2fbce6bda0e201e
Evgeny Zheleznyakov <ezheleznyakov@exadel.com>
2007-09-27 13:41:53 +0000

    Add dump source feature, and little fix with html tag <img/>.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches@3862 a97e2381-89e5-4abb-bab3-167db6db766c

--- jbosstools_xulrunner/vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/template/expression/VpeAttributeOperand.java
+++ jbosstools_xulrunner/vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/template/expression/VpeAttributeOperand.java
@@ -17,11 +17,9 @@ import org.w3c.dom.Node;
 
 public class VpeAttributeOperand extends VpeOperand {
 	private String name;
-	private boolean caseSensitive;
 
 	VpeAttributeOperand(String name, boolean caseSensitive) {
 		this.name = name;
-		this.caseSensitive = caseSensitive;
 	}
 	
 	int getPriority() {
@@ -33,8 +31,8 @@ public class VpeAttributeOperand extends VpeOperand {
 
 		try {
 			value = ((Element)sourceNode).getAttribute(name);
-			if (!caseSensitive) {
-				value = (value == null ? "" : value.toLowerCase());
+			if (value == null) {
+				value = "";
 			}
 		} catch (Exception e) {
 //			throw new VpeExpressionError(x.getMessage());
