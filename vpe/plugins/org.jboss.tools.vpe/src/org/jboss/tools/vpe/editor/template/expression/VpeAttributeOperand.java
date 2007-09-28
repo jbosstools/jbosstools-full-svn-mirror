559635a91744aa1191c54388e355de2edd9932f7
Evgeny Zheleznyakov <ezheleznyakov@exadel.com>
2007-09-28 12:13:10 +0000

    Little fix with html tag <img/>.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@3901 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/template/expression/VpeAttributeOperand.java
+++ vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/template/expression/VpeAttributeOperand.java
@@ -33,8 +33,8 @@ public class VpeAttributeOperand extends VpeOperand {
 
 		try {
 			value = ((Element)sourceNode).getAttribute(name);
-			if (!caseSensitive) {
-				value = (value == null ? "" : value.toLowerCase());
+			if (value == null) {
+				value = "";
 			}
 		} catch (Exception e) {
 //			throw new VpeExpressionError(x.getMessage());
