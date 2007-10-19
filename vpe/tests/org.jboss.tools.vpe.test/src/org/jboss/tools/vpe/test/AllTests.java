935d13c19087269aee7854814ebb5f06395d1e81
Sergey Vasilyev <sergey.vasilyev@exadel.com>
2007-10-19 10:14:24 +0000

    Tests were fixed to be run on XULRunner
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@4371 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/tests/org.jboss.tools.vpe.test/src/org/jboss/tools/vpe/test/AllTests.java
+++ vpe/tests/org.jboss.tools.vpe.test/src/org/jboss/tools/vpe/test/AllTests.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2007 Red Hat, Inc.
+ * Copyright (c) 2007 Exadel, Inc. and Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Red Hat, Inc. - initial API and implementation
- ******************************************************************************/
-package org.jboss.tools.vpe.tests;
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/ 
+package org.jboss.tools.vpe.test;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -21,7 +21,7 @@ import junit.framework.TestSuite;
  * 
  */
 
-public class VpeAllTests extends TestCase{
+public class AllTests extends TestCase{
 	
 	public static Test suite(){
 		TestSuite suite = new TestSuite("Tests for vpe");
