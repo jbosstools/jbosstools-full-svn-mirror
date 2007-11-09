e033da786a70310941219b041814aed1d0309dc1
Marshall Culpepper <mculpepper@jboss.com>
2007-11-09 06:02:38 +0000

    tagging for jbosstools-2.0.0.CR1
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@4805 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jbosstools-2.0.0.CR1/trunk/netbeans/hibernate-console/src/org/hibernate/netbeans/console/ConfigureSessionFactoryAction.java
@@ -0,0 +1,68 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.hibernate.netbeans.console;
+
+import org.openide.ErrorManager;
+import org.openide.nodes.Node;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CookieAction;
+
+/**
+ * @author leon
+ */
+public class ConfigureSessionFactoryAction extends CookieAction {
+    
+    public ConfigureSessionFactoryAction() {
+    }
+    
+    protected int mode() {
+        return MODE_EXACTLY_ONE;
+    }
+    
+    public Class[] cookieClasses() {
+        return new Class[] { SessionFactoryCookie.class };
+    }
+    
+    public void performAction(Node[] activatedNodes) {
+        if (activatedNodes.length != 1) {
+            return;
+        }
+        Node n = activatedNodes[0];
+        SessionFactoryCookie c = (SessionFactoryCookie) n.getCookie(SessionFactoryCookie.class);
+        c.configure();
+    }
+    
+    public String getName() {
+        return NbBundle.getMessage(ConfigureSessionFactoryAction.class, "LBL_Configure");
+    }
+    
+    public HelpCtx getHelpCtx() {
+        return null;
+    }
+
+    public boolean asynchronous() {
+        return true;
+    }
+    
+}
---
+++ jbosstools-2.0.0.CR1/trunk/vpe/plugins/org.jboss.tools.vpe.xulrunner/src/org/jboss/tools/vpe/xulrunner/editor/XulRunnerConstants.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.vpe.xulrunner.editor;
+
+/**
+ * @author A. Yukhovich
+ *
+ */
+public class XulRunnerConstants {
+	/** HTML tags */
+	
+	/** HTML_TAG_SPAN */
+	public static final String HTML_TAG_SPAN = "span";
+
+	
+	/** HTML attributes */
+	
+	/** HTML_ATTR_CLASS */
+	public static final String HTML_ATTR_CLASS = "class";
+
+	/** HTML_ATTR_ANONLOCATION */
+	public static final String HTML_ATTR_ANONLOCATION = "anonlocation";
+
+
+	/** HTML_ATTR_LEFT */
+	public static final String HTML_ATTR_LEFT = "left";
+
+	/** HTML_ATTR_TOP */
+	public static final String HTML_ATTR_TOP = "top";
+	
+	/** HTML_ATTR_WIDTH */
+	public static final String HTML_ATTR_WIDTH = "width";
+	
+	/** HTML_ATTR_HEIGHT */
+	public static final String HTML_ATTR_HEIGHT = "height";
+
+	/** HTML values */
+	
+	/** HTML_VALUE_HIDDEN */
+	public static final String HTML_VALUE_HIDDEN = "hidden";
+
+	/** HTML_VALUE_TRUE */
+	public static final String HTML_VALUE_TRUE = "true";
+
+	/** HTML_VALUE_FALSE */
+	public static final String HTML_VALUE_FALSE = "false";
+	
+	/** EVENT NAMES */
+	
+	/** EVENT_NAME_MOUSEDOWN */
+	public static final String EVENT_NAME_MOUSEDOWN = "mousedown";
+	
+	/** EVENT_NAME_MOUSEUP */
+	public static final String EVENT_NAME_MOUSEUP = "mouseup";
+	
+	/** EVENT_NAME_MOUSEMOVE */
+	public static final String EVENT_NAME_MOUSEMOVE = "mousemove";
+
+	/** */
+	
+	/** STRING_MOZ_ACTIVATED */
+	public static final String STRING_MOZ_ACTIVATED = "_moz_activated";
+
+	/** STRING_MOZ_ANONCLASS */
+	public static final String STRING_MOZ_ANONCLASS = "_moz_anonclass";
+	
+	/** VPE_CLASSNAME_MOZ_RESIZER */
+	public static final String VPE_CLASSNAME_MOZ_RESIZER = "mozResizer";
+	
+	/** VPE_CLASS_NAME_MOZ_RESIZING_SHADOW */
+	public static final String VPE_CLASS_NAME_MOZ_RESIZING_SHADOW = "mozResizingShadow";
+
+
+
+}
