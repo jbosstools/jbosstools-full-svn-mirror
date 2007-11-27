cdd2be90f4134aa6e0b7be6d43864117703fdc6b
Marshall Culpepper <mculpepper@jboss.com>
2007-11-27 20:29:12 +0000

    tagging for 2.0.0.GA
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@5128 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jbosstools-2.0.0.GA/core/plugins/org.jboss.ide.eclipse.archives.ui/src/main/org/jboss/ide/eclipse/archives/ui/actions/NewJARAction.java
@@ -0,0 +1,42 @@
+package org.jboss.ide.eclipse.archives.ui.actions;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.PlatformUI;
+import org.jboss.ide.eclipse.archives.ui.ArchivesSharedImages;
+import org.jboss.ide.eclipse.archives.ui.views.ProjectArchivesView;
+import org.jboss.ide.eclipse.archives.ui.wizards.NewJARWizard;
+
+public class NewJARAction extends ActionWithDelegate {
+	public void run() {
+		try {
+		NewJARWizard wizard = new NewJARWizard();
+		
+		wizard.init(PlatformUI.getWorkbench(), getSelection());
+		
+		WizardDialog dialog = new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), wizard);
+		int response = dialog.open();
+		if (response == Dialog.OK) {
+		}
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
+	}
+	
+	public IStructuredSelection getSelection() {
+		return ProjectArchivesView.getInstance().getSelection();
+	}
+	public ImageDescriptor getImageDescriptor() {
+		return ArchivesSharedImages.getImageDescriptor(ArchivesSharedImages.IMG_NEW_PACKAGE);
+	}
+	
+	public String getText() {
+		return "JAR";
+	}
+	
+	public String getToolTipText() {
+		return "Create a new JAR package";
+	}
+}
