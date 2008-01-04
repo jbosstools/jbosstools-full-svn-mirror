b00670d8541dbc9922a66122d6cf3217ecbbe972
Sergey Dzmitrovich <sdzmitrovich@gmail.com>
2008-01-04 11:01:05 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1501
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@5504 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/util/HTML.java
+++ vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/util/HTML.java
@@ -19,43 +19,45 @@ public final class HTML {
 
 	private HTML() {}
 
-	public static final String TAG_HTML		= "HTML";
-	public static final String TAG_HEAD 	= "HEAD";
-	public static final String TAG_BODY 	= "BODY";
-	public static final String TAG_IMG      = "IMG";
-	public static final String TAG_LINK     = "LINK";
-	public static final String TAG_SELECT	= "SELECT";
-	public static final String TAG_OPTION	= "OPTION";
-	public static final String TAG_STYLE    = "STYLE";
-	public static final String TAG_TABLE    = "TABLE";
-	public static final String TAG_TBODY    = "TBODY";
-	public static final String TAG_THEAD    = "THEAD";
-	public static final String TAG_TFOOT	= "TFOOT";
-	public static final String TAG_TH		= "TH"; 
-	public static final String TAG_TR       = "TR";
-	public static final String TAG_TD       = "TD";
-	public static final String TAG_COL      = "COL";
-	public static final String TAG_COLS     = "COLS";
-	public static final String TAG_COLGROUP = "COLGROUP";
-	public static final String TAG_BR       = "BR";
-	public static final String TAG_LI       = "LI";
-	public static final String TAG_DIV      = "DIV";
-	public static final String TAG_SPAN     = "SPAN";
-	public static final String TAG_P        = "P";
-	public static final String TAG_TEXTAREA	= "TEXTAREA";
-	public static final String TAG_INPUT	= "INPUT";
-	public static final String TAG_BUTTON	= "BUTTON";
-	public static final String TAG_OL		= "OL";
-	public static final String TAG_UL		= "UL";
+	public static final String TAG_HTML		 = "HTML";
+	public static final String TAG_HEAD 	 = "HEAD";
+	public static final String TAG_BODY 	 = "BODY";
+	public static final String TAG_IMG       = "IMG";
+	public static final String TAG_LINK      = "LINK";
+	public static final String TAG_SELECT	 = "SELECT";
+	public static final String TAG_OPTION	 = "OPTION";
+	public static final String TAG_STYLE     = "STYLE";
+	public static final String TAG_TABLE     = "TABLE";
+	public static final String TAG_TBODY     = "TBODY";
+	public static final String TAG_THEAD     = "THEAD";
+	public static final String TAG_TFOOT	 = "TFOOT";
+	public static final String TAG_TH		 = "TH"; 
+	public static final String TAG_TR        = "TR";
+	public static final String TAG_TD        = "TD";
+	public static final String TAG_COL       = "COL";
+	public static final String TAG_COLS      = "COLS";
+	public static final String TAG_COLGROUP  = "COLGROUP";
+	public static final String TAG_BR        = "BR";
+	public static final String TAG_LI        = "LI";
+	public static final String TAG_DIV       = "DIV";
+	public static final String TAG_SPAN      = "SPAN";
+	public static final String TAG_P         = "P";
+	public static final String TAG_TEXTAREA	 = "TEXTAREA";
+	public static final String TAG_INPUT	 = "INPUT";
+	public static final String TAG_BUTTON	 = "BUTTON";
+	public static final String TAG_OL		 = "OL";
+	public static final String TAG_UL		 = "UL";
 	
 	
 	
-	public static final String ATTR_ID 		= "ID";
-	public static final String ATTR_TYPE	="TYPE";
-	public static final String ATTR_TEXT	="TEXT";
-	public static final String ATTR_CLASS	="CLASS";
-	public static final String ATTR_TITLE	="TITLE";
-	public static final String ATTR_NAME	="NAME";
-	public static final String ATTR_VALUE	="VALUE";
-	public static final String ATTR_STYLE	="STYLE";
+	public static final String ATTR_ID 		 = "ID";
+	public static final String ATTR_TYPE	 ="TYPE";
+	public static final String ATTR_TEXT	 ="TEXT";
+	public static final String ATTR_CLASS	 ="CLASS";
+	public static final String ATTR_TITLE	 ="TITLE";
+	public static final String ATTR_NAME	 ="NAME";
+	public static final String ATTR_VALUE	 ="VALUE";
+	public static final String ATTR_STYLE	 ="STYLE";
+	public static final String ATTR_SIZE	 ="SIZE";
+	public static final String ATTR_MULTIPLE = "MULTIPLE";
 }
