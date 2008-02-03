090acdc8c324d4af8b821f7df56cf17bba6bb9eb
Sergey Vasilyev <sergey.vasilyev@exadel.com>
2008-02-03 15:51:12 +0000

    Code adjustment
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6101 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe.xulrunner/src/org/jboss/tools/vpe/xulrunner/XPCOM.java
+++ vpe/plugins/org.jboss.tools.vpe.xulrunner/src/org/jboss/tools/vpe/xulrunner/XPCOM.java
@@ -16,16 +16,19 @@ package org.jboss.tools.vpe.xulrunner;
  *
  */
 public final class XPCOM {
-	private XPCOM() {};
+	private XPCOM() {}
 	/*
 	 * Contract IDs
 	 */
-	public static final String NS_DRAGSERVICE_CONTRACTID = "@mozilla.org/widget/dragservice;1;"; //$NON-NLS-1$
+	public static final String NS_DRAGSERVICE_CONTRACTID = "@mozilla.org/widget/dragservice;1"; //$NON-NLS-1$
 	public static final String NS_TRANSFERABLE_CONTRACTID = "@mozilla.org/widget/transferable;1"; //$NON-NLS-1$
 	public static final String NS_WINDOWWATCHER_CONTRACTID = "@mozilla.org/embedcomp/window-watcher;1"; //$NON-NLS-1$
 	public static final String NS_PREFSERVICE_CONTRACTID = "@mozilla.org/preferences-service;1"; //$NON-NLS-1$
-	public static final String IN_FLASHER_CONTRACTID = "@mozilla.org/inspector/flasher;1";
+	public static final String NS_SUPPORTSSTRING_CONTRACTID = "@mozilla.org/supports-string;1"; //$NON-NLS-1$
+	public static final String NS_SUPPORTSARRAY_CONTRACTID = "@mozilla.org/supports-array;1"; //$NON-NLS-1$
 	
-	public static final String NS_IWEBBROWSER_CID = "F1EAC761-87E9-11d3-AF80-00A024FFC08C"; // $NON-NLS-1$
-	public static final String NS_IAPPSHELL_CID = "2d96b3df-c051-11d1-a827-0040959a28c9"; // $NON-NLS-1$
+	public static final String IN_FLASHER_CONTRACTID = "@mozilla.org/inspector/flasher;1"; //$NON-NLS-1$
+	
+	public static final String NS_IWEBBROWSER_CID = "F1EAC761-87E9-11d3-AF80-00A024FFC08C"; //$NON-NLS-1$
+	public static final String NS_IAPPSHELL_CID = "2d96b3df-c051-11d1-a827-0040959a28c9"; //$NON-NLS-1$
 }
