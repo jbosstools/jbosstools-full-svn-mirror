994107567d31386a012c3a3d1fc87a043a9b0475
Rob Stryker <rob.stryker@jboss.com>
2008-02-22 22:21:17 +0000

    using new logger api (removed catch(Exception e) {})
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6546 a97e2381-89e5-4abb-bab3-167db6db766c

--- core/plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/build/ModelChangeListener.java
+++ core/plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/build/ModelChangeListener.java
@@ -4,7 +4,7 @@
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
-* This is free software; you can redistribute it and/or modify it
+ * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
