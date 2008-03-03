a887564709b6e9994e07f1681d2cf9103c2cc935
Dzmitry Sakovich <dsakovich@exadel.com>
2008-03-03 13:42:48 +0000

    Add JUnit test for http://jira.jboss.com/jira/browse/JBIDE-1713
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6669 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/TestUtil.java
+++ vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/TestUtil.java
@@ -28,6 +28,7 @@ import org.eclipse.ui.wizards.datatransfer.ImportOperation;
 import org.jboss.tools.vpe.ui.test.beans.ImportBean;
 import org.mozilla.interfaces.nsIDOMNode;
 import org.mozilla.interfaces.nsIDOMNodeList;
+import org.mozilla.xpcom.XPCOMException;
 
 /**
  * Class for importing project from jar file
@@ -37,176 +38,203 @@ import org.mozilla.interfaces.nsIDOMNodeList;
  */
 public class TestUtil {
 
-    private static final String COMPONENTS_PATH = "WebContent/pages"; //$NON-NLS-1$
+	private static final String COMPONENTS_PATH = "WebContent/pages"; //$NON-NLS-1$
 
-    private static final String WEBCONTENT_PATH = "WebContent"; //$NON-NLS-1$
+	private static final String WEBCONTENT_PATH = "WebContent"; //$NON-NLS-1$
 
-    @SuppressWarnings("restriction")
-    static void importProjectIntoWorkspace(String path, String projectName) {
+	@SuppressWarnings("restriction")
+	static void importProjectIntoWorkspace(String path, String projectName) {
 
-	IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
-		projectName);
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
+				projectName);
 
-	try {
+		try {
 
-	    IOverwriteQuery overwrite = new IOverwriteQuery() {
-		public String queryOverwrite(String pathString) {
-		    return ALL;
+			IOverwriteQuery overwrite = new IOverwriteQuery() {
+				public String queryOverwrite(String pathString) {
+					return ALL;
+				}
+			};
+
+			ImportProvider importProvider = new ImportProvider();
+
+			// need to remove from imported project "svn" files
+			List<String> unimportedFiles = new ArrayList<String>();
+			unimportedFiles.add(".svn"); //$NON-NLS-1$
+
+			importProvider.setUnimportedFiles(unimportedFiles);
+
+			// create import operation
+			ImportOperation importOp = new ImportOperation(project
+					.getFullPath(), new File(path), importProvider, overwrite);
+
+			// import files just to project folder ( without old structure )
+			importOp.setCreateContainerStructure(false);
+
+			importOp.setContext(PlatformUI.getWorkbench()
+					.getActiveWorkbenchWindow().getShell());
+
+			// run import
+			importOp.run(new NullProgressMonitor());
+
+		} catch (InvocationTargetException ite) {
+			VPETestPlugin.getDefault().logError(ite.getCause());
+		} catch (InterruptedException ie) {
+			VPETestPlugin.getDefault().logError(ie);
 		}
-	    };
-
-	    ImportProvider importProvider = new ImportProvider();
-
-	    // need to remove from imported project "svn" files
-	    List<String> unimportedFiles = new ArrayList<String>();
-	    unimportedFiles.add(".svn"); //$NON-NLS-1$
-
-	    importProvider.setUnimportedFiles(unimportedFiles);
-
-	    // create import operation
-	    ImportOperation importOp = new ImportOperation(project
-		    .getFullPath(), new File(path), importProvider, overwrite);
-
-	    // import files just to project folder ( without old structure )
-	    importOp.setCreateContainerStructure(false);
-
-	    importOp.setContext(PlatformUI.getWorkbench()
-		    .getActiveWorkbenchWindow().getShell());
-
-	    // run import
-	    importOp.run(new NullProgressMonitor());
-
-	} catch (InvocationTargetException ite) {
-	    VPETestPlugin.getDefault().logError(ite.getCause());
-	} catch (InterruptedException ie) {
-	    VPETestPlugin.getDefault().logError(ie);
-	}
-    }
-
-    /**
-     * 
-     * @return
-     * @throws CoreException
-     */
-    public static IResource getComponentPath(String componentPage,
-	    String projectName) throws CoreException {
-	IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
-		projectName);
-	if (project != null) {
-	    return project.getFolder(COMPONENTS_PATH).findMember(componentPage);
-
 	}
 
-	return null;
-    }
+	/**
+	 * 
+	 * @return
+	 * @throws CoreException
+	 */
+	public static IResource getComponentPath(String componentPage,
+			String projectName) throws CoreException {
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
+				projectName);
+		if (project != null) {
+			return project.getFolder(COMPONENTS_PATH).findMember(componentPage);
 
-    /**
-     * 
-     * @return
-     * @throws CoreException
-     */
-    public static IResource getWebContentPath(String componentPage,
-	    String projectName) throws CoreException {
-	IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
-		projectName);
-	if (project != null) {
-	   return project.getFolder(WEBCONTENT_PATH).findMember(componentPage);
+		}
+
+		return null;
 	}
 
-	return null;
-    }
+	/**
+	 * 
+	 * @return
+	 * @throws CoreException
+	 */
+	public static IResource getWebContentPath(String componentPage,
+			String projectName) throws CoreException {
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
+				projectName);
+		if (project != null) {
+			return project.getFolder(WEBCONTENT_PATH).findMember(componentPage);
+		}
 
-    /**
-     * 
-     * @throws CoreException
-     */
-    static void removeProject(String projectName) throws CoreException {
-	IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
-		projectName);
-	if (project != null) {
-	    project.delete(IResource.ALWAYS_DELETE_PROJECT_CONTENT,
-		    new NullProgressMonitor());
-	}
-    }
-
-    /**
-     * Process UI input but do not return for the specified time interval.
-     * 
-     * @param waitTimeMillis
-     *                the number of milliseconds
-     */
-    public static void delay(long waitTimeMillis) {
-	Display display = Display.getCurrent();
-	if (display != null) {
-	    long endTimeMillis = System.currentTimeMillis() + waitTimeMillis;
-	    while (System.currentTimeMillis() < endTimeMillis) {
-		if (!display.readAndDispatch())
-		    display.sleep();
-	    }
-	    display.update();
-	}
-	// Otherwise, perform a simple sleep.
-	else {
-	    try {
-		Thread.sleep(waitTimeMillis);
-	    } catch (InterruptedException e) {
-		// Ignored.
-	    }
-	}
-    }
-
-    /**
-     * Wait until all background tasks are complete.
-     */
-    public static void waitForJobs() {
-	while (Job.getJobManager().currentJob() != null)
-	    delay(100);
-    }
-
-    /**
-     * find elements by name
-     * 
-     * @param node -
-     *                current node
-     * @param elements -
-     *                list of found elements
-     * @param name -
-     *                name element
-     */
-    static public void findElementsByName(nsIDOMNode node,
-	    List<nsIDOMNode> elements, String name) {
-
-	// get children
-	nsIDOMNodeList children = node.getChildNodes();
-	for (int i = 0; i < children.getLength(); i++) {
-	    nsIDOMNode child = children.item(i);
-
-	    // if current child is required then add his to list
-	    if (name.equalsIgnoreCase((child.getNodeName()))) {
-
-		elements.add(child);
-
-	    } else {
-
-		findElementsByName(child, elements, name);
-
-	    }
+		return null;
 	}
 
-    }
+	/**
+	 * 
+	 * @throws CoreException
+	 */
+	static void removeProject(String projectName) throws CoreException {
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
+				projectName);
+		if (project != null) {
+			project.delete(IResource.ALWAYS_DELETE_PROJECT_CONTENT,
+					new NullProgressMonitor());
+		}
+	}
 
-    static public List<ImportBean> createImportBeanList(String projectName,
-	    String resourcePath) {
-	List<ImportBean> projectToImport = new ArrayList<ImportBean>();
-	projectToImport.add(createImportBean(projectName, resourcePath));
-	return projectToImport;
-    }
+	/**
+	 * Process UI input but do not return for the specified time interval.
+	 * 
+	 * @param waitTimeMillis
+	 *            the number of milliseconds
+	 */
+	public static void delay(long waitTimeMillis) {
+		Display display = Display.getCurrent();
+		if (display != null) {
+			long endTimeMillis = System.currentTimeMillis() + waitTimeMillis;
+			while (System.currentTimeMillis() < endTimeMillis) {
+				if (!display.readAndDispatch())
+					display.sleep();
+			}
+			display.update();
+		}
+		// Otherwise, perform a simple sleep.
+		else {
+			try {
+				Thread.sleep(waitTimeMillis);
+			} catch (InterruptedException e) {
+				// Ignored.
+			}
+		}
+	}
 
-    static public ImportBean createImportBean(String projectName,
-	    String resourcePath) {
-	ImportBean importBean = new ImportBean();
-	importBean.setImportProjectName(projectName);
-	importBean.setImportProjectPath(resourcePath);
-	return importBean;
-    }
+	/**
+	 * Wait until all background tasks are complete.
+	 */
+	public static void waitForJobs() {
+		while (Job.getJobManager().currentJob() != null)
+			delay(100);
+	}
+
+	/**
+	 * find elements by name
+	 * 
+	 * @param node -
+	 *            current node
+	 * @param elements -
+	 *            list of found elements
+	 * @param name -
+	 *            name element
+	 */
+	static public void findElementsByName(nsIDOMNode node,
+			List<nsIDOMNode> elements, String name) {
+
+		// get children
+		nsIDOMNodeList children = node.getChildNodes();
+		for (int i = 0; i < children.getLength(); i++) {
+			nsIDOMNode child = children.item(i);
+
+			// if current child is required then add his to list
+			if (name.equalsIgnoreCase((child.getNodeName()))) {
+
+				elements.add(child);
+
+			} else {
+
+				findElementsByName(child, elements, name);
+
+			}
+		}
+
+	}
+
+	/**
+	 * find all elements by name
+	 * 
+	 * @param node -
+	 *            current node
+	 * @param elements -
+	 *            list of found elements
+	 * @param name -
+	 *            name element
+	 */
+	static public void findAllElementsByName(nsIDOMNode node,
+			List<nsIDOMNode> elements, String name) {
+
+		try {
+			nsIDOMNodeList list = node.getChildNodes();
+			if (node.getNodeName().equalsIgnoreCase(name)) {
+				elements.add(node);
+			}
+			for (int i = 0; i < list.getLength(); i++) {
+				findAllElementsByName(list.item(i), elements, name);
+			}
+		} catch (XPCOMException e) {
+			// Ignore
+			return;
+		}
+	}
+
+	static public List<ImportBean> createImportBeanList(String projectName,
+			String resourcePath) {
+		List<ImportBean> projectToImport = new ArrayList<ImportBean>();
+		projectToImport.add(createImportBean(projectName, resourcePath));
+		return projectToImport;
+	}
+
+	static public ImportBean createImportBean(String projectName,
+			String resourcePath) {
+		ImportBean importBean = new ImportBean();
+		importBean.setImportProjectName(projectName);
+		importBean.setImportProjectPath(resourcePath);
+		return importBean;
+	}
 }
