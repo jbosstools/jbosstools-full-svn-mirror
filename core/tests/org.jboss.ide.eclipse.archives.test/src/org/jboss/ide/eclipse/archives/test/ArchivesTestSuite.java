b84343ad6e8b33adb44df777acb52bb8e71f94d1
Rob Stryker <rob.stryker@jboss.com>
2008-03-08 03:43:41 +0000

    Archives Unit Tests
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6803 a97e2381-89e5-4abb-bab3-167db6db766c

--- core/tests/org.jboss.ide.eclipse.archives.test/src/org/jboss/ide/eclipse/archives/test/ArchivesTestSuite.java
+++ core/tests/org.jboss.ide.eclipse.archives.test/src/org/jboss/ide/eclipse/archives/test/ArchivesTestSuite.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.jboss.ide.eclipse.archives.test.core.ArchivesCoreTest;
+import org.jboss.ide.eclipse.archives.test.model.MarshallTest;
+import org.jboss.ide.eclipse.archives.test.model.UnmarshallTest;
 import org.jboss.ide.eclipse.archives.test.util.TruezipUtilTest;
-import org.jboss.ide.eclipse.archives.test.xb.MarshallTest;
-import org.jboss.ide.eclipse.archives.test.xb.UnmarshallTest;
 
 public class ArchivesTestSuite extends TestSuite {
     public static Test suite() { 
