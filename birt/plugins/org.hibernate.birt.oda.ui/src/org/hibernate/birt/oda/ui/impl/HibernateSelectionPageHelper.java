5a0a1a660834b1bc7b8886bbd016770116ba5539
Snjezana Peco <snjezana.peco@redhat.com>
2008-05-15 20:38:12 +0000

    Initial import.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8124 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ birt/plugins/org.hibernate.birt.oda.ui/src/org/hibernate/birt/oda/ui/impl/HibernateSelectionPageHelper.java
@@ -0,0 +1,239 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ ******************************************************************************/
+package org.hibernate.birt.oda.ui.impl;
+
+import java.util.Properties;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.birt.oda.impl.HibernateConnection;
+import org.hibernate.birt.oda.ui.Activator;
+import org.hibernate.console.ConsoleConfiguration;
+import org.hibernate.console.KnownConfigurations;
+
+/**
+ * Helper class for Hibernate wizard and property page
+ * 
+ * @author snjeza
+ * 
+ */
+public class HibernateSelectionPageHelper {
+
+	private static final String DEFAULT_MAX_ROWS = "100";
+	private WizardPage wizardPage;
+	private PreferencePage propertyPage;
+	private Combo configurationCombo;
+	private Text maxRows;
+	private Button testButton;
+
+	public HibernateSelectionPageHelper(WizardPage page) {
+		this.wizardPage = page;
+	}
+
+	public HibernateSelectionPageHelper(PreferencePage page) {
+		propertyPage = page;
+	}
+
+	void createCustomControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 3;
+		layout.verticalSpacing = 10;
+		layout.marginBottom = 10;
+		composite.setLayout(layout);
+
+		GridData gridData;
+
+		Label configurationLabel = new Label(composite, SWT.RIGHT);
+		configurationLabel.setText("Configuration:");
+
+		configurationCombo = new Combo(composite, SWT.NONE);
+		gridData = new GridData();
+		gridData.horizontalSpan = 2;
+		gridData.horizontalAlignment = SWT.FILL;
+		gridData.grabExcessHorizontalSpace = true;
+		configurationCombo.setLayoutData(gridData);
+
+		ConsoleConfiguration[] configurations = KnownConfigurations
+				.getInstance().getConfigurations();
+		String[] configurationNames = new String[configurations.length];
+		for (int i = 0; i < configurations.length; i++) {
+			configurationNames[i] = configurations[i].getName();
+		}
+		configurationCombo.setItems(configurationNames);
+
+		Label maxFetchLabel = new Label(composite, SWT.NONE);
+		maxFetchLabel.setText("Max results:");
+
+		maxRows = new Text(composite, SWT.BORDER);
+		maxRows.setLayoutData(gridData);
+
+		configurationCombo.addModifyListener(new ModifyListener() {
+
+			public void modifyText(ModifyEvent e) {
+				validateData();
+			}
+
+		});
+
+		new Label(composite, SWT.NONE);
+		testButton = new Button(composite, SWT.PUSH);
+		testButton.setText("Test Connection...");
+
+		testButton.setLayoutData(new GridData(GridData.CENTER));
+		testButton.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent e) {
+				testConnection();
+			}
+
+		});
+
+		validateData();
+
+	}
+
+	private void setMessage(String message, int type) {
+		if (wizardPage != null)
+			wizardPage.setMessage(message, type);
+		else if (propertyPage != null)
+			propertyPage.setMessage(message, type);
+	}
+
+	private void setMessage(String message) {
+		if (wizardPage != null)
+			wizardPage.setMessage(message);
+		else if (propertyPage != null)
+			propertyPage.setMessage(message);
+	}
+
+	private void validateData() {
+		if (configurationCombo.getText().trim().length() > 0) {
+			setMessage("Edit the selected data source");
+			testButton.setEnabled(true);
+			setPageComplete(true);
+		} else {
+			setMessage("Configuration must not be empty",
+					IMessageProvider.ERROR);
+			testButton.setEnabled(false);
+			setPageComplete(false);
+		}
+	}
+
+	public void initCustomControl(Properties profileProps) {
+		if (profileProps != null) {
+			String confName = profileProps
+					.getProperty(HibernateConnection.CONFIGURATION);
+			if (confName != null) {
+				configurationCombo.setText(confName);
+			}
+			String maxResult = profileProps
+					.getProperty(HibernateConnection.MAX_ROWS);
+			maxRows.setText(maxResult);
+		} else {
+			if (configurationCombo.getItemCount() > 0) {
+				configurationCombo.select(0);
+			}
+			maxRows.setText(DEFAULT_MAX_ROWS);
+		}
+	}
+
+	protected void testConnection() {
+		String configurationName = configurationCombo.getText();
+		ConsoleConfiguration[] configurations = KnownConfigurations
+				.getInstance().getConfigurations();
+		ConsoleConfiguration consoleConfiguration = null;
+		String title = "Test connection";
+		for (int i = 0; i < configurations.length; i++) {
+			if (configurations[i].getName().equals(configurationName)) {
+				consoleConfiguration = configurations[i];
+				break;
+			}
+		}
+		if (consoleConfiguration != null) {
+			try {
+				SessionFactory sessionFactory = consoleConfiguration
+						.getSessionFactory();
+				if (sessionFactory == null) {
+					consoleConfiguration.build();
+					consoleConfiguration.buildSessionFactory();
+					sessionFactory = consoleConfiguration.getSessionFactory();
+				}
+				MessageDialog.openInformation(getShell(), title,
+						"Connection successfull.");
+			} catch (HibernateException e) {
+				String message = e.getLocalizedMessage();
+				IStatus status = new Status(Status.ERROR, Activator.PLUGIN_ID,
+						message, e);
+				Activator.getDefault().getLog().log(status);
+				ErrorDialog.openError(getShell(), title, message, status);
+			}
+		} else {
+			MessageDialog.openConfirm(getShell(), title,
+					"Invalid configuration '" + configurationName + "'.");
+
+		}
+
+	}
+
+	private Shell getShell() {
+		if (wizardPage != null)
+			return wizardPage.getShell();
+		if (propertyPage != null)
+			return propertyPage.getShell();
+		return null;
+	}
+
+	public String getConfiguration() {
+		return configurationCombo.getText();
+	}
+
+	public String getMaxRows() {
+		return maxRows.getText();
+	}
+
+	Properties collectCustomProperties(Properties props) {
+		if (props == null)
+			props = new Properties();
+
+		props
+				.setProperty(HibernateConnection.CONFIGURATION,
+						getConfiguration());
+		props.setProperty(HibernateConnection.MAX_ROWS, getMaxRows());
+		return props;
+	}
+
+	private void setPageComplete(boolean complete) {
+		if (wizardPage != null)
+			wizardPage.setPageComplete(complete);
+		else if (propertyPage != null)
+			propertyPage.setValid(complete);
+	}
+}
---
+++ birt/plugins/org.hibernate.birt.oda.ui/src/org/hibernate/birt/oda/ui/impl/TextMenuManager.java
@@ -0,0 +1,134 @@
+package org.hibernate.birt.oda.ui.impl;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.text.ITextOperationTarget;
+import org.eclipse.jface.text.TextViewer;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+
+
+/**
+ * Text menu manager contains 'undo,redo,cut,copy,paste,select all' menuItem. It
+ * displays on textVeiwer.
+ * 
+ */
+class TextMenuManager
+{
+
+	private Hashtable htActions = new Hashtable( );
+	private MenuManager manager;
+
+	/**
+	 * Constructor to specify the textMenuManager for a text viewer.
+	 * 
+	 * @param viewer
+	 */
+	TextMenuManager( TextViewer viewer )
+	{
+		manager = new MenuManager( );
+		Separator separator = new Separator( "undo" );//$NON-NLS-1$
+		manager.add( separator );
+		separator = new Separator( "copy" );//$NON-NLS-1$
+		manager.add( separator );
+		separator = new Separator( "select" );//$NON-NLS-1$
+		manager.add( separator );
+		manager.appendToGroup( "undo", getAction( "undo", viewer, "Undo", ITextOperationTarget.UNDO ) );
+		manager.appendToGroup( "undo", getAction( "redo", viewer, "Redo", ITextOperationTarget.REDO ) );
+		manager.appendToGroup( "copy", getAction( "cut", viewer, "Cut", ITextOperationTarget.CUT ) );
+		manager.appendToGroup( "copy", getAction( "copy", viewer, "Copy", ITextOperationTarget.COPY ) );//$NON-NLS-1$
+		manager.appendToGroup( "copy", getAction( "paste", viewer, "Paste", ITextOperationTarget.PASTE ) );//$NON-NLS-1$
+		manager.appendToGroup( "select", getAction( "selectall", viewer, "Select all", ITextOperationTarget.SELECT_ALL ) );//$NON-NLS-1$
+
+		manager.add( new Separator("hql") );
+		manager.appendToGroup( "hql", getAction("contentAssist", viewer, "Content Assist",13));
+		manager.appendToGroup( "hql", getAction("contentTip", viewer, "Content Tip",14));
+		manager.appendToGroup( "hql", getAction("format", viewer, "Format HQL source",15));
+		
+		manager.addMenuListener( new IMenuListener( ) {
+
+			public void menuAboutToShow( IMenuManager manager )
+			{
+				Enumeration elements = htActions.elements( );
+				while ( elements.hasMoreElements( ) )
+				{
+					SourceViewerAction action = (SourceViewerAction) elements.nextElement( );
+					action.update( );
+				}
+			}
+		} );
+	}
+	
+	/**
+	 * 
+	 * @param control
+	 * @return
+	 */
+	public Menu getContextMenu( Control control )
+	{
+		return manager.createContextMenu( control );
+	}
+
+	/**
+	 * 
+	 * @param id
+	 * @param viewer
+	 * @param name
+	 * @param operation
+	 * @return
+	 */
+	private final SourceViewerAction getAction( String id, TextViewer viewer,
+			String name, int operation )
+	{
+		SourceViewerAction action = (SourceViewerAction) htActions.get( id );
+		if ( action == null )
+		{
+			action = new SourceViewerAction( viewer, name, operation );
+			htActions.put( id, action );
+		}
+		return action;
+	}
+
+	
+	class SourceViewerAction extends Action
+	{
+
+		private int operationCode = -1;
+		private TextViewer viewer = null;
+
+		public SourceViewerAction( TextViewer viewer, String text,
+				int operationCode )
+		{
+			super( text );
+			this.operationCode = operationCode;
+			this.viewer = viewer;
+		}
+
+		/*
+		 * 
+		 * @see org.eclipse.jface.action.IAction#run()
+		 */
+		public void run( )
+		{
+			viewer.doOperation( operationCode );
+		}
+
+		/**
+		 * update the operation
+		 * 
+		 */
+		public void update( )
+		{
+			setEnabled( viewer.canDoOperation( operationCode ) );
+		}
+
+	}
+
+}
