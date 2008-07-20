650562bf757d0cf06b7e7bbc49f529ac91ee9dec
Snjezana Peco <snjezana.peco@redhat.com>
2008-07-20 01:48:38 +0000

    JBIDE-2233 Ability to create a new "Portlet" project
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@9188 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ portlet/plugins/org.jboss.tools.portlet.ui/src/org/jboss/tools/portlet/ui/INewPortletClassDataModelProperties.java
@@ -0,0 +1,44 @@
+package org.jboss.tools.portlet.ui;
+
+import org.eclipse.jst.j2ee.internal.web.operations.INewServletClassDataModelProperties;
+
+public interface INewPortletClassDataModelProperties extends
+		INewServletClassDataModelProperties {
+
+	static final String GET_PORTLET_CONFIG = "NewPortletClassDataModel.GET_PORTLET_CONFIG"; //$NON-NLS-1$
+	
+	static final String DO_DISPATCH = "NewPortletClassDataModel.DO_DISPATCH"; //$NON-NLS-1$
+	
+	static final String DO_EDIT = "NewPortletClassDataModel.DO_EDIT"; //$NON-NLS-1$
+	
+	static final String DO_VIEW = "NewPortletClassDataModel.DO_VIEW"; //$NON-NLS-1$
+	
+	static final String DO_HELP = "NewPortletClassDataModel.DO_HELP"; //$NON-NLS-1$
+	
+	static final String PROCESS_ACTION = "NewPortletClassDataModel.PROCESS_ACTION"; //$NON-NLS-1$
+	
+	static final String RENDER = "NewPortletClassDataModel.RENDER"; //$NON-NLS-1$
+	
+	static final String NAME = "NewPortletClassDataModel.NAME"; //$NON-NLS-1$
+	
+	static final String TITLE = "NewPortletClassDataModel.TITLE"; //$NON-NLS-1$
+
+	static final String VIEW_MODE = "NewPortletClassDataModel.VIEW_MODE";
+	
+	static final String EDIT_MODE = "NewPortletClassDataModel.EDIT_MODE";
+	
+	static final String HELP_MODE = "NewPortletClassDataModel.HELP_MODE";
+	
+	static final String INSTANCE_NAME = "NewPortletClassDataModel.INSTANCE_NAME";
+	
+	static final String WINDOW_NAME = "NewPortletClassDataModel.WINDOW_NAME";
+	
+	static final String PARENT_PORTAL = "NewPortletClassDataModel.PARENT_PORTAL";
+	
+	static final String PAGE_REGION = "NewPortletClassDataModel.PAGE_REGION";
+	
+	static final String PORTLET_HEIGHT = "NewPortletClassDataModel.PORTLET_HEIGHT";
+	
+	static final String IF_EXISTS = "NewPortletClassDataModel.IF_EXISTS";
+	
+}
---
+++ portlet/plugins/org.jboss.tools.portlet.ui/src/org/jboss/tools/portlet/ui/IPortletUIConstants.java
@@ -0,0 +1,61 @@
+package org.jboss.tools.portlet.ui;
+
+/**
+ * @author snjeza
+ */
+public interface IPortletUIConstants {
+
+	static final String NEW_PORTLET_WIZARD_PAGE_TITLE = "Create Portlet";
+	static final String ADD_PORTLET_WIZARD_PAGE_DESC = "Enter portlet deployment descriptor specific information.";
+	
+	static final String ADD_JBOSS_PORTLET_WIZARD_PAGE_TITLE = "Create Portlet";
+	static final String ADD_JBOSS_PORTLET_WIZARD_PAGE_DESC = "Enter JBoss portlet specific information";
+
+	static final String DISPLAY_NAME_LABEL = "Display name:";
+	static final String TITLE_LABEL = "Title:";
+
+	static final String QUALIFIED_PORTLET = "javax.portlet.Portlet"; //$NON-NLS-1$
+	static final String QUALIFIED_GENERIC_PORTLET = "javax.portlet.GenericServlet"; //$NON-NLS-1$
+	
+	static final String QUALIFIED_PORTLET_EXCEPTION = "javax.portlet.PortletException"; //$NON-NLS-1$
+	static final String QUALIFIED_IO_EXCEPTION = "java.io.IOException"; //$NON-NLS-1$
+	static final String QUALIFIED_SECURITY_EXCEPTION = "javax.portlet.PortletSecurityException"; //$NON-NLS-1$
+	static final String QUALIFIED_UNAVALIABLE_EXCEPTION = "javax.portlet.UnavailableException"; //$NON-NLS-1$
+	static final String QUALIFIED_PRINTWRITER = "java.io.PrintWriter"; //$NON-NLS-1$
+	
+	static final String QUALIFIED_PORTLET_CONFIG = "javax.portlet.PortletConfig"; //$NON-NLS-1$
+	static final String QUALIFIED_PORTLET_REQUEST = "javax.portlet.RenderRequest"; //$NON-NLS-1$
+	static final String QUALIFIED_PORTLET_RESPONSE = "javax.portlet.RenderResponse"; //$NON-NLS-1$
+	static final String QUALIFIED_ACTION_REQUEST = "javax.portlet.ActionRequest"; //$NON-NLS-1$
+	static final String QUALIFIED_ACTION_RESPONSE = "javax.portlet.ActionResponse"; //$NON-NLS-1$
+	
+	
+	static final String METHOD_INIT = "init"; //$NON-NLS-1$
+	static final String METHOD_DESTROY = "destroy"; //$NON-NLS-1$
+	static final String METHOD_GET_PORTLET_CONFIG = "getPortletConfig"; //$NON-NLS-1$
+	static final String METHOD_DO_EDIT = "doEdit"; //$NON-NLS-1$
+	static final String METHOD_DO_VIEW = "doView"; //$NON-NLS-1$
+	static final String METHOD_DO_HELP = "doHelp"; //$NON-NLS-1$
+	static final String METHOD_DO_DISPATCH = "doDispatch"; //$NON-NLS-1$
+	static final String METHOD_PROCESS_ACTION = "doProcessAction"; //$NON-NLS-1$
+	static final String METHOD_RENDER = "render"; //$NON-NLS-1$
+	static final String METHOD_TO_STRING = "toString"; //$NON-NLS-1$
+	
+	static final String PORTLET_INIT_SIGNATURE = "(Ljavax/portlet/PortletConfig;)V"; //$NON-NLS-1$
+	static final String DESTROY_SIGNATURE = "()V"; //$NON-NLS-1$
+	static final String GET_PORTLET_CONFIG_SIGNATURE = "()Ljavax/portlet/PortletConfig;"; //$NON-NLS-1$
+	static final String DO_VIEW_SIGNATURE = "(Ljavax/portlet/RenderRequest;Ljavax/portlet/RenderResponse;)V"; //$NON-NLS-1$
+	static final String DO_EDIT_SIGNATURE = "(Ljavax/portlet/RenderRequest;Ljavax/portlet/RenderResponse;)V"; //$NON-NLS-1$
+	static final String DO_HELP_SIGNATURE = "(Ljavax/portlet/RenderRequest;Ljavax/portlet/RenderResponse;)V"; //$NON-NLS-1$
+	static final String DO_DISPATCH_SIGNATURE = "(Ljavax/portlet/RenderRequest;Ljavax/portlet/RenderResponse;)V"; //$NON-NLS-1$
+	static final String RENDER_SIGNATURE = "(Ljavax/portlet/RenderRequest;Ljavax/portlet/RenderResponse;)V"; //$NON-NLS-1$
+	static final String PROCESS_ACTION_SIGNATURE = "(Ljavax/portlet/ActionRequest;Ljavax/portlet/ActionResponse;)V"; //$NON-NLS-1$
+	
+	static final String INSTANCE_NAME_LABEL = "Instance Name:";
+	static final String WINDOW_NAME_LABEL = "Window Name:";
+	static final String PARENT_PORTAL_LABEL = "Parent Reference:";
+	static final String PAGE_REGION_LABEL = "Region:";
+	static final String PORTLET_HEIGHT_LABEL = "Height:";
+	static final String IF_EXISTS_LABEL = "If Exists:";
+	
+}
