91081af015a2bd07e910566e33b749ae286c7028
Marshall Culpepper <mculpepper@jboss.com>
2008-07-22 17:15:36 +0000

    tagging
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/jbosstools-3.0.0.Alpha1@9207 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ core/plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/ant/AntArchivesCore.java
@@ -0,0 +1,43 @@
+package org.jboss.ide.eclipse.archives.core.ant;
+
+import org.eclipse.core.runtime.IPath;
+import org.jboss.ide.eclipse.archives.core.ArchivesCore;
+import org.jboss.ide.eclipse.archives.core.build.ModelChangeListener;
+import org.jboss.ide.eclipse.archives.core.model.ArchivesModel;
+import org.jboss.ide.eclipse.archives.core.model.IArchivesLogger;
+import org.jboss.ide.eclipse.archives.core.model.IExtensionManager;
+import org.jboss.ide.eclipse.archives.core.model.IPreferenceManager;
+import org.jboss.ide.eclipse.archives.core.model.IArchivesVFS;
+
+/**
+ * A core API entry point for ant.
+ * @author rob stryker (rob.stryker@redhat.com)
+ *
+ */
+public class AntArchivesCore extends ArchivesCore {
+
+	public AntArchivesCore () {
+		super(STANDALONE);
+		ArchivesModel.instance().addModelListener(new ModelChangeListener());
+	}
+	
+	protected IExtensionManager createExtensionManager() {
+		return new AntExtensionManager();
+	}
+
+	protected IPreferenceManager createPreferenceManager() {
+		return new AntPreferenceManager();
+	}
+
+	protected IArchivesVFS createVFS() {
+		return new AntVariables();
+	}
+
+	public void preRegisterProject(IPath project) {
+		
+	}
+
+	protected IArchivesLogger createLogger() {
+		return null;
+	}
+}
