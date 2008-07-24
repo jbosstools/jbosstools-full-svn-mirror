ca5ab33a310271364cb569d540dc1d682a445371
Rob Stryker <rob.stryker@jboss.com>
2008-07-24 20:09:27 +0000

    tagging
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@9312 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ 3.0.0.Alpha1/vpe/plugins/org.jboss.tools.vpe.xulrunner/src/org/jboss/tools/vpe/xulrunner/browser/AppFileLocProvider.java
@@ -0,0 +1,74 @@
+package org.jboss.tools.vpe.xulrunner.browser;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.Platform;
+import org.jboss.tools.vpe.xulrunner.BrowserPlugin;
+import org.mozilla.xpcom.IAppFileLocProvider;
+
+public class AppFileLocProvider implements IAppFileLocProvider {
+	private File xulRunnerPath;
+	private File userDataPath;
+	
+	private static final String PLUGINS_DIRECTORY = "plugins"; // $NON-NLS-1$
+	private static final String HISTORY_FILE = "history.dat"; // $NON-NLS-1$
+	private static final String COMPREG_FILE = "compreg.dat"; // $NON-NLS-1$
+	private static final String XPTI_FILE = "xpti.dat"; // $NON-NLS-1$
+	private static final String COMPONENTS_DIRECTORY = "components"; //$NON-NLS-1$
+	
+	
+	public AppFileLocProvider(File xulRunnerPath) {
+		this.xulRunnerPath = xulRunnerPath;
+		this.userDataPath = Platform.getLocation().append(".metadata/.plugins") // $NON-NLS-1$
+				.append(BrowserPlugin.PLUGIN_ID)
+				.append("xulrunner").toFile(); // $NON-NLS-1$
+	}
+	
+	public File getFile(String prop, boolean[] persistent) {
+		persistent[0] = false;
+		
+		File resultFile = null;
+		
+		if ("ProfD".equals(prop)) { // $NON-NLS-1$
+			resultFile = userDataPath;
+		} else if ("UHist".equals(prop)) { // $NON-NLS-1$
+			resultFile = new File(userDataPath, HISTORY_FILE); 
+		} else if ("ComRegF".equals(prop)) { // $NON-NLS-1$
+			resultFile = new File(userDataPath, COMPREG_FILE);
+		} else if ("XptiRegF".equals(prop)) { // $NON-NLS-1$
+			resultFile = new File(userDataPath, XPTI_FILE);
+		} else if ("GreD".equals(prop)) { // $NON-NLS-1$
+			resultFile = xulRunnerPath;
+		} else if ("GreComsD".equals(prop) || "ComsD".equals(prop)) { // $NON-NLS-1$ // $NON-NLS-1$
+			resultFile = new File(xulRunnerPath, COMPONENTS_DIRECTORY);
+		}
+
+		if (BrowserPlugin.DEBUG_BROWSERSTART) {
+			System.out.println("AppFileLocProvider.getFile(" + prop + ")=\"" + (resultFile == null ? "null" : resultFile.toString()) + "\"");
+		}
+		
+		return resultFile;
+	}
+
+	public File[] getFiles(String prop) {
+		File[] resultFiles = null;
+		if("APluginsDL".equals(prop)) { // $NON-NLS-1$
+			resultFiles = new File[] {new File(xulRunnerPath, PLUGINS_DIRECTORY)};
+		}
+		
+		if (BrowserPlugin.DEBUG_BROWSERSTART) {
+			if (resultFiles != null) {
+				int size = resultFiles.length;
+
+				System.out.print("AppFileLocProvider.getFiles(" + prop + ")={\"");
+				for(int i=0; i<size; i++) {
+					System.out.print((i > 0 ? ", " : "") + resultFiles[i].toString());
+				}
+				System.out.println("\"}");
+			}
+		}
+		
+		return resultFiles;
+	}
+
+}
---
+++ 3.0.0.Alpha1/vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/util/IELService.java
@@ -0,0 +1,60 @@
+
+
+package org.jboss.tools.vpe.editor.util;
+
+
+import org.eclipse.core.resources.IFile;
+import org.jboss.tools.vpe.editor.context.VpePageContext;
+import org.w3c.dom.Element;
+
+
+/**
+ * The service that substitute El values in vpe source editor. For more details
+ * see issues http://jira.jboss.com/jira/browse/JBIDE-2010 and
+ * http://jira.jboss.com/jira/browse/JBIDE-1410
+ * 
+ * @author Eugeny Stherbin
+ */
+public interface IELService {
+
+    /**
+     * Return the {@link String} with substitued el values for given resource.
+     * 
+     * @param resourceFile the resource file where resource
+     * @param resourceString the source string that will be substitute
+     * 
+     * @return string where el values was substituted.
+     */
+    String replaceEl(IFile resourceFile, String resourceString);
+    
+    
+    /**
+     * Reverse replace.
+     * 
+     * @param resourceFile the resource file
+     * @param replacedString the replaced string
+     * 
+     * @return the string
+     */
+    String reverseReplace(IFile resourceFile,String replacedString);
+
+    /**
+     * Checks if is available.
+     * 
+     * @param resourceFile the resource file
+     * 
+     * @return true, if is available
+     */
+    boolean isAvailable(IFile resourceFile);
+
+    /**
+     * Checks if is cloneable node.
+     * 
+     * @param sourceNode the source node
+     * @param pageContext the page context
+     * 
+     * @return true, if is cloneable node
+     */
+    public boolean isCloneableNode(VpePageContext pageContext, Element sourceNode);
+
+}
