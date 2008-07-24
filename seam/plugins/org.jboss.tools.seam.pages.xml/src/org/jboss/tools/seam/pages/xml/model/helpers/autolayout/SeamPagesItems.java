1a0ffb835c57795798f0918a471b062bdd14e543
Rob Stryker <rob.stryker@jboss.com>
2008-07-24 20:30:53 +0000

    tagging
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/jbosstools-3.0.0.Alpha1@9317 a97e2381-89e5-4abb-bab3-167db6db766c

--- seam/plugins/org.jboss.tools.seam.pages.xml/src/org/jboss/tools/seam/pages/xml/model/helpers/autolayout/SeamPagesItems.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.tools.seam.pages.xml.model.helpers.autolayout;
-
-import org.jboss.tools.jst.web.model.helpers.autolayout.Items;
-
-public class SeamPagesItems extends Items {
-
-}
