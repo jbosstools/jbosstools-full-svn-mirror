8ab496a880d6fe1f83b5b4a020cc5ac98eb29772
Rob Stryker <rob.stryker@jboss.com>
2008-07-24 19:16:38 +0000

    tagging
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/-jbosstools-3.0.0.Alpah1@9287 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jbpm/plugins/org.jboss.tools.flow.jpdl4.core/src/org/jboss/tools/flow/jpdl4/core/Process.java
@@ -0,0 +1,54 @@
+package org.jboss.tools.flow.jpdl4.core;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class Process {
+	
+	private String name;
+	private HashMap<String, Object> metaData = new HashMap<String, Object>();
+	private ArrayList<Node> nodes = new ArrayList<Node>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Object getMetaData(String key) {
+		return metaData.get(key);
+	}
+
+	public void setMetaData(String key, Object value) {
+		metaData.put(key, value);
+	}
+
+	public List<Node> getNodes() {
+		return nodes;
+	}
+
+	public void addNode(Node node) {
+		nodes.add(node);
+		node.setNodeContainer(this);
+	}
+
+	public void removeNode(Node node) {
+		node.setNodeContainer(null);
+		nodes.remove(node);
+	}
+	
+	public StartState getStartState() {
+		for (Iterator<Node> iterator = nodes.iterator(); iterator.hasNext(); ) {
+			Node node = iterator.next();
+			if (node instanceof StartState) {
+				return (StartState)node;
+			}
+		}
+		return null;
+	}
+
+}
---
+++ jbpm/plugins/org.jboss.tools.flow.jpdl4.graph/src/org/jboss/tools/flow/jpdl4/graph/wrapper/StartStateWrapper.java
@@ -0,0 +1,24 @@
+package org.jboss.tools.flow.jpdl4.graph.wrapper;
+
+import org.jboss.tools.flow.editor.core.AbstractConnectionWrapper;
+import org.jboss.tools.flow.editor.core.NodeWrapper;
+import org.jboss.tools.flow.jpdl4.core.StartState;
+
+public class StartStateWrapper extends BaseNodeWrapper {
+
+    private static final long serialVersionUID = 1L;
+
+    public StartStateWrapper() {
+        setElement(new StartState());
+        setName("Start");
+    }
+    
+    public StartState getStartState() {
+        return (StartState) getElement();
+    }
+    
+    public boolean acceptsIncomingConnection(AbstractConnectionWrapper connection, NodeWrapper source) {
+        return false;
+    }
+
+}
