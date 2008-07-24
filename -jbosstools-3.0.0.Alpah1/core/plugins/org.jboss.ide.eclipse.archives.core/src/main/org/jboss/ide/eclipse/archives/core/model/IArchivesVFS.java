88cba555c0d828cf43c650819bd22a23f8322ca9
Rob Stryker <rob.stryker@jboss.com>
2008-07-24 19:16:26 +0000

    tagging
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@9282 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ -jbosstools-3.0.0.Alpah1/core/plugins/org.jboss.ide.eclipse.archives.core/src/main/org/jboss/ide/eclipse/archives/core/model/IArchivesVFS.java
@@ -0,0 +1,52 @@
+/**
+ * JBoss, a Division of Red Hat
+ * Copyright 2006, Red Hat Middleware, LLC, and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.ide.eclipse.archives.core.model;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+
+/**
+ * 
+ * @author rob.stryker <rob.stryker@redhat.com>
+ *
+ */
+public interface IArchivesVFS {
+	
+	/**
+	 * Returns a list of workspace-relative paths
+	 * that are children of the parameter. 
+	 * @param path
+	 * @return
+	 */
+	public IPath[] getWorkspaceChildren(IPath path);
+	
+	/**
+	 * Translates a workspace path to a global path
+	 * @param path
+	 * @return
+	 */
+	public IPath workspacePathToAbsolutePath(IPath path);
+	public IPath[] workspacePathToAbsolutePath(IPath[] paths);
+	public String getProjectName(IPath absolutePath);
+	public String performStringSubstitution(String expression,	boolean reportUndefinedVariables) throws CoreException;
+	public String performStringSubstitution(String expression,	String projectName, boolean reportUndefinedVariables) throws CoreException;
+}
