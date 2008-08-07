851cceb50b3eccbaf8655b52123d7d5932bb98cd
Denis Golovin <dgolovin@exadel.com>
2008-08-07 18:46:20 +0000

    Tag components for JBT 3.0.0.Alpha1 release
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/jbosstools-3.0.0.Alpha1@9606 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ portlet/plugins/org.jboss.tools.portlet.core/src/org/jboss/tools/portlet/core/internal/project/facet/JSFPortletFacetInstallDataModelProvider.java
@@ -0,0 +1,42 @@
+/*************************************************************************************
+ * Copyright (c) 2008 JBoss, a division of Red Hat and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     JBoss, a division of Red Hat - Initial implementation.
+ ************************************************************************************/
+package org.jboss.tools.portlet.core.internal.project.facet;
+
+import java.util.Set;
+
+import org.eclipse.wst.common.componentcore.datamodel.FacetInstallDataModelProvider;
+import org.jboss.tools.portlet.core.IPortletConstants;
+
+/**
+ * @author snjeza
+ * 
+ */
+public class JSFPortletFacetInstallDataModelProvider extends
+		FacetInstallDataModelProvider implements IPortletConstants {
+
+	@Override
+	public Object getDefaultProperty(String propertyName) {
+		if(propertyName.equals(FACET_ID)){
+			return IPortletConstants.JSFPORTLET_FACET_ID;
+		}
+		if (propertyName.equals(IPortletConstants.DEPLOY_JARS)) {
+			return Boolean.TRUE;
+		}
+		return super.getDefaultProperty(propertyName);
+	}
+
+	@Override
+	public Set<String> getPropertyNames() {
+		Set<String> propertyNames = super.getPropertyNames();
+		propertyNames.add(IPortletConstants.DEPLOY_JARS);
+		return propertyNames;
+	}
+}
---
+++ portlet/plugins/org.jboss.tools.portlet.ui/src/org/jboss/tools/portlet/ui/internal/project/facet/PortletFacetInstallPage.java
@@ -0,0 +1,61 @@
+package org.jboss.tools.portlet.ui.internal.project.facet;
+
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.wst.common.frameworks.datamodel.AbstractDataModelProvider;
+import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
+import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.common.frameworks.internal.datamodel.ui.DataModelWizardPage;
+import org.eclipse.wst.common.project.facet.ui.IFacetWizardPage;
+import org.eclipse.wst.common.project.facet.ui.IWizardContext;
+import org.jboss.tools.portlet.ui.PortletUIActivator;
+
+/**
+ * @author snjeza
+ */
+public class PortletFacetInstallPage extends DataModelWizardPage implements
+		IFacetWizardPage {
+
+	private IDialogSettings dialogSettings;
+
+	public PortletFacetInstallPage() {
+		super(DataModelFactory.createDataModel(new AbstractDataModelProvider() {
+		}), "jboss.portal.facet.install.page");
+		setTitle("JBoss Portlet Capabilities");
+		setDescription("Add JBoss Portlet capabilities to this Web Project");
+		dialogSettings = PortletUIActivator.getDefault().getDialogSettings();
+	}
+
+	@Override
+	protected Composite createTopLevelComposite(Composite parent) {
+		initializeDialogUnits(parent);
+		Composite composite = new Composite(parent, SWT.NONE);
+		return composite;
+	}
+
+	@Override
+	protected String[] getValidationPropertyNames() {
+		return new String[0];
+	}
+
+	public void setConfig(Object config) {
+		model.removeListener(this);
+		synchHelper.dispose();
+
+		model = (IDataModel) config;
+		model.addListener(this);
+		synchHelper = initializeSynchHelper(model);
+	}
+
+	public void setWizardContext(IWizardContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void transferStateToConfig() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
