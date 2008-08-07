1e0d84f0ae66284bf896e8de41860ccf7e3ac4ff
Denis Golovin <dgolovin@exadel.com>
2008-08-07 18:44:07 +0000

    Delete 3.0.0.Alpha1 tag content to reatag it
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@9594 a97e2381-89e5-4abb-bab3-167db6db766c

--- jbosstools-3.0.0.Alpha1/vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.tools.vpe.editor.util;
-
-public class Constants {
-
-	public static final String TRUE = "true"; //$NON-NLS-1$
-	public static final String FALSE = "false"; //$NON-NLS-1$
-	public static final String EMPTY = ""; //$NON-NLS-1$
-
-}
