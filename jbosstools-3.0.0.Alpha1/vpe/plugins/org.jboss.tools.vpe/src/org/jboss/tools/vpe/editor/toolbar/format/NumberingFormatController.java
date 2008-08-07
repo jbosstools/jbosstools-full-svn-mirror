c1b30ddd8762b625d2282bf0e845f5e116551887
Denis Golovin <dgolovin@exadel.com>
2008-08-07 18:46:41 +0000

    Tag components for JBT 3.0.0.Alpha1 release
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags@9611 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jbosstools-3.0.0.Alpha1/vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/toolbar/format/NumberingFormatController.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/ 
+package org.jboss.tools.vpe.editor.toolbar.format;
+
+import org.eclipse.swt.widgets.ToolItem;
+import org.w3c.dom.Attr;
+
+import org.jboss.tools.vpe.editor.template.textformating.FormatAttributeData;
+
+/**
+ * @author Igels
+ */
+public class NumberingFormatController extends ToolItemFormatController {
+
+	public static String TYPE = "NumberingFormat";
+
+	/**
+	 * @param manager
+	 * @param toolItem
+	 */
+	public NumberingFormatController(FormatControllerManager manager, ToolItem toolItem) {
+		super(manager, toolItem);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jboss.tools.vpe.editor.toolbar.format.IFormatController#getType()
+	 */
+	public String getType() {
+		return TYPE;
+	}
+
+	protected void setStyle(Attr styleAttribute, FormatAttributeData templateData) {
+		// TODO
+	}
+
+	public void setToolbarItemSelection() {
+		// TODO
+	}
+
+	protected boolean isStyleSet(Attr attribute, FormatAttributeData templateData) {
+		// TODO
+		return false;
+	}
+}
---
+++ jbosstools-3.0.0.Alpha1/vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/util/Constants.java
@@ -0,0 +1,20 @@
+/******************************************************************************* 
+ * Copyright (c) 2007 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+package org.jboss.tools.vpe.editor.util;
+
+public class Constants {
+
+	public static final String TRUE = "true"; //$NON-NLS-1$
+	public static final String FALSE = "false"; //$NON-NLS-1$
+	public static final String EMPTY = ""; //$NON-NLS-1$
+	public static final String YES_STRING = "yes"; //$NON-NLS-1$
+}
