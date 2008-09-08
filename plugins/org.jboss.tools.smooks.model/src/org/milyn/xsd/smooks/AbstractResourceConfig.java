f7c97d73db9cc525fd2e5fa6f909ba8e5009e694
DartPeng <dpeng@redhat.com>
2008-09-08 03:14:17 +0000

    first creation
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/smooks@10124 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.smooks.model/src/org/milyn/xsd/smooks/AbstractResourceConfig.java
@@ -0,0 +1,23 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.milyn.xsd.smooks;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Abstract Resource Config</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.milyn.xsd.smooks.SmooksPackage#getAbstractResourceConfig()
+ * @model abstract="true"
+ *        extendedMetaData="name='abstract-resource-config' kind='empty'"
+ * @generated
+ */
+public interface AbstractResourceConfig extends EObject {
+} // AbstractResourceConfig
