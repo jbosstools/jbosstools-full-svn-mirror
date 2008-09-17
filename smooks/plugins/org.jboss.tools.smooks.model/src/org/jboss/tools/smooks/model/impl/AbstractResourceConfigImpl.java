f55b889fb5f8ca72d48c55be34d70c62025439e9
DartPeng <dpeng@redhat.com>
2008-09-17 03:19:38 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10271 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ smooks/plugins/org.jboss.tools.smooks.model/src/org/jboss/tools/smooks/model/impl/AbstractResourceConfigImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.milyn.xsd.smooks.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+import org.milyn.xsd.smooks.AbstractResourceConfig;
+import org.milyn.xsd.smooks.SmooksPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Abstract Resource Config</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class AbstractResourceConfigImpl extends EObjectImpl implements AbstractResourceConfig {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected AbstractResourceConfigImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return SmooksPackage.Literals.ABSTRACT_RESOURCE_CONFIG;
+	}
+
+} //AbstractResourceConfigImpl
