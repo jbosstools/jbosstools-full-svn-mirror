464e7acb2ff0d4bdfc7319386b3e92af1aedba11
DartPeng <dpeng@redhat.com>
2008-09-22 10:47:14 +0000

    change the extension points information store way
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10388 a97e2381-89e5-4abb-bab3-167db6db766c

--- smooks/plugins/org.jboss.tools.smooks.ui/src/org/jboss/tools/smooks/ui/gef/model/GraphRootModel.java
+++ smooks/plugins/org.jboss.tools.smooks.ui/src/org/jboss/tools/smooks/ui/gef/model/GraphRootModel.java
@@ -10,10 +10,40 @@
  ******************************************************************************/
 package org.jboss.tools.smooks.ui.gef.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author Dart Peng
  * @Date Jul 30, 2008
  */
 public class GraphRootModel extends AbstractStructuredDataModel {
+	private List<SourceModel> sourceModelList = new ArrayList<SourceModel>();
 	
+	private List<TargetModel> targetModelList = new ArrayList<TargetModel>();
+
+	public List<SourceModel> loadSourceModelList() {
+		sourceModelList.clear();
+		for (Iterator iterator = children.iterator(); iterator.hasNext();) {
+			Object obj = (Object) iterator.next();
+			if(obj.getClass() == SourceModel.class){
+				sourceModelList.add((SourceModel)obj);
+			}
+		}
+		return sourceModelList;
+	}
+
+
+	public List<TargetModel> loadTargetModelList() {
+		targetModelList.clear();
+		for (Iterator iterator = children.iterator(); iterator.hasNext();) {
+			Object obj = (Object) iterator.next();
+			if(obj.getClass() == TargetModel.class){
+				targetModelList.add((TargetModel)obj);
+			}
+		}
+		return targetModelList;
+	}
+
 }
