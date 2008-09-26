db12e28e34da1c9181cd32b07d4c24f272b6d8e8
DartPeng <dpeng@redhat.com>
2008-09-26 06:22:23 +0000

    delete the freemarker.jar. I will find other way to instead of it.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10493 a97e2381-89e5-4abb-bab3-167db6db766c

--- smooks/tests/org.jboss.tools.smooks.test/src/org/jboss/tools/smooks/test/xml/FreeMarkerTest.java
+++ smooks/tests/org.jboss.tools.smooks.test/src/org/jboss/tools/smooks/test/xml/FreeMarkerTest.java
@@ -10,18 +10,11 @@
  ******************************************************************************/
 package org.jboss.tools.smooks.test.xml;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
 import junit.framework.TestCase;
 
-import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.io.SAXReader;
-import org.jboss.tools.smooks.java2xml.analyzer.DOM2FreeMarkerTransformor;
-import org.jboss.tools.smooks.java2xml.analyzer.FreeMarkerContentReplacer;
 
 /**
  * @author Dart Peng
@@ -29,17 +22,17 @@ import org.jboss.tools.smooks.java2xml.analyzer.FreeMarkerContentReplacer;
  */
 public class FreeMarkerTest extends TestCase {
 	public void testParse() throws DocumentException, IOException {
-		Reader reader = new InputStreamReader(FreeMarkerTest.class
-				.getResourceAsStream("test.flt"));
-		FreeMarkerContentReplacer replacer = new FreeMarkerContentReplacer();
-		String contents = replacer.replaceFreeMarkerTemplate(reader);
-		System.out.println(contents);
-
-		SAXReader sax = new SAXReader();
-		Document doc = sax.read(new ByteArrayInputStream(contents.getBytes()));
-		DOM2FreeMarkerTransformor trans = new DOM2FreeMarkerTransformor();
-		String result = trans.transformDOM(doc);
-		
-		System.out.println(result);
+//		Reader reader = new InputStreamReader(FreeMarkerTest.class
+//				.getResourceAsStream("test.flt"));
+//		FreeMarkerContentReplacer replacer = new FreeMarkerContentReplacer();
+//		String contents = replacer.replaceFreeMarkerTemplate(reader);
+//		System.out.println(contents);
+//
+//		SAXReader sax = new SAXReader();
+//		Document doc = sax.read(new ByteArrayInputStream(contents.getBytes()));
+//		DOM2FreeMarkerTransformor trans = new DOM2FreeMarkerTransformor();
+//		String result = trans.transformDOM(doc);
+//		
+//		System.out.println(result);
 	}
 }
