295d1f8d40984e2f214559200bb603b9faf56ccc
Maxim Areshkau <mareshkau@exadel.com>
2008-10-13 12:55:28 +0000

    =JBIDE-2828, validation for css file was added
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10790 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe.resref/src/org/jboss/tools/vpe/resref/core/VpeCSSFileValidator.java
+++ vpe/plugins/org.jboss.tools.vpe.resref/src/org/jboss/tools/vpe/resref/core/VpeCSSFileValidator.java
@@ -1,55 +1,53 @@
-/*******************************************************************************
-* Copyright (c) 2007-2008 Red Hat, Inc.
+/******************************************************************************* 
+* Copyright (c) 2007 Red Hat, Inc.
 * Distributed under license by Red Hat, Inc. All rights reserved.
 * This program is made available under the terms of the
 * Eclipse Public License v1.0 which accompanies this distribution,
 * and is available at http://www.eclipse.org/legal/epl-v10.html
 *
-* Contributor:
+* Contributors:
 *     Red Hat, Inc. - initial API and implementation
 ******************************************************************************/
 package org.jboss.tools.vpe.resref.core;
 
+import java.io.File;
 import java.util.Properties;
 
-import org.jboss.tools.common.el.core.parser.ELParser;
-import org.jboss.tools.common.el.core.parser.ELParserUtil;
+import org.eclipse.osgi.util.NLS;
 import org.jboss.tools.common.meta.action.impl.DefaultWizardDataValidator;
 import org.jboss.tools.common.meta.action.impl.SpecialWizardSupport;
+import org.jboss.tools.jst.web.messages.xpl.WebUIMessages;
 
 /**
  * @author mareshkau
  *
  */
-public class VpeElVariableValidator extends DefaultWizardDataValidator {
-
+public class VpeCSSFileValidator extends DefaultWizardDataValidator {
+	
 	/**
 	 * 
 	 * @param support
 	 * @param step
 	 */
-	public VpeElVariableValidator(SpecialWizardSupport support, int step) {
+	public VpeCSSFileValidator(SpecialWizardSupport support, int step) {
 		super.setSupport(support, step);
 	}
-
 	/* (non-Javadoc)
 	 * @see org.jboss.tools.common.meta.action.impl.DefaultWizardDataValidator#validate(java.util.Properties)
 	 */
 	@Override
 	public void validate(Properties data) {
-
+		
 		super.validate(data);
+		
 		if( (getErrorMessage()!=null) ||
 				(getWarningMessage()!=null)) {
 			return;
 		} 
 	 	String location = (String) data.get("location"); //$NON-NLS-1$
-	 	ELParser elParser = ELParserUtil.getDefaultFactory().createParser();
-	 	elParser.parse("#{"+location+'}'); //$NON-NLS-1$
-	 	if(elParser.getSyntaxErrors().size()>0) {
-	 		message=Messages.INVALID_EL_EXPRESSION;
+	 	if(!new File(location).isFile()) {
+	 		message = NLS.bind(WebUIMessages.FILE_DOESNOT_EXIST,location);
 	 	}
-	 	
+		
 	}
-
 }
