35ba3239b972010657f1f9a17dcf43b047f9c5bd
DartPeng <dpeng@redhat.com>
2008-10-20 10:28:27 +0000

    JBIDE-2835
    Delete them , they are unusable
    
    
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.0.0.Beta1@10980 a97e2381-89e5-4abb-bab3-167db6db766c

--- smooks/plugins/org.jboss.tools.smooks.javabean/src/org/jboss/tools/smooks/javabean/model/JavaStructuredDataModel.java
+++
@@ -1,6 +0,0 @@
-package org.jboss.tools.smooks.javabean.model;
-
-import org.jboss.tools.smooks.ui.gef.model.StructuredDataModel;
-
-public class JavaStructuredDataModel extends StructuredDataModel {
-}
