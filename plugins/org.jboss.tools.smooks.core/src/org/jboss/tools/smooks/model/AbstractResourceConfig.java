10c5b0cd89159b69c0a4f97442a333b3455499f3
DartPeng <dpeng@redhat.com>
2008-10-20 10:08:30 +0000

    Share project "org.jboss.tools.smooks.core" into "https://svn.jboss.org/repos/jbosstools/trunk/smooks/plugins"
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/smooks@10973 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.smooks.core/src/org/jboss/tools/smooks/model/AbstractResourceConfig.java
@@ -0,0 +1,23 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.jboss.tools.smooks.model;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Abstract Resource Config</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.jboss.tools.smooks.model.SmooksPackage#getAbstractResourceConfig()
+ * @model abstract="true"
+ *        extendedMetaData="name='abstract-resource-config' kind='empty'"
+ * @generated
+ */
+public interface AbstractResourceConfig extends EObject {
+} // AbstractResourceConfig
