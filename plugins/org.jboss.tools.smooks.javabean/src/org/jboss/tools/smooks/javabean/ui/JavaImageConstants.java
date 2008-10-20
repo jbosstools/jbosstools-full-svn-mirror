4833dd0af390131aa17c48de31d2a8d76a300f5a
DartPeng <dpeng@redhat.com>
2008-10-20 10:26:07 +0000

    JBIDE-2835
    Delete them , they are unusable
    
    
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/smooks@10979 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.smooks.javabean/src/org/jboss/tools/smooks/javabean/ui/JavaImageConstants.java
+++
@@ -1,6 +0,0 @@
-package org.jboss.tools.smooks.javabean.ui;
-
-public class JavaImageConstants {
-	public static final String IMAGE_JAVA_ATTRIBUTE = "java_attribute";
-	public static final String IMAGE_JAVA_OBJECT = "java_object";
-}
