70151a3175861192090daed3dde5652ed801d3b3
DartPeng <dpeng@redhat.com>
2008-10-21 03:42:46 +0000

    JBIDE-2942
    remove all needless codes
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/smooks@11025 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.smooks.ui/src/org/jboss/tools/smooks/ui/gef/figures/IExpandable.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.tools.smooks.ui.gef.figures;
-
-import org.eclipse.draw2d.IFigure;
-
-public interface IExpandable extends IFigure {
-	boolean isExpanded();
-
-	void setExpanded(boolean isExpanded);
-}
