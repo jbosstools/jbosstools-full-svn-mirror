65c6e4d0ecc8b287d657299517cba007f180a9bc
Rob Stryker <rob.stryker@jboss.com>
2008-10-22 23:22:51 +0000

    erasing bad copy
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.0.0.Beta1@11090 a97e2381-89e5-4abb-bab3-167db6db766c

--- as/as/plugins/org.jboss.ide.eclipse.as.ui/jbossui/org/jboss/ide/eclipse/as/ui/IPreferenceKeys.java
+++
@@ -1,6 +0,0 @@
-package org.jboss.ide.eclipse.as.ui;
-
-
-public interface IPreferenceKeys {
-	public static final String ENABLED_DECORATORS = "org.jboss.ide.eclipse.as.ui.preferences.enabledDecorators";
-}
