90bcf5ad54c13357f43d768dd6fbef3110de110b
DartPeng <dpeng@redhat.com>
2008-11-05 09:49:17 +0000

    JBIDE-2988
    Check the "selector" string , if there is any "/" "\" ":" , the analyzer will throw a exception 
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/smooks@11515 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.smooks.ui/src/org/jboss/tools/smooks/xml2java/analyzer/XMLSourceModelAnalyzer.java
+++ plugins/org.jboss.tools.smooks.ui/src/org/jboss/tools/smooks/xml2java/analyzer/XMLSourceModelAnalyzer.java
@@ -10,15 +10,68 @@
  ******************************************************************************/
 package org.jboss.tools.smooks.xml2java.analyzer;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.ecore.xml.type.AnyType;
+import org.jboss.tools.smooks.graphical.GraphInformations;
+import org.jboss.tools.smooks.model.AbstractResourceConfig;
+import org.jboss.tools.smooks.model.ResourceConfigType;
+import org.jboss.tools.smooks.model.SmooksResourceListType;
+import org.jboss.tools.smooks.model.util.SmooksModelUtils;
 
 /**
  * @author Dart Peng<br>
  *         Date : Sep 23, 2008
  */
-public class XMLSourceModelAnalyzer extends AbstractXMLModelAnalyzer{
+public class XMLSourceModelAnalyzer extends AbstractXMLModelAnalyzer {
+
+	public static final String[] SELECTORE_SPLITER = new String[] { ":", "\\", "/" };
 
 	public XMLSourceModelAnalyzer() {
 		super("sourceDataPath");
 	}
-	
+
+	@Override
+	public Object buildSourceInputObjects(GraphInformations graphInfo,
+			SmooksResourceListType listType, IFile sourceFile)
+			throws InvocationTargetException {
+		List<AbstractResourceConfig> lists = listType
+				.getAbstractResourceConfig();
+		for (Iterator<AbstractResourceConfig> iterator = lists.iterator(); iterator
+				.hasNext();) {
+			ResourceConfigType resourceConfig = (ResourceConfigType) iterator
+					.next();
+			String selector = resourceConfig.getSelector();
+			checkBindingSelector(selector);
+			List<Object> list = SmooksModelUtils
+					.getBindingListFromResourceConfigType(resourceConfig);
+			if(list == null) continue;
+			for (Iterator<Object> iterator2 = list.iterator(); iterator2
+					.hasNext();) {
+				AnyType binding = (AnyType) iterator2.next();
+				String bindingMessage = SmooksModelUtils
+						.getAttributeValueFromAnyType(binding,
+								SmooksModelUtils.ATTRIBUTE_SELECTOR);
+				checkBindingSelector(bindingMessage);
+			}
+		}
+		return super.buildSourceInputObjects(graphInfo, listType, sourceFile);
+	}
+
+	protected void checkBindingSelector(String message)
+			throws InvocationTargetException {
+		if(message == null) return;
+		for (int i = 0; i < SELECTORE_SPLITER.length; i++) {
+			String splitString = SELECTORE_SPLITER[i];
+			if (message.indexOf(splitString) != -1) {
+				throw new InvocationTargetException(new Exception(
+						"[XML] The Selector string dosen't support \""
+								+ splitString + "\" character"));
+			}
+		}
+	}
+
 }
