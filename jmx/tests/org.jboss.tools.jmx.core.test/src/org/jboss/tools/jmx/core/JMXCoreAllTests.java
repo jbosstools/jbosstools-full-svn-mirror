f3dad48d9fd52caf60aaaf5a229e65b138dc1b08
Nick Boldt <nboldt@redhat.com>
2008-12-19 16:35:21 +0000

    [JBIDE-3401] backport from trunk
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.0.0.CR1@12728 a97e2381-89e5-4abb-bab3-167db6db766c

--- jmx/tests/org.jboss.tools.jmx.core.test/src/org/jboss/tools/jmx/core/JMXCoreAllTests.java
+++ jmx/tests/org.jboss.tools.jmx.core.test/src/org/jboss/tools/jmx/core/JMXCoreAllTests.java
@@ -2,15 +2,13 @@ package org.jboss.tools.jmx.core;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.jboss.tools.jmx.core.tests.DefaultMBeanServerConnectionFactoryTest;
 import org.jboss.tools.jmx.core.tests.DefaultProviderTest;
 import org.jboss.tools.jmx.core.tests.NodeBuilderTestCase;
 
 
-public class AllTests {
+public class JMXCoreAllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite();
-		suite.addTestSuite(DefaultMBeanServerConnectionFactoryTest.class);
 		suite.addTestSuite(DefaultProviderTest.class);
 		suite.addTestSuite(NodeBuilderTestCase.class);
 		return suite;
