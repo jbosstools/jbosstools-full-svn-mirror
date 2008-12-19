c2a1ff15176d0c8dc83800822b4db05777066932
Nick Boldt <nboldt@redhat.com>
2008-12-19 17:54:39 +0000

    tagging
    git-svn-id: http://svn.jboss.org/repos/jbosstools/tags/jbosstools-3.0.0.CR1@12740 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ jbpm/plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/editor/JpdlEditor.java
@@ -0,0 +1,45 @@
+package org.jboss.tools.flow.jpdl4.editor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.eclipse.gef.EditPartFactory;
+import org.eclipse.gef.palette.PaletteRoot;
+import org.jboss.tools.flow.common.editor.GenericModelEditor;
+import org.jboss.tools.flow.common.editpart.DefaultEditPartFactory;
+import org.jboss.tools.flow.common.registry.ElementRegistry;
+import org.jboss.tools.flow.common.wrapper.Wrapper;
+
+public class JpdlEditor extends GenericModelEditor {
+
+    protected EditPartFactory createEditPartFactory() {
+        return new DefaultEditPartFactory();
+    }
+
+    protected PaletteRoot createPalette() {
+        return new JpdlPaletteFactory().createPalette();
+    }
+
+    protected Object createModel() {
+        return ElementRegistry.createWrapper("org.jboss.tools.flow.jpdl4.process");
+    }
+    
+    protected void writeModel(OutputStream os) throws IOException {
+        Object object = getModel();
+        if (object instanceof Wrapper) {
+        	JpdlSerializer.serialize((Wrapper)object, os);
+        }
+    }
+    
+    protected void createModel(InputStream is) {
+    	boolean empty = true;
+    	try {
+    		empty = is.available() == 0;
+    	} catch (IOException e) {
+    		// ignored
+    	}
+    	setModel(empty ? createModel() : JpdlDeserializer.deserialize(is));
+    }
+    
+}
