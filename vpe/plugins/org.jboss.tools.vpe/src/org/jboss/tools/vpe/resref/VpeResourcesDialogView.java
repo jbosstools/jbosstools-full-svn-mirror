a57bafc1191532e785b6eeece878c8681be7e234
Maxim Areshkau <mareshkau@exadel.com>
2009-01-06 20:11:46 +0000

    =https://jira.jboss.org/jira/browse/JBIDE-3484, size was adjusted for windows
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@12909 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/resref/VpeResourcesDialogView.java
+++ vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/resref/VpeResourcesDialogView.java
@@ -111,7 +111,7 @@ public class VpeResourcesDialogView extends AbstractQueryWizardView {
 	// http://jira.jboss.com/jira/browse/JBIDE-2010
 	String os_name = System.getProperty("os.name"); //$NON-NLS-1$
 	if (os_name != null && os_name.indexOf("Windows") >= 0) { //$NON-NLS-1$
-	    return new Point(800, 230);
+	    return new Point(500, 230);
 	}
 	// changed by estherbin
 	// http://jira.jboss.com/jira/browse/JBIDE-2010
