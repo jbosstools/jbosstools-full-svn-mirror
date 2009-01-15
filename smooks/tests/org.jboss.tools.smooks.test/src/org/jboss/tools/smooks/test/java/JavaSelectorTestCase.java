840c4d95d187e6f6f0a7feaa79db62f179754aa7
DartPeng <dpeng@redhat.com>
2009-01-15 02:16:32 +0000

    JBIDE-3479
    Add a new test case for testing local the Javabean with "selector"
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@13048 a97e2381-89e5-4abb-bab3-167db6db766c

--- smooks/tests/org.jboss.tools.smooks.test/src/org/jboss/tools/smooks/test/java/JavaSelectorTestCase.java
+++ smooks/tests/org.jboss.tools.smooks.test/src/org/jboss/tools/smooks/test/java/JavaSelectorTestCase.java
@@ -6,9 +6,10 @@ package org.jboss.tools.smooks.test.java;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-import org.jboss.tools.smooks.javabean.analyzer.JavaBeanAnalyzer;
 import org.jboss.tools.smooks.javabean.model.JavaBeanModel;
 import org.jboss.tools.smooks.javabean.model.JavaBeanModelFactory;
+import org.jboss.tools.smooks.ui.IXMLStructuredObject;
+import org.jboss.tools.smooks.utils.UIUtils;
 
 /**
  * @author Dart
@@ -23,19 +24,16 @@ public class JavaSelectorTestCase extends TestCase {
 		JavaBeanModel model = JavaBeanModelFactory
 				.getJavaBeanModelWithLazyLoad(A.class);
 
-		JavaBeanModel aModel = JavaBeanAnalyzer.localJavaBeanModelWithSelector(
-				"a", model);
+		IXMLStructuredObject aModel = UIUtils.localXMLNodeWithPath("a", model);
 		Assert.assertNotNull(aModel);
-		Assert.assertEquals(aModel.getBeanClass(), A.class);
+		Assert.assertEquals(((JavaBeanModel) aModel).getBeanClass(), A.class);
 
-		JavaBeanModel nullModel = JavaBeanAnalyzer
-				.localJavaBeanModelWithSelectorWithoutException("a" + sperator
-						+ "null", model);
+		IXMLStructuredObject nullModel = UIUtils.localXMLNodeWithPath("a" + sperator
+				+ "null", model, null, false);
 		Assert.assertNull(nullModel);
 
 		// avoid died loop
-		nullModel = JavaBeanAnalyzer
-				.localJavaBeanModelWithSelectorWithoutException("null", model);
+		nullModel =   UIUtils.localXMLNodeWithPath("null", model, null, false);
 		Assert.assertNull(nullModel);
 	}
 }
