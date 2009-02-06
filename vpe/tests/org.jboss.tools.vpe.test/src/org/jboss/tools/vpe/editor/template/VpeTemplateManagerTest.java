a289bbb31e51d3c460956948bc95dd46cd382fe8
Maxim Areshkau <mareshkau@exadel.com>
2009-02-06 13:01:13 +0000

    https://jira.jboss.org/jira/browse/JBIDE-883, fixing errors in junit tests
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@13508 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/tests/org.jboss.tools.vpe.test/src/org/jboss/tools/vpe/editor/template/VpeTemplateManagerTest.java
+++ vpe/tests/org.jboss.tools.vpe.test/src/org/jboss/tools/vpe/editor/template/VpeTemplateManagerTest.java
@@ -1,12 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2009 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.vpe.editor.template;
 
+import java.util.Collections;
 import java.util.List;
 
 import junit.framework.TestCase;
 
+/**
+ * Test for Template Manager
+ * @author mareshkau
+ */
 public class VpeTemplateManagerTest extends TestCase {
 
+	private List<VpeAnyData> oldTemplates;
+	
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();		
+		oldTemplates = VpeTemplateManager.getInstance().getAnyTemplates();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		VpeTemplateManager.getInstance().setAnyTemplates(oldTemplates);
+		super.tearDown();	
+	}
+
 	public void testSetAnyTemplates() {
+		List<VpeAnyData> clearList = Collections.emptyList();
+		VpeTemplateManager.getInstance().setAnyTemplates(clearList);
 		VpeAnyData data = new VpeAnyData(
 				"tag-name1", //$NON-NLS-1$
 				"tag-value", //$NON-NLS-1$
@@ -22,9 +59,6 @@ public class VpeTemplateManagerTest extends TestCase {
 				);
 		
 		VpeTemplateManager.getInstance().setAnyTemplate(data);
-	}
-	
-	public void testGetAnyTemplates() {
 		List<VpeAnyData> templates = VpeTemplateManager.getInstance().getAnyTemplates();
 		assertEquals(2, templates.size());
 	}
