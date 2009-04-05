bcb6cedd08c02d1afba7e959e9229264d1909af0
Snjezana Peco <snjezana.peco@redhat.com>
2009-04-05 00:10:59 +0000

    JBIDE-4123 Migration to Eclipse 3.5 M Builds
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@14510 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ birt/plugins/org.jboss.tools.birt.oda/src/org/eclipse/datatools/connectivity/oda/spec/QuerySpecification.java
@@ -0,0 +1,10 @@
+package org.eclipse.datatools.connectivity.oda.spec;
+
+/**
+ * 
+ * A dummy interface to provide the compatibility with Galileo
+ * 
+ */
+public interface QuerySpecification {
+
+}
