761a10b163b364351e99773044b9f80242b6a11a
DartPeng <dpeng@redhat.com>
2009-04-14 09:10:36 +0000

    JBIDE-4171
    Delete useless codes
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/smooks@14729 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.smooks.core/src/org/jboss/tools/smooks/model/common/impl/CommonPackageImpl.java
+++ plugins/org.jboss.tools.smooks.core/src/org/jboss/tools/smooks/model/common/impl/CommonPackageImpl.java
@@ -17,8 +17,8 @@ import org.eclipse.emf.ecore.xml.type.XMLTypePackage;
 import org.jboss.tools.smooks.model.common.AbstractAnyType;
 import org.jboss.tools.smooks.model.common.CommonFactory;
 import org.jboss.tools.smooks.model.common.CommonPackage;
-import org.jboss.tools.smooks.model.edi.EdiPackage;
-import org.jboss.tools.smooks.model.edi.impl.EdiPackageImpl;
+import org.jboss.tools.smooks.model.medi.EdiPackage;
+import org.jboss.tools.smooks.model.medi.impl.EdiPackageImpl;
 
 
 
