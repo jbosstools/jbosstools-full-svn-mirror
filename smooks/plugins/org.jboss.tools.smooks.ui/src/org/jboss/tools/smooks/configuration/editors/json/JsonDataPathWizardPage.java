c1cf90a3083321cb3bf347d8fd2b79409cd524ef
DartPeng <dpeng@redhat.com>
2009-06-30 08:36:59 +0000

    JBIDE-4540
    Add json data wizard and wizard page
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16275 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ smooks/plugins/org.jboss.tools.smooks.ui/src/org/jboss/tools/smooks/configuration/editors/json/JsonDataPathWizardPage.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.smooks.configuration.editors.json;
+
+import java.util.List;
+
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.jboss.tools.smooks.configuration.editors.xml.AbstractFileSelectionWizardPage;
+
+/**
+ * @author Dart (dpeng@redhat.com)
+ *
+ */
+public class JsonDataPathWizardPage extends AbstractFileSelectionWizardPage {
+
+	public JsonDataPathWizardPage(String pageName, boolean multiSelect, Object[] initSelections,
+			List<ViewerFilter> filters) {
+		super(pageName, multiSelect, initSelections, filters);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JsonDataPathWizardPage(String pageName, String[] fileExtensionNames) {
+		super(pageName, fileExtensionNames);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jboss.tools.smooks.configuration.editors.xml.AbstractFileSelectionWizardPage#loadedTheObject(java.lang.String)
+	 */
+	@Override
+	protected Object loadedTheObject(String path) throws Exception {
+		return null;
+	}
+	
+}
---
+++ smooks/plugins/org.jboss.tools.smooks.ui/src/org/jboss/tools/smooks/configuration/editors/json/JsonDataWizard.java
@@ -0,0 +1,128 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.smooks.configuration.editors.json;
+
+import java.util.Properties;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.jboss.tools.smooks.configuration.editors.wizard.IStructuredDataSelectionWizard;
+
+/**
+ * @author Dart (dpeng@redhat.com)
+ * 
+ */
+public class JsonDataWizard extends Wizard implements IStructuredDataSelectionWizard, INewWizard {
+
+	private JsonDataPathWizardPage pathPage = null;
+
+	private JsonDataConfiguraitonWizardPage configPage = null;
+
+	public JsonDataWizard() {
+		super();
+		pathPage = new JsonDataPathWizardPage("Json Input Data Selection ", new String[]{});
+		configPage = new JsonDataConfiguraitonWizardPage("Json data configuration page");
+	}
+
+	
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+		this.addPage(pathPage);
+		this.addPage(configPage);
+	}
+
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
+	 * org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.jboss.tools.smooks.configuration.editors.wizard.
+	 * IStructuredDataSelectionWizard#getInputDataTypeID()
+	 */
+	public String getInputDataTypeID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.jboss.tools.smooks.configuration.editors.wizard.
+	 * IStructuredDataSelectionWizard#getProperties()
+	 */
+	public Properties getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.jboss.tools.smooks.configuration.editors.wizard.
+	 * IStructuredDataSelectionWizard#getReturnData()
+	 */
+	public Object getReturnData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.jboss.tools.smooks.configuration.editors.wizard.
+	 * IStructuredDataSelectionWizard#getStructuredDataSourcePath()
+	 */
+	public String getStructuredDataSourcePath() {
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.jboss.tools.smooks.configuration.editors.wizard.
+	 * IStructuredDataSelectionWizard#init(org.eclipse.ui.IEditorSite,
+	 * org.eclipse.ui.IEditorInput)
+	 */
+	public void init(IEditorSite site, IEditorInput input) {
+
+	}
+}
