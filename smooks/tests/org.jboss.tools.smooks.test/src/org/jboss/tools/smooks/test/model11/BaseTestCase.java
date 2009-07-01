06002139c557ef562979f0314031127b29ae3337
DartPeng <dpeng@redhat.com>
2009-07-01 08:19:58 +0000

    JBIDE-4217
    Add Json data parser unit test
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16312 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ smooks/tests/org.jboss.tools.smooks.test/src/org/jboss/tools/smooks/test/model11/BaseTestCase.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.jboss.tools.smooks.test.model11;
+
+import org.eclipse.emf.ecore.EPackage.Registry;
+import org.jboss.tools.smooks.model.calc.CalcPackage;
+import org.jboss.tools.smooks.model.common.CommonPackage;
+import org.jboss.tools.smooks.model.csv.CsvPackage;
+import org.jboss.tools.smooks.model.datasource.DatasourcePackage;
+import org.jboss.tools.smooks.model.dbrouting.DbroutingPackage;
+import org.jboss.tools.smooks.model.edi.EdiPackage;
+import org.jboss.tools.smooks.model.esbrouting.EsbroutingPackage;
+import org.jboss.tools.smooks.model.fileRouting.FileRoutingPackage;
+import org.jboss.tools.smooks.model.freemarker.FreemarkerPackage;
+import org.jboss.tools.smooks.model.graphics.ext.SmooksGraphicsExtPackage;
+import org.jboss.tools.smooks.model.groovy.GroovyPackage;
+import org.jboss.tools.smooks.model.iorouting.IoroutingPackage;
+import org.jboss.tools.smooks.model.javabean.JavabeanPackage;
+import org.jboss.tools.smooks.model.jmsrouting.JmsroutingPackage;
+import org.jboss.tools.smooks.model.json.JsonPackage;
+import org.jboss.tools.smooks.model.medi.MEdiPackage;
+import org.jboss.tools.smooks.model.smooks.SmooksPackage;
+import org.jboss.tools.smooks.model.xsl.XslPackage;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Dart
+ *
+ */
+public class BaseTestCase extends TestCase {
+	static {
+		// regist emf model uri mapping
+		Registry.INSTANCE.put(SmooksGraphicsExtPackage.eNS_URI, SmooksGraphicsExtPackage.eINSTANCE);
+		Registry.INSTANCE.put(SmooksPackage.eNS_URI, SmooksPackage.eINSTANCE);
+		Registry.INSTANCE.put(CalcPackage.eNS_URI, CalcPackage.eINSTANCE);
+		Registry.INSTANCE.put(CommonPackage.eNS_URI, CommonPackage.eINSTANCE);
+		Registry.INSTANCE.put(CsvPackage.eNS_URI, CsvPackage.eINSTANCE);
+		Registry.INSTANCE.put(DatasourcePackage.eNS_URI, DatasourcePackage.eINSTANCE);
+		Registry.INSTANCE.put(DbroutingPackage.eNS_URI, DbroutingPackage.eINSTANCE);
+		Registry.INSTANCE.put(EdiPackage.eNS_URI, EdiPackage.eINSTANCE);
+		Registry.INSTANCE.put(FileRoutingPackage.eNS_URI, FileRoutingPackage.eINSTANCE);
+
+		Registry.INSTANCE.put(FreemarkerPackage.eNS_URI, FreemarkerPackage.eINSTANCE);
+		Registry.INSTANCE.put(GroovyPackage.eNS_URI, GroovyPackage.eINSTANCE);
+		Registry.INSTANCE.put(IoroutingPackage.eNS_URI, IoroutingPackage.eINSTANCE);
+		Registry.INSTANCE.put(JavabeanPackage.eNS_URI, JavabeanPackage.eINSTANCE);
+		Registry.INSTANCE.put(JmsroutingPackage.eNS_URI, JmsroutingPackage.eINSTANCE);
+		Registry.INSTANCE.put(JsonPackage.eNS_URI, JsonPackage.eINSTANCE);
+		Registry.INSTANCE.put(MEdiPackage.eNS_URI, MEdiPackage.eINSTANCE);
+		Registry.INSTANCE.put(XslPackage.eNS_URI, XslPackage.eINSTANCE);
+		Registry.INSTANCE.put(EsbroutingPackage.eNS_URI, EsbroutingPackage.eINSTANCE);
+		Registry.INSTANCE.put(org.jboss.tools.smooks10.model.smooks.SmooksPackage.eNS_URI,
+				org.jboss.tools.smooks10.model.smooks.SmooksPackage.eINSTANCE);
+	}
+	public void testNull(){//ignore
+	}
+}
