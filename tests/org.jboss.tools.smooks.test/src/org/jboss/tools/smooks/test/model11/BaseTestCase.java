7795adb32bcd69bb3e14da1d2e769decfd7553a7
DartPeng <dpeng@redhat.com>
2009-07-24 08:37:43 +0000

    JBIDE-4217
    Add new Smooks 1.2 extension configuration file.
    
    
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk/smooks@16761 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.smooks.test/src/org/jboss/tools/smooks/test/model11/BaseTestCase.java
+++ tests/org.jboss.tools.smooks.test/src/org/jboss/tools/smooks/test/model11/BaseTestCase.java
@@ -9,9 +9,11 @@ import org.eclipse.emf.ecore.EPackage.Registry;
 import org.jboss.tools.smooks.model.calc.CalcPackage;
 import org.jboss.tools.smooks.model.common.CommonPackage;
 import org.jboss.tools.smooks.model.csv.CsvPackage;
+import org.jboss.tools.smooks.model.csv12.Csv12Package;
 import org.jboss.tools.smooks.model.datasource.DatasourcePackage;
 import org.jboss.tools.smooks.model.dbrouting.DbroutingPackage;
 import org.jboss.tools.smooks.model.edi.EdiPackage;
+import org.jboss.tools.smooks.model.edi12.Edi12Package;
 import org.jboss.tools.smooks.model.esbrouting.EsbroutingPackage;
 import org.jboss.tools.smooks.model.fileRouting.FileRoutingPackage;
 import org.jboss.tools.smooks.model.freemarker.FreemarkerPackage;
@@ -19,10 +21,16 @@ import org.jboss.tools.smooks.model.graphics.ext.GraphPackage;
 import org.jboss.tools.smooks.model.groovy.GroovyPackage;
 import org.jboss.tools.smooks.model.iorouting.IoroutingPackage;
 import org.jboss.tools.smooks.model.javabean.JavabeanPackage;
+import org.jboss.tools.smooks.model.javabean12.Javabean12Package;
 import org.jboss.tools.smooks.model.jmsrouting.JmsroutingPackage;
+import org.jboss.tools.smooks.model.jmsrouting12.Jmsrouting12Package;
 import org.jboss.tools.smooks.model.json.JsonPackage;
+import org.jboss.tools.smooks.model.json12.Json12Package;
 import org.jboss.tools.smooks.model.medi.MEdiPackage;
+import org.jboss.tools.smooks.model.persistence12.Persistence12Package;
+import org.jboss.tools.smooks.model.rules10.Rules10Package;
 import org.jboss.tools.smooks.model.smooks.SmooksPackage;
+import org.jboss.tools.smooks.model.validation10.Validation10Package;
 import org.jboss.tools.smooks.model.xsl.XslPackage;
 
 /**
@@ -31,7 +39,18 @@ import org.jboss.tools.smooks.model.xsl.XslPackage;
  */
 public class BaseTestCase extends TestCase {
 	static {
-		// regist emf model uri mapping
+		// regist emf model uri mapping 
+		
+		// smooks 1.2
+		Registry.INSTANCE.put(Javabean12Package.eNS_URI, Javabean12Package.eINSTANCE);
+		Registry.INSTANCE.put(Json12Package.eNS_URI, Json12Package.eINSTANCE);
+		Registry.INSTANCE.put(Jmsrouting12Package.eNS_URI, Jmsrouting12Package.eINSTANCE);
+		Registry.INSTANCE.put(Validation10Package.eNS_URI, Validation10Package.eINSTANCE);
+		Registry.INSTANCE.put(Rules10Package.eNS_URI, Rules10Package.eINSTANCE);
+		Registry.INSTANCE.put(Persistence12Package.eNS_URI, Persistence12Package.eINSTANCE);
+		Registry.INSTANCE.put(Csv12Package.eNS_URI, Csv12Package.eINSTANCE);
+		Registry.INSTANCE.put(Edi12Package.eNS_URI, Edi12Package.eINSTANCE);
+		// smooks 1.1.2 and smooks 1.0
 		Registry.INSTANCE.put(GraphPackage.eNS_URI, GraphPackage.eINSTANCE);
 		Registry.INSTANCE.put(SmooksPackage.eNS_URI, SmooksPackage.eINSTANCE);
 		Registry.INSTANCE.put(CalcPackage.eNS_URI, CalcPackage.eINSTANCE);
