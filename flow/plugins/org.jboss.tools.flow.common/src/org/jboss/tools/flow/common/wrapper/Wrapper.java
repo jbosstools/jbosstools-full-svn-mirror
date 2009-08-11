757bfe77024817c9649b0641bb7249e783d48ccc
Koen Aers <koen.aers@jboss.com>
2009-08-11 09:46:12 +0000

    removed comments
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17043 a97e2381-89e5-4abb-bab3-167db6db766c

--- flow/plugins/org.jboss.tools.flow.common/src/org/jboss/tools/flow/common/wrapper/Wrapper.java
+++ flow/plugins/org.jboss.tools.flow.common/src/org/jboss/tools/flow/common/wrapper/Wrapper.java
@@ -9,10 +9,6 @@ import org.jboss.tools.flow.common.model.Element;
 
 public interface Wrapper extends IAdaptable, IPropertySource, Element {
 	
-//	int ADD_INCOMING_CONNECTION = 1;
-//	int REMOVE_INCOMING_CONNECTION = 2;
-//	int ADD_OUTGOING_CONNECTION = 3;
-//	int REMOVE_OUTGOING_CONNECTION = 4;
 	int CHANGE_VISUAL = 5;
 	int ADD_ELEMENT = 6;
 	int REMOVE_ELEMENT = 7;
