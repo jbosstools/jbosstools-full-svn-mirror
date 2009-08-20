4ab8ebfda24c920aa5e5a43273776aa7656fa2e0
Denis Maliarevich <dmaliarevich@exadel.com>
2009-08-20 14:35:00 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4547, simple junit for VpeResourcesDialog was added.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17188 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/dialog/VpeResourcesDialogTest.java
+++ vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/dialog/VpeResourcesDialogTest.java
@@ -9,57 +9,40 @@
  ******************************************************************************/
 package org.jboss.tools.vpe.ui.test.dialog;
 
-//import java.util.Properties;
-
-//import org.eclipse.core.resources.IFile;
-//import org.eclipse.swt.widgets.Shell;
-//import org.jboss.tools.common.model.options.PreferenceModelUtilities;
-//import org.jboss.tools.common.model.ui.ModelUIPlugin;
-//import org.jboss.tools.common.model.ui.wizards.query.AbstractQueryDialog;
-//import org.jboss.tools.common.model.ui.wizards.query.IQueryDialog;
-//import org.jboss.tools.vpe.resref.VpeResourcesDialog;
-//import org.jboss.tools.vpe.ui.test.TestUtil;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.ui.PlatformUI;
+import org.jboss.tools.vpe.resref.core.VpeResourcesDialog;
+import org.jboss.tools.vpe.ui.test.TestUtil;
 import org.jboss.tools.vpe.ui.test.VpeTest;
-//import org.jboss.tools.vpe.ui.test.VpeUiTests;
+import org.jboss.tools.vpe.ui.test.VpeUiTests;
 
 public class VpeResourcesDialogTest extends VpeTest {
-    
-    private final String FILE_NAME = "hello.jsp"; //$NON-NLS-1$
-    
-    public VpeResourcesDialogTest(String name) {
-	super(name);
-    }
 
-//    public void _testVpeResourcesDialogOpen() throws Throwable {
-//        IFile file = (IFile) TestUtil.getComponentPath(FILE_NAME,
-//        	VpeUiTests.IMPORT_PROJECT_NAME);
-//        
-//        assertNotNull("Specified file does not exist: fileName = " + FILE_NAME  //$NON-NLS-1$
-//		+ "; projectName = " + VpeUiTests.IMPORT_PROJECT_NAME, file); //$NON-NLS-1$
-//  
-//        VpeResourcesDialog dialog = new VpeResourcesDialog();
-//	Properties p = new Properties();
-//	p.setProperty("help", "VpeResourcesDialog"); //$NON-NLS-1$ //$NON-NLS-2$
-//	p.put("file", file); //$NON-NLS-1$
-//	p.put("model", PreferenceModelUtilities.getPreferenceModel()); //$NON-NLS-1$
-//	dialog.setObject(p);
-//	Shell shell = ModelUIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getShell();
-//	IQueryDialog dialogWindow = new AbstractQueryDialog(shell);
-//	
-//	dialogWindow.setView(dialog.getView());
-//	dialogWindow.getDialog().create();
-//	dialog.getView().setDialog(dialogWindow.getDialog());
-//	dialogWindow.getDialog().setBlockOnOpen(false);
-//	dialogWindow.getDialog().open();
-//	int code = dialogWindow.getDialog().getReturnCode();
-//	
-//	/*
-//	 * Assert that window has been created.
-//	 */
-//	assertEquals(0, code);
-//	
-//	dialogWindow.getDialog().close();
-//    }
-    
-    
+	private final String FILE_NAME = "hello.jsp"; //$NON-NLS-1$
+
+	public VpeResourcesDialogTest(String name) {
+		super(name);
+	}
+
+	public void testVpeResourcesDialogOpen() throws Throwable {
+		IFile file = (IFile) TestUtil.getComponentPath(FILE_NAME,
+				VpeUiTests.IMPORT_PROJECT_NAME);
+
+		assertNotNull("Specified file does not exist: fileName = " + FILE_NAME //$NON-NLS-1$
+				+ "; projectName = " + VpeUiTests.IMPORT_PROJECT_NAME, file); //$NON-NLS-1$
+
+		VpeResourcesDialog dialog = new VpeResourcesDialog(PlatformUI
+				.getWorkbench().getDisplay().getActiveShell(), file);
+
+		dialog.setBlockOnOpen(false);
+		int code = dialog.open();
+
+		/*
+		 * Assert that window has been created.
+		 */
+		assertEquals(0, code);
+
+		dialog.close();
+	}
+
 }
