07c3c191593a630000ec1410fb03bdfed6dd2bf8
Denis Golovin <dgolovin@exadel.com>
2009-09-02 17:02:54 +0000

    fix for
    NullPointerException in BPEL ValidationHelper
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17419 a97e2381-89e5-4abb-bab3-167db6db766c

--- bpel/plugins/org.eclipse.bpel.validator/src/org/eclipse/bpel/validator/ValidatorHelper.java
+++ bpel/plugins/org.eclipse.bpel.validator/src/org/eclipse/bpel/validator/ValidatorHelper.java
@@ -10,10 +10,15 @@
  *******************************************************************************/
 package org.eclipse.bpel.validator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.wst.validation.internal.operations.WorkbenchContext;
 import org.eclipse.wst.validation.internal.operations.WorkbenchReporter;
 
@@ -26,7 +31,14 @@ public class ValidatorHelper extends WorkbenchContext {
 
 	@Override
 	public void registerResource(IResource resource) {
-		getValidationFileURIs().add(resource.getFullPath().toOSString());		
+		if(resource!=null) {
+			if(getValidationFileURIs()==null) {
+				setValidationFileURIs(new ArrayList<String>());
+			}
+			getValidationFileURIs().add(resource.getFullPath().toOSString());
+		} else {
+			Activator.getDefault().getLog().log(new Status(IStatus.WARNING,Activator.PLUGIN_ID,"Cannot register null resource for validation"));
+		}
 	}
 
 }
