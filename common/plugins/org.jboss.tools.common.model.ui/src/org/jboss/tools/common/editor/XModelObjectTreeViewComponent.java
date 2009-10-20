14d571bd6b842b09d04797d7810985ecf0fb27b4
Denis Golovin <dgolovin@exadel.com>
2009-10-20 23:00:15 +0000

    The exceptions in JUnit tests were fixed: Caused by: java.lang.NullPointerException      [java] 	at org.jboss.tools.common.editor.XModelObjectTreeViewComponent$4.run(XModelObjectTreeViewComponent.java:134)      [java] 	at org.eclipse.swt.widgets.RunnableLock.run(RunnableLock.java:35)      [java] 	at org.eclipse.swt.widgets.Synchronizer.runAsyncMessages(Synchronizer.java:134)      [java] 	... 28 more
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.1.0.M4@18189 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/editor/XModelObjectTreeViewComponent.java
+++ common/plugins/org.jboss.tools.common.model.ui/src/org/jboss/tools/common/editor/XModelObjectTreeViewComponent.java
@@ -128,13 +128,13 @@ public class XModelObjectTreeViewComponent {
 		final TreeViewer treeViewer = getViewer();
 		Menu menu = menuMgr.createContextMenu(treeViewer.getTree());
 		treeViewer.getTree().setMenu(menu);
-		Display.getDefault().asyncExec(new Runnable() {
-			public void run() {
-				ModelUIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow()
-				.getActivePage().getActiveEditor().getSite()
-				.registerContextMenu(menuMgr, treeViewer);
-			}
-		});
+//		Display.getDefault().asyncExec(new Runnable() {
+//			public void run() {
+//				ModelUIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow()
+//				.getActivePage().getActiveEditor().getSite()
+//				.registerContextMenu(menuMgr, treeViewer);
+//			}
+//		});
 
 	}
 	protected void fillContextMenu(IMenuManager menu) {
