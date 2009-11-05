8877e4b6582312c5511652b64a78f0ffb570ea02
Yura Zhishko <yzhishko@exadel.com>
2009-11-05 16:48:41 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18421 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/proxy/VpeProxyUtil.java
+++ vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/proxy/VpeProxyUtil.java
@@ -13,6 +13,9 @@ package org.jboss.tools.vpe.editor.proxy;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.List;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMAttr;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMElement;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
 import org.jboss.tools.vpe.editor.context.VpePageContext;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
@@ -20,42 +23,58 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 /**
- * Utill Class created for creating proxis for vpe 
- * and manipulation them
+ * Utill Class created for creating proxis for vpe and manipulation them
  * 
  * @author mareshkau
  */
+@SuppressWarnings("restriction")
 public class VpeProxyUtil {
 
 	/**
 	 * Create proxy for source nodes. This function should be called for nodes,
 	 * in which used el expressions which should be replaced
+	 * 
 	 * @param sourceNode
 	 * @return proxy for node
 	 */
-	public static Node createProxyForELExpressionNode(VpePageContext pageContext,
-			Node sourceNode) {
-	
+	public static Node createProxyForELExpressionNode(
+			VpePageContext pageContext, Node sourceNode) {
+
 		Node proxy = null;
 		List<Class<?>> interfaceses = new ArrayList<Class<?>>();
 		interfaceses.add(Node.class);
-		if(sourceNode instanceof Element) {
-				interfaceses.add(Element.class);		
-		} else if(sourceNode instanceof Attr) {
+		boolean isDOMImpl = false;
+		if (sourceNode instanceof IDOMElement) {
+			interfaceses.add(IDOMElement.class);
+			isDOMImpl = true;
+		} else if (sourceNode instanceof IDOMAttr) {
+			interfaceses.add(IDOMAttr.class);
+			isDOMImpl = true;
+		}
+		if (isDOMImpl) {
+			proxy = (Node) Proxy.newProxyInstance(IDOMNode.class
+					.getClassLoader(), interfaceses.toArray(new Class[0]),
+					new VpeNodeInvocationHandler(pageContext, sourceNode));
+		} else {
+			if (sourceNode instanceof Element) {
+				interfaceses.add(Element.class);
+			} else if (sourceNode instanceof Attr) {
 				interfaceses.add(Attr.class);
-		} 
-		proxy = (Node) Proxy.newProxyInstance(Node.class.getClassLoader(),
-				  interfaceses.toArray(new Class[0]),
-				  new VpeNodeInvocationHandler(pageContext,sourceNode));
-
+			}
+			proxy = (Node) Proxy.newProxyInstance(Node.class.getClassLoader(),
+					interfaceses.toArray(new Class[0]),
+					new VpeNodeInvocationHandler(pageContext, sourceNode));
+		}
 		return proxy;
 	}
-	
-	public static NamedNodeMap createProxyForNamedNodeMap(VpePageContext pageContext,
-			NamedNodeMap namedNodeMap) {
-		NamedNodeMap proxy =(NamedNodeMap)Proxy.newProxyInstance(NamedNodeMap.class.getClassLoader(),
-				 new Class[]  {NamedNodeMap.class},
-				  new VpeNamedNodeMapInvocationHandler(pageContext,namedNodeMap));
+
+	public static NamedNodeMap createProxyForNamedNodeMap(
+			VpePageContext pageContext, NamedNodeMap namedNodeMap) {
+		NamedNodeMap proxy = (NamedNodeMap) Proxy
+				.newProxyInstance(NamedNodeMap.class.getClassLoader(),
+						new Class[] { NamedNodeMap.class },
+						new VpeNamedNodeMapInvocationHandler(pageContext,
+								namedNodeMap));
 
 		return proxy;
 	}
