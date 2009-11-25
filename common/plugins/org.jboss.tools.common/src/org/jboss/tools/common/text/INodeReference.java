795dddc8711f1fad7e6a0d8b2de899b50d26c042
Alexey Kazakov <akazakov@exadel.com>
2009-11-25 08:21:44 +0000

    https://jira.jboss.org/jira/browse/JBIDE-4943
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18815 a97e2381-89e5-4abb-bab3-167db6db766c

--- common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/text/INodeReference.java
+++ common/plugins/org.jboss.tools.common/src/org/jboss/tools/common/text/INodeReference.java
@@ -8,31 +8,32 @@
  * Contributors: 
  * Red Hat, Inc. - initial API and implementation 
  ******************************************************************************/
-package org.jboss.tools.cdi.core;
+package org.jboss.tools.common.text;
 
-import org.jboss.tools.common.text.ITextSourceReference;
 
 /**
- * Represents a reference to XML element. For example there is
+ * Represents a reference to XML node. For example there is
  * <class>
  * 		org.mycompany.Logger
  * </class>
- * then getBody() will return "org.mycompany.Logger"
+ * then getValue() will return "org.mycompany.Logger"
  * and getStartPosition() will return the offset of the trimmed value (the index of "o")
  * and getLength() will return the length of "org.mycompany.Logger" string.
  * 
  * @author Alexey Kazakov
  */
-public interface IXmlElementReference extends ITextSourceReference {
+public interface INodeReference extends ITextSourceReference {
 
 	/**
-	 * Returns the trimmed string representation of the body of this XML
-	 * element. For example if this object is reference to <class>
-	 * org.mycompany.Logger </class> then this method will return
-	 * "org.mycompany.Logger".
+	 * Returns the trimmed string representation of the value of the
+	 * node. For example if this object is reference to
+	 * <class>
+	 *     org.mycompany.Logger
+	 * </class>
+	 * then this method will return "org.mycompany.Logger".
 	 * 
 	 * @return the trimmed string representation of the body of this XML
 	 *         element.
 	 */
-	String getBody();
+	String getValue();
 }
