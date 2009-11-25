9c8b759427928959667b4c5a48f93c0fd1566ec1
Maxim Areshkau <mareshkau@exadel.com>
2009-11-25 13:53:01 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5184
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@18823 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/mozilla/XulRunnerPreview.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2009 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.vpe.editor.mozilla;
+
+import org.eclipse.swt.widgets.Composite;
+import org.jboss.tools.vpe.xulrunner.XulRunnerException;
+import org.jboss.tools.vpe.xulrunner.editor.XulRunnerEditor;
+
+public class XulRunnerPreview extends XulRunnerEditor {
+
+	private MozillaPreview mozillaPreview;
+
+	public XulRunnerPreview(Composite parent, MozillaPreview mozillaPreview) throws XulRunnerException {
+		super(parent);
+		this.mozillaPreview = mozillaPreview;
+	}
+
+	public void onLoadWindow() {
+		super.onLoadWindow();
+		mozillaPreview.onLoadWindow();
+	}
+	
+	public void onShowTooltip(int x, int y, String text) {
+		if (mozillaPreview != null && mozillaPreview.getEditorDomEventListener() != null) {
+			mozillaPreview.getEditorDomEventListener().onShowTooltip(x, y, text);
+		}
+	}
+	public void onHideTooltip() {
+		if (mozillaPreview != null && mozillaPreview.getEditorDomEventListener() != null) {
+			mozillaPreview.getEditorDomEventListener().onHideTooltip();
+		}
+	}
+	
+	public void onDispose() {
+		if (mozillaPreview != null) {
+			mozillaPreview.removeDomEventListeners();
+			mozillaPreview = null;
+		}
+		super.onDispose();
+	}
+}
