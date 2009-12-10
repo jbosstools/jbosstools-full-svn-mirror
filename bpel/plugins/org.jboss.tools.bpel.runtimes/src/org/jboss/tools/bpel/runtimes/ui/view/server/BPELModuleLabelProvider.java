af8350b4d8c6b8d416fad83b52fe68a427b9851a
Denny Xu <dxu@redhat.com>
2009-12-10 10:50:45 +0000

    JBIDE-5363: Change the BPEL version number to be format: yyyymmddHHMMSS
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19180 a97e2381-89e5-4abb-bab3-167db6db766c

--- bpel/plugins/org.jboss.tools.bpel.runtimes/src/org/jboss/tools/bpel/runtimes/ui/view/server/BPELModuleLabelProvider.java
+++ bpel/plugins/org.jboss.tools.bpel.runtimes/src/org/jboss/tools/bpel/runtimes/ui/view/server/BPELModuleLabelProvider.java
@@ -3,12 +3,14 @@ package org.jboss.tools.bpel.runtimes.ui.view.server;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
+import org.jboss.tools.bpel.runtimes.IRuntimesUIConstants;
+import org.jboss.tools.bpel.runtimes.RuntimesPlugin;
 import org.jboss.tools.bpel.runtimes.ui.view.server.BPELModuleContentProvider.BPELVersionDeployment;
 
 public class BPELModuleLabelProvider extends LabelProvider {
 	public Image getImage(Object element) {
-		// TODO Add an icon 
-		return null;
+		
+		return RuntimesPlugin.getPlugin().getImage(IRuntimesUIConstants.ICON_BPEL_FACET);
 	}
 
 	public String getText(Object element) {
