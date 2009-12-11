62b3b9847325f15ab752a09598082f52035581ac
Tom Fennelly <tom.fennelly@jboss.com>
2009-12-11 15:55:41 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5205 Create FreeMarker Template builder for generating XML messages (from XSD)
    Added templating bundle
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19214 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ smooks/plugins/org.jboss.tools.smooks.templating/src/org/jboss/tools/smooks/templating/model/ModelBuilder.java
@@ -0,0 +1,237 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, JBoss Inc., and others contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ * (C) 2005-2006, JBoss Inc.
+ */
+package org.jboss.tools.smooks.templating.model;
+
+import java.util.Properties;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.milyn.xml.DomUtils;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Abstract message model builder.
+ * 
+ * @author <a href="mailto:tom.fennelly@jboss.com">tom.fennelly@jboss.com</a>
+ */
+public abstract class ModelBuilder {
+    
+    public static final String NAMESPACE = "http://www.jboss.org/xsd/tools/smooks"; // TODO: Make same as JBT eclipse extension namespace ?? //$NON-NLS-1$
+    public static final String REQUIRED = "#required"; //$NON-NLS-1$
+    public static final String OPTIONAL = "#optional"; //$NON-NLS-1$
+
+    private static final String HIDDEN_ELEMENT = "hidden"; //$NON-NLS-1$
+
+    public static enum ElementType {
+    	simple,
+    	complex
+    }
+    
+    private Properties namespaces = new Properties();
+    
+    /**
+     * Build the message model.
+     * @return The message model.
+     * @throws ModelBuilderException Error building model.
+     */
+    public abstract Document buildModel() throws ModelBuilderException;
+
+    /**
+     * Get the namespace prefix-to-URI mappings for this model.
+     * @return The namespace prefix-to-URI mappings for this model.
+     */
+    public Properties getNamespaces() {
+		return namespaces;
+	}
+
+
+
+	/**
+     * Mark a fragment as being hidden.
+     * <p/>
+     * When hidden, it is illegal to attempt a mapping onto a fragment.
+     *
+     * @param fragment The fragment to be marked as hidden.
+     */
+    public static void hideFragment(Element fragment) {
+        fragment.setAttributeNS(NAMESPACE, HIDDEN_ELEMENT, "true"); //$NON-NLS-1$
+    }
+
+    /**
+     * Unmark a fragment as being hidden.
+     * <p/>
+     * When hidden, it is illegal to attempt a mapping onto a fragment.
+     *
+     * @param fragment The fragment to be unmarked as hidden.
+     */
+    public static void unhideFragment(Element fragment) {
+        fragment.removeAttributeNS(NAMESPACE, HIDDEN_ELEMENT);
+    }
+
+    /**
+     * Is the specified node marked as being hidden.
+     * <p/>
+     * When hidden, it is illegal to attempt a mapping onto a fragment.
+     *
+     * @return True if the node is hidden, otherwise false.
+     */
+    public static boolean isHidden(Node node) {
+        if(node != null) {
+            switch(node.getNodeType()) {
+                case Node.ATTRIBUTE_NODE :
+                    return isHidden(node.getParentNode());
+                case Node.ELEMENT_NODE :
+                    if(((Element)node).getAttributeNS(NAMESPACE, HIDDEN_ELEMENT).equals("true")) { //$NON-NLS-1$
+                        return true;
+                    }
+                    return isHidden(node.getParentNode());
+            }
+        }
+        
+        return false;
+    }
+
+    /**
+     * Is the specified node required.
+     * <p/>
+     * For an Element node, this means does it have a minOccurs > 0.  For an Attr node,
+     * it means is its value set to "#REQUIRED".
+     *
+     * @return True if the node is hidden, otherwise false.
+     */
+    public static boolean isRequired(Node node) {
+        if(node != null) {
+            switch(node.getNodeType()) {
+                case Node.ATTRIBUTE_NODE :
+                    return (REQUIRED.equals(node.getTextContent()) || XMLConstants.XML_NS_URI.equals(node.getNamespaceURI()));
+                case Node.ELEMENT_NODE :
+                    if(getMinOccurs((Element) node) > 0) {
+                        return true;
+                    }
+            }
+        }
+        
+        return false;
+    }
+
+    /**
+     * Is the specified node in the reserved namespace.
+     *
+     * @return True if the node is in the reserved namespace, otherwise false.
+     */
+    public static boolean isInReservedNamespace(Node node) {
+        if(node != null) {
+            return NAMESPACE.equals(node.getNamespaceURI());
+        }
+        
+        return false;
+    }
+
+    protected Document createModelInstance() throws ModelBuilderException {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+
+        try {
+            builder = builderFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new ModelBuilderException("Error constructing DOM DocumentBuilder.", e); //$NON-NLS-1$
+        }
+
+        return builder.newDocument();
+    }
+
+    protected void setMinMax(Element element, int minOccurs, int maxOccurs) {
+        element.setAttributeNS(NAMESPACE, "smk:minOccurs", Integer.toString(minOccurs)); //$NON-NLS-1$
+        element.setAttributeNS(NAMESPACE, "smk:maxOccurs", Integer.toString(maxOccurs)); //$NON-NLS-1$
+    }
+
+    public static Node getParentNode(Node node) {
+		if(node.getNodeType() == Node.ATTRIBUTE_NODE) {
+			return ((Attr)node).getOwnerElement();
+		} else {
+			return node.getParentNode();
+		}
+	}
+
+	public static int getMinOccurs(Element element) {
+        String minOccurs = element.getAttributeNS(NAMESPACE, "minOccurs"); //$NON-NLS-1$
+
+        if(minOccurs.equals("")) {
+            return 1;
+        }
+
+        return Integer.parseInt(minOccurs);
+    }
+
+    public static int getMaxOccurs(Element element) {
+        String maxOccurs = element.getAttributeNS(NAMESPACE, "maxOccurs"); //$NON-NLS-1$
+
+        if(maxOccurs.equals("")) {
+            return 1;
+        }
+
+        return Integer.parseInt(maxOccurs);
+    }
+    
+    public static void setElementType(Element element, ElementType type) {
+        element.setAttributeNS(NAMESPACE, "smk:elementType", type.toString()); //$NON-NLS-1$    	
+    }
+    
+    public static ElementType getElementType(Element element) {
+        String elementType = element.getAttributeNS(NAMESPACE, "elementType"); //$NON-NLS-1$
+        
+        if(elementType == null || elementType.length() == 0) {
+        	return null;
+        }
+
+        return ElementType.valueOf(elementType);
+    }
+
+	/**
+	 * Create a compositor Element.
+	 * @param document Owner Document node.
+	 * @return The compositor Element.
+	 */
+	public static Element createCompositor(Document document) {
+		return document.createElementNS(NAMESPACE, "smk:compositor"); //$NON-NLS-1$
+	}
+
+	/**
+	 * Is the supplied DOM node the compositor Element.
+	 * @param node The DOM node to test.
+	 * @return True if the node is the compositor element, otherwise false.
+	 */
+	public static boolean isCompositor(Node node) {
+		if(node.getNodeType() == Node.ELEMENT_NODE) {
+			if(DomUtils.getName((Element)node).equals("compositor")) { //$NON-NLS-1$
+				if(NAMESPACE.equals(node.getNamespaceURI())) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ smooks/plugins/org.jboss.tools.smooks.templating/src/org/jboss/tools/smooks/templating/template/csv/CSVFreeMarkerTemplateBuilder.java
@@ -0,0 +1,178 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, JBoss Inc., and others contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ * (C) 2005-2006, JBoss Inc.
+ */
+package org.jboss.tools.smooks.templating.template.csv;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Enumeration;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.jboss.tools.smooks.templating.model.ModelBuilder;
+import org.jboss.tools.smooks.templating.model.ModelBuilderException;
+import org.jboss.tools.smooks.templating.template.*;
+import org.jboss.tools.smooks.templating.template.exception.TemplateBuilderException;
+import org.jboss.tools.smooks.templating.template.exception.UnmappedCollectionNodeException;
+import org.jboss.tools.smooks.templating.template.util.FreeMarkerUtil;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import freemarker.core.TemplateElement;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+/**
+ * Freemarker Template Builder for a CSV messages.
+ *
+ * @author <a href="mailto:tom.fennelly@jboss.com">tom.fennelly@jboss.com</a>
+ */
+public class CSVFreeMarkerTemplateBuilder extends TemplateBuilder {
+
+    private char separatorChar;
+    private char quoteChar;    
+
+    /**
+     * Construct a new FreeMarker Template Builder with no mappings.
+     * @param model The model.
+     * @param separatorChar The CSV field separator character.
+     * @param quoteChar The CSV field quote character.
+     * @throws ModelBuilderException Error building model.
+     */
+    public CSVFreeMarkerTemplateBuilder(ModelBuilder modelBuilder, char separatorChar, char quoteChar) throws ModelBuilderException {
+        super(modelBuilder);
+        this.separatorChar = separatorChar;
+        this.quoteChar = quoteChar;
+    }
+
+    /**
+     * Construct a new FreeMarker Template Builder, extracting the mappings from the
+     * supplied FreeMarker template String.
+     * @param model The model.
+     * @param separatorChar The CSV field separator character.
+     * @param quoteChar The CSV field quote character.
+     * @param ftlTemplate The FreeMarker Template from which the mappings are to be extracted.
+     * @throws ModelBuilderException Error building model.
+     * @throws TemplateBuilderException Error adding mappings extracted from template. 
+     */
+    public CSVFreeMarkerTemplateBuilder(ModelBuilder modelBuilder, char separatorChar, char quoteChar, String ftlTemplate) throws ModelBuilderException, TemplateBuilderException {
+        this(modelBuilder, separatorChar, quoteChar);
+        addMappings(ftlTemplate);
+    }
+
+    private void addMappings(String ftlTemplate) throws TemplateBuilderException {
+    	Template template;
+    	
+    	try {
+			template = new Template("csvTemplate", new StringReader(ftlTemplate), new Configuration()); //$NON-NLS-1$
+		} catch (IOException e) {
+			throw new TemplateBuilderException ("Failed to parse the Supplied FreeMarker template.", e); //$NON-NLS-1$
+		}
+
+		TemplateElement listElement = template.getRootTreeNode();
+		if(!listElement.getNodeName().equals("IteratorBlock") || !listElement.getDescription().startsWith("list")) { //$NON-NLS-1$
+			throw new TemplateBuilderException ("Unable to recognize template as being a CSV template.  Expecting first template token to be a 'list' IteratorBlock node."); //$NON-NLS-1$
+		}
+		
+		// Add the mapping for the list itself...
+		addCSVListMapping(listElement.getDescription());
+		
+		// Add the mappings for the individual fields...
+		addCSVFieldMappings(listElement);
+    }
+
+	private void addCSVListMapping(String description) throws TemplateBuilderException {
+		String[] tokens = description.split(" +?"); //$NON-NLS-1$
+		Element csvRecordElement = getModel().getDocumentElement();
+		
+		// 2nd and 4th tokens contain the info we're looking for e.g. "list message.people as person"
+		addCollectionMapping(tokens[1], csvRecordElement, tokens[3]);
+	}
+
+	private void addCSVFieldMappings(TemplateElement listElement) throws TemplateBuilderException {
+		StringBuilder parseBuffer = new StringBuilder();
+		Enumeration children = listElement.children();
+		
+		while(children != null && children.hasMoreElements()) {
+			TemplateElement child = (TemplateElement) children.nextElement();
+			parseBuffer.append(child.getCanonicalForm());
+		}
+		
+		Element csvRecordElement = getModel().getDocumentElement();
+		CSVReader csvReader = new CSVReader(new StringReader(parseBuffer.toString()), separatorChar, quoteChar);
+		try {
+			String[] fields = csvReader.readNext();
+			int fieldIndex = 0;
+			NodeList csvFieldModelNodes = csvRecordElement.getChildNodes();
+			
+			for(int i = 0; i < csvFieldModelNodes.getLength(); i++) {
+				Node node = csvFieldModelNodes.item(i);
+				if(node.getNodeType() == Node.ELEMENT_NODE) {
+					if(fieldIndex >= fields.length) {
+						throw new TemplateBuilderException("CSV Template fieldset size does not match that of the specified message model.  Check the supplied fieldset.  Check the specified 'separator' and 'quote' characters match those used in the template."); //$NON-NLS-1$
+					}
+					
+					if(FreeMarkerUtil.isDollarVariable(fields[fieldIndex])) {
+						addValueMapping(FreeMarkerUtil.extractJavaPath(fields[fieldIndex]), node);
+					}					
+					fieldIndex++;
+				}
+			}
+		} catch (IOException e) {
+			throw new TemplateBuilderException("Failed to parse CSV fields in CSV template.", e); //$NON-NLS-1$
+		}
+	}
+
+	public String buildTemplate() throws TemplateBuilderException {
+        Element recordElement = getModel().getDocumentElement();
+        CollectionMapping collectionMapping = getCollectionMapping(recordElement);
+
+        if(collectionMapping == null) {
+            throw new UnmappedCollectionNodeException(recordElement);
+        } else {
+            StringBuilder template = new StringBuilder();
+            NodeList nodeList = recordElement.getChildNodes();
+            int fieldIndex = 0;
+
+            template.append("<#list " + collectionMapping.getSrcPath() + " as " + collectionMapping.getCollectionItemName() + ">\n"); //$NON-NLS-1$
+            for(int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if(node.getNodeType() == Node.ELEMENT_NODE) {
+                    Mapping fieldMapping = getMapping(node);
+
+                    if(fieldIndex > 0) {
+                        template.append(separatorChar);
+                    }
+
+                    if(fieldMapping != null) {
+                        template.append(quoteChar);
+                        template.append("${" + fieldMapping.getSrcPath() + "?string}"); //$NON-NLS-1$
+                        template.append(quoteChar);
+                    }
+
+                    fieldIndex++;
+                }
+            }
+            template.append("\n</#list>"); //$NON-NLS-1$
+
+            return template.toString();
+        }
+    }
+}
