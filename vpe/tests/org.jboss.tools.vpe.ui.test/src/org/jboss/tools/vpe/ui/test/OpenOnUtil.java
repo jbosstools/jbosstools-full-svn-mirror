505cf1d5ccc8b90a0f0a566f1146b59bab885af3
Yura Zhishko <yzhishko@exadel.com>
2010-01-15 17:39:41 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5682 - fixed
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19782 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/OpenOnUtil.java
+++ vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/OpenOnUtil.java
@@ -11,14 +11,20 @@
 package org.jboss.tools.vpe.ui.test;
 
 import java.lang.reflect.Method;
-
+import static junit.framework.Assert.assertEquals;
 import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.MultiPageEditorPart;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 import org.eclipse.wst.sse.ui.StructuredTextEditor;
+import org.jboss.tools.common.model.ui.editor.EditorPartWrapper;
 import org.jboss.tools.common.text.ext.hyperlink.AbstractHyperlink;
+import org.jboss.tools.jst.jsp.jspeditor.JSPMultiPageEditor;
 
 /**
  * @author Sergey Dzmitrovich
@@ -56,6 +62,63 @@ public class OpenOnUtil {
 				break;
 			}
 		}
+
+	}
+	
+	/**
+	 * Function for checking openOn functionality
+	 * 
+	 * @param editorInput
+	 * @param editorId
+	 * @param lineNumber
+	 * @param lineOffset
+	 * @param openedOnFileName
+	 * @throws Throwable
+	 * 
+	 * @author mareshkau
+	 */
+	
+	public static final void checkOpenOnInEditor(IEditorInput editorInput,String editorId,int lineNumber, int lineOffset, String openedOnFileName) throws Throwable {
+		IEditorPart editorPart = PlatformUI
+				.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+				.openEditor(editorInput, editorId);
+		StructuredTextEditor textEditor = null;
+		if(editorPart instanceof MultiPageEditorPart){
+			IEditorPart[] editorParts = ((MultiPageEditorPart)editorPart).findEditors(editorInput);
+			((MultiPageEditorPart)editorPart).setActiveEditor(editorParts[0]);
+			textEditor = (StructuredTextEditor) editorParts[0];
+		} else if(editorPart instanceof JSPMultiPageEditor) {
+			textEditor = ((JSPMultiPageEditor)editorPart).getSourceEditor();
+		} else if(editorPart instanceof EditorPartWrapper) {
+			IEditorPart[] editorParts = ((MultiPageEditorPart)((EditorPartWrapper)editorPart).getEditor()).findEditors(editorInput);
+			((MultiPageEditorPart)((EditorPartWrapper)editorPart).getEditor()).setActiveEditor(editorParts[1]);
+			textEditor = (StructuredTextEditor) editorParts[1];
+		}
+		int openOnPosition = TestUtil.getLinePositionOffcet(textEditor
+				.getTextViewer(),lineNumber, lineOffset);
+		// hack to get hyperlinks detectors, no other was have been founded
+		Method method = AbstractTextEditor.class
+				.getDeclaredMethod("getSourceViewerConfiguration"); //$NON-NLS-1$
+		method.setAccessible(true);
+		SourceViewerConfiguration sourceViewerConfiguration = (SourceViewerConfiguration) method
+				.invoke(textEditor);
+		IHyperlinkDetector[] hyperlinkDetectors = sourceViewerConfiguration
+				.getHyperlinkDetectors(textEditor.getTextViewer());
+		for (IHyperlinkDetector iHyperlinkDetector : hyperlinkDetectors) {
+			IHyperlink[] hyperLinks = iHyperlinkDetector.detectHyperlinks(
+					textEditor.getTextViewer(), new Region(openOnPosition, 0),
+					false);
+			if (hyperLinks != null && hyperLinks.length > 0
+					&& hyperLinks[0] instanceof AbstractHyperlink) {
+				AbstractHyperlink abstractHyperlink = (AbstractHyperlink) hyperLinks[0];
+				abstractHyperlink.open();
+				break;
+			}
+		}
+		IEditorPart activeEditor = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		assertEquals(
+				"Active page should be ", openedOnFileName, activeEditor.getEditorInput().getName()); //$NON-NLS-1$
 
 	}
 
