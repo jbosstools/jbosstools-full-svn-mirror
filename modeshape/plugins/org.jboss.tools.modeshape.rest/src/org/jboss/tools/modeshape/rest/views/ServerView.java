4912f57723f86eb64fa867987a82ac35cbc42282
Dan Florian <dflorian@redhat.com>
2010-01-29 19:52:02 +0000

    MODE-580 Rebrand project to new name. Made minor changes to file headers based on input from our legal folks.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20025 a97e2381-89e5-4abb-bab3-167db6db766c

--- modeshape/plugins/org.jboss.tools.modeshape.rest/src/org/jboss/tools/modeshape/rest/views/ServerView.java
+++ modeshape/plugins/org.jboss.tools.modeshape.rest/src/org/jboss/tools/modeshape/rest/views/ServerView.java
@@ -2,9 +2,8 @@
  * See the COPYRIGHT.txt file distributed with this work for information
  * regarding copyright ownership.
  *
- * Distributed under license by Red Hat, Inc. All rights reserved.
- * This program is made available under the terms of the
- * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * This software is made available by Red Hat, Inc. under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution and is
  * available at http://www.eclipse.org/legal/epl-v10.html.
  *
  * See the AUTHORS.txt file in the distribution for a full listing of
