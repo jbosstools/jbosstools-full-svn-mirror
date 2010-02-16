4bcb8d91a3c46eefb4a4207fcdbeecc3b4afed0c
Maxim Areshkau <mareshkau@exadel.com>
2010-02-16 11:10:05 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5820 https://jira.jboss.org/jira/browse/JBIDE-5821
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@20294 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/VpeTestSetup.java
+++ vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/VpeTestSetup.java
@@ -17,6 +17,8 @@ import junit.extensions.TestSetup;
 import junit.framework.TestSuite;
 
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.PlatformUI;
 import org.jboss.tools.test.util.ResourcesUtils;
 import org.jboss.tools.tests.ImportBean;
 
@@ -34,6 +36,8 @@ public class VpeTestSetup extends TestSetup {
 	 */
 	private List<ImportBean> testProjectNames;
 	
+	private static final String CONTENT_OUTLINE_VIEW_ID = "org.eclipse.ui.views.ContentOutline"; //$NON-NLS-1$
+	
 	public VpeTestSetup(TestSuite test, List<ImportBean> testProjectNames) {
 		super(test);
 		setTestProjects(testProjectNames);
@@ -50,6 +54,14 @@ public class VpeTestSetup extends TestSetup {
 				ResourcesUtils.importProjectIntoWorkspace((importBean.getImportProjectPath()
 						+ File.separator+importBean.getImportProjectName()),importBean.getImportProjectName());
 			}		
+		}
+		//added by Maksim Areshkau, Fix for https://jira.jboss.org/jira/browse/JBIDE-5820 https://jira.jboss.org/jira/browse/JBIDE-5821
+        //remove this code when we will move on wtp 3.2
+		IViewReference[] iviewReferences= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getViewReferences();
+        for (IViewReference iViewReference : iviewReferences) {
+        	if(VpeTestSetup.CONTENT_OUTLINE_VIEW_ID.equalsIgnoreCase(iViewReference.getId())){
+        		 PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().hideView(iViewReference);
+        	}
 		}
 	}
 
