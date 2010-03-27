eab6612921d41c6e2485250ea65179488b3470e6
Max Rydahl Andersen <max@hibernate.org>
2010-03-27 00:31:05 +0000

    exclude sdk from feature.xml
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/modular_build@21088 a97e2381-89e5-4abb-bab3-167db6db766c

--- genpom.scala
+++ genpom.scala
@@ -156,8 +156,7 @@ object HelloWorld {
     )
     
     val realModules = dirs.filter(
-      (n) => n.isDirectory() && !n.getName().startsWith(".") && (new File(new File(n, "META-INF"), "MANIFEST.MF").exists() || new File(n, "feature.xml").exists())
-    )
+      (n) => n.isDirectory() && !n.getName().startsWith(".") && (new File(new File(n, "META-INF"), "MANIFEST.MF").exists() || (new File(n, "feature.xml").exists() && !n.getName().contains(".sdk."))))
     var modules =
       <project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
     <modelVersion>4.0.0</modelVersion> 
