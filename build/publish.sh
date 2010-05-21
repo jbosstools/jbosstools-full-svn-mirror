ea08aca98f053965133e2d76abf064149e8380be
Nick Boldt <nboldt@redhat.com>
2010-05-21 23:38:12 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22254 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publish.sh
+++ build/publish.sh
@@ -5,10 +5,10 @@
 ZIPNAME=${JOB_NAME}-Update-H${BUILD_NUMBER}-${BUILD_ID}.zip
 
 # copy into workspace for archiving
-rm -fr ${WORKSPACE}/site; mkdir -p ${WORKSPACE}/site
-rsync -aq ${WORKSPACE}/*/site/target/site.zip ${WORKSPACE}/site/${ZIPNAME}
+rm -fr ${WORKSPACE}/site; mkdir -p ${WORKSPACE}/site/${JOB_NAME}
+rsync -aq ${WORKSPACE}/*/site/target/site.zip ${WORKSPACE}/site/${JOB_NAME}/${ZIPNAME}
 
 # publish to download.jboss.org
 if [[ $DESTINATION == "" ]]; then DESTINATION="tools@filemgmt.jboss.org:/downloads_htdocs/tools"; fi
-rsync -arzq --delete ${WORKSPACE}/site/* $DESTINATION/builds/nightly/3.2.helios/${JOB_NAME}/
+rsync -arzq --delete ${WORKSPACE}/site/${JOB_NAME} $DESTINATION/builds/nightly/3.2.helios/
 
