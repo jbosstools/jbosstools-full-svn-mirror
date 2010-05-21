27732c3d8a767c8ec03d9ca928b733e5618da4a1
Nick Boldt <nboldt@redhat.com>
2010-05-21 23:05:17 +0000

    move zip into site/ folder to allow better 'rsync --delete'
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22252 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publish.sh
+++ build/publish.sh
@@ -5,11 +5,10 @@
 ZIPNAME=${JOB_NAME}-Update-H${BUILD_NUMBER}-${BUILD_ID}.zip
 
 # copy into workspace for archiving
-rm -fr ${WORKSPACE}/${JOB_NAME}-Update*.zip
-rsync -aq ${WORKSPACE}/*/site/target/site.zip ${WORKSPACE}/${ZIPNAME}
+rm -fr ${WORKSPACE}/site; mkdir -p ${WORKSPACE}/site
+rsync -aq ${WORKSPACE}/*/site/target/site.zip ${WORKSPACE}/site/${ZIPNAME}
 
 # publish to download.jboss.org
 if [[ $DESTINATION == "" ]]; then DESTINATION="tools@filemgmt.jboss.org:/downloads_htdocs/tools"; fi
-rsync -aq --delete ${WORKSPACE}/${ZIPNAME} $DESTINATION/builds/nightly/3.2.helios/${JOB_NAME}/
-
+rsync -aq --delete ${WORKSPACE}/site/* $DESTINATION/builds/nightly/3.2.helios/${JOB_NAME}/
 
