d67fe6fa23b243fff91c8a8c2efe43d1d0646514
Nick Boldt <nboldt@redhat.com>
2010-06-10 19:06:14 +0000

    tweak example and filter
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22733 a97e2381-89e5-4abb-bab3-167db6db766c

--- runstack.sh
+++ runstack.sh
@@ -7,7 +7,7 @@ workingdir=~/workspace36/jbosstools-modular_build
 if [[ $# -lt 1 ]]; then
 	echo "Usage: $0 component1 component2 ..."
 	echo "Eg. $0 tests common jmx archives as"
-	echo "Eg. $0 jst jsf vpe struts seam"
+	echo "Eg. $0 jst jsf vpe struts seam -Dmaven.test.skip"
 	exit 1
 fi
 
@@ -32,7 +32,8 @@ done
 # collect compilation results and failures
 for d in $components; do 
 	cd $workingdir; echo "==== $d ===="
-	egrep -v "org\.|com\." $d/buildlog.latest.txt | egrep "SUCCESS|FAIL"
+	egrep -v "org\.|com\." $d/buildlog.latest.txt | egrep "SUCCESS"
+	egrep "FAILURE|SKIPPED" $d/buildlog.latest.txt
 	egrep -A1 "Cannot complete the request|depends on|satisfy dependency|Missing requirement|requires '.+'" $d/buildlog.latest.txt
 	echo ""
 done
