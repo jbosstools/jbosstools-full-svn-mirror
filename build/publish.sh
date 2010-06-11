a36e1432dffd68175b37a3c40b44cf50e72ffac9
Nick Boldt <nboldt@redhat.com>
2010-06-11 03:57:35 +0000

    merge from trunk
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/3.2.helios@22744 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publish.sh
+++ build/publish.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # Hudson script used to publish Tycho-built p2 update sites
 
-# define target zip filename date and hudson build id marked for clarity and publication
-ZIPNAME=${JOB_NAME}-Update-H${BUILD_NUMBER}-${BUILD_ID}.zip
-
 # define target zip filename for inclusion in uberbuilder's bucky aggregator
 SNAPNAME=${JOB_NAME}-Update-SNAPSHOT.zip
 
 rm -fr ${WORKSPACE}/site; mkdir -p ${WORKSPACE}/site/${JOB_NAME}
 if [[ -f ${WORKSPACE}/sources/site/target/site.zip ]]; then 
-	# copy into workspace for archiving
-	rsync -aq ${WORKSPACE}/sources/site/target/site.zip ${WORKSPACE}/site/${JOB_NAME}/${ZIPNAME}
-	# copy into workspace for access by bucky aggregator
+	# note the job name, build number, and build ID of the latest snapshot zip
+	echo "JOB_NAME = ${JOB_NAME}" > ${WORKSPACE}/site/${JOB_NAME}/JOB_NAME.txt
+	echo "BUILD_NUMBER = ${BUILD_NUMBER}" > ${WORKSPACE}/site/${JOB_NAME}/BUILD_NUMBER.txt
+	echo "BUILD_ID = ${BUILD_ID}" > ${WORKSPACE}/site/${JOB_NAME}/BUILD_ID.txt
+	
+	# unzip into workspace for publishing as unpacked site
+	unzip -q ${WORKSPACE}/sources/site/target/site.zip -d ${WORKSPACE}/site/${JOB_NAME}/
+	
+	# copy into workspace for access by bucky aggregator (same name every time)
 	rsync -aq ${WORKSPACE}/sources/site/target/site.zip ${WORKSPACE}/site/${SNAPNAME}
 fi
 
@@ -26,5 +29,6 @@ if [[ -d ${WORKSPACE}/site/${JOB_NAME} ]]; then
 	rsync -arzq --delete ${WORKSPACE}/site/${JOB_NAME} $DESTINATION/builds/nightly/3.2.helios/
 fi
 if [[ -f ${WORKSPACE}/site/${SNAPNAME} ]]; then
+	# publish snapshot zip
 	rsync -arzq --delete ${WORKSPACE}/site/${SNAPNAME} $DESTINATION/builds/nightly/3.2.helios/
 fi
