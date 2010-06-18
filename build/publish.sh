5b69918928e6504b1b6a3d96614b5f4177e717b2
Nick Boldt <nboldt@redhat.com>
2010-06-18 17:08:08 +0000

    fix unzip flag order
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22888 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publish.sh
+++ build/publish.sh
@@ -7,13 +7,14 @@ SNAPNAME=${JOB_NAME}-Update-SNAPSHOT.zip
 rm -fr ${WORKSPACE}/site; mkdir -p ${WORKSPACE}/site/${JOB_NAME}
 for z in ${WORKSPACE}/sources/site/target/site.zip ${WORKSPACE}/sources/site/target/site_assembly.zip; do
 	if [[ -f $z ]]; then
+		echo "$z ..."
 		# note the job name, build number, and build ID of the latest snapshot zip
 		echo "JOB_NAME = ${JOB_NAME}" > ${WORKSPACE}/site/${JOB_NAME}/JOB_NAME.txt
 		echo "BUILD_NUMBER = ${BUILD_NUMBER}" > ${WORKSPACE}/site/${JOB_NAME}/BUILD_NUMBER.txt
 		echo "BUILD_ID = ${BUILD_ID}" > ${WORKSPACE}/site/${JOB_NAME}/BUILD_ID.txt
 	
 		# unzip into workspace for publishing as unpacked site
-		unzip $z -fo -q -d ${WORKSPACE}/site/${JOB_NAME}/
+		unzip -f -o -q -d ${WORKSPACE}/site/${JOB_NAME}/ $z
 	
 		# copy into workspace for access by bucky aggregator (same name every time)
 		rsync -aq $z ${WORKSPACE}/site/${SNAPNAME}
@@ -23,7 +24,7 @@ done
 # if zips exist produced & renamed by ant script, copy them too
 for z in $(find ${WORKSPACE} -maxdepth 5 -mindepth 3 -name "*Update*.zip"); do 
 	echo "$z ..."
-	unzip $z -fo -q -d ${WORKSPACE}/site/${JOB_NAME}/
+	unzip -f -o -q -d ${WORKSPACE}/site/${JOB_NAME}/ $z
 	rsync -aq $z ${WORKSPACE}/site/${SNAPNAME}
 done
 
