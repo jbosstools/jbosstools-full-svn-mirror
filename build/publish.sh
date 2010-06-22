52f73f24936ace181a4eafd0e2a73b03386d38f5
Nick Boldt <nboldt@redhat.com>
2010-06-22 20:06:37 +0000

    clean up console logging in Hudson - less verbose
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22934 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publish.sh
+++ build/publish.sh
@@ -9,7 +9,7 @@ SNAPNAME=${JOB_NAME}-Update-SNAPSHOT.zip
 rm -fr ${WORKSPACE}/site; mkdir -p ${WORKSPACE}/site/${JOB_NAME}
 for z in ${WORKSPACE}/sources/site/target/site.zip ${WORKSPACE}/sources/site/target/site_assembly.zip; do
 	if [[ -f $z ]]; then
-		echo "$z ..."
+		#echo "$z ..."
 		# note the job name, build number, and build ID of the latest snapshot zip
 		echo "JOB_NAME = ${JOB_NAME}" > ${WORKSPACE}/site/${JOB_NAME}/JOB_NAME.txt
 		echo "BUILD_NUMBER = ${BUILD_NUMBER}" > ${WORKSPACE}/site/${JOB_NAME}/BUILD_NUMBER.txt
@@ -25,20 +25,20 @@ done
 
 # if component zips exist, copy them too; first site.zip, then site_assembly.zip
 for z in $(find ${WORKSPACE}/sources/*/site/target -type f -name "site*.zip" | sort -r); do 
-	if [[ -f $z ]]; then
+	#if [[ -f $z ]]; then
 		y=${z%%/site/target/*}; y=${y##*/}
-		echo "[$y] $z ..."
+		#echo "[$y] $z ..."
 		# unzip into workspace for publishing as unpacked site
 		mkdir -p ${WORKSPACE}/site/${JOB_NAME}/$y
 		unzip -u -o -q -d ${WORKSPACE}/site/${JOB_NAME}/$y $z
 		# copy into workspace for access by bucky aggregator (same name every time)
 		rsync -aq $z ${WORKSPACE}/site/${JOB_NAME}/${y}-Update-SNAPSHOT.zip
-	fi
+	#fi
 done
 
 # if zips exist produced & renamed by ant script, copy them too
 for z in $(find ${WORKSPACE} -maxdepth 5 -mindepth 3 -name "*Update*.zip"); do 
-	echo "$z ..."
+	#echo "$z ..."
 	unzip -u -o -q -d ${WORKSPACE}/site/${JOB_NAME}/ $z
 	rsync -aq $z ${WORKSPACE}/site/${SNAPNAME}
 done
