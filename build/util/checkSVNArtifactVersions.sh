8bb31f58a4e157cf931ebd14521661cb71766f16
Nick Boldt <nboldt@redhat.com>
2010-06-23 05:57:15 +0000

    tweak checkSVNArtifactVersions.sh to improve logged output; store copy of baseline in checkCompiledArtifactVersions.sh_versionlist_JBT3.1.0.txt
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@22938 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/util/checkSVNArtifactVersions.sh
+++ build/util/checkSVNArtifactVersions.sh
@@ -38,52 +38,57 @@ for f in $dirs; do
 	fi
 done
 echo "Found $features features and $plugins plugins to check."
-echo ""
 
 . ${versionlist}
 
 for f in $dirs; do 
 	if [[ -f ${basedir}${f}/META-INF/MANIFEST.MF ]] || [[ -f ${basedir}${f}/feature.xml ]]; then
-		#echo ${f};
-		if [[ -f ${basedir}${f}/META-INF/MANIFEST.MF ]]; then
+		echo ""
+		echo "== ${basedir}${f} ==";
+		echo ""
+		if [[ -f ${basedir}${f}/feature.xml ]]; then
+			dos2unix -q ${basedir}${f}/feature.xml
+			v=$(head -16 ${basedir}${f}/feature.xml | grep -v "<?xml" | grep -v "version=\"0.0.0\"" | grep "version=" | head -1 | tr -d "a-zA-Z=\"> 	"); v=${v%.};
+			a=${f##*/}  # org.jboss.tools.maven.seam.feature
+			b=${a//./_} # org_jboss_tools_maven_seam_feature
+
+			echo "diff svn artifact:"
+			svn diff --summarize --new=${newsvnbasedir}${f} --old=${oldsvnbasedir}${f} | perl -pe "s#${oldsvnbasedir}${f}#${a}#g"
+			echo ""
+
+			echo "diff svn manifest:"
+			svn diff --new=${newsvnbasedir}${f}/feature.xml --old=${oldsvnbasedir}${f}/feature.xml | grep -v "<?xml" | grep -v "version=\"0.0.0\"" | grep "version="
+			echo ""
+
+			echo "diff rel baseline:"
+			if [[ ${!b} ]]; then
+				echo "- ${!b} :: $a"
+			else
+				echo "- ?.?.? :: $a"
+			fi
+			echo "+ $v :: $a"
+		elif [[ -f ${basedir}${f}/META-INF/MANIFEST.MF ]]; then
 			dos2unix -q ${basedir}${f}/META-INF/MANIFEST.MF
-			#svn diff --new=${newsvnbasedir}${f}/META-INF/MANIFEST.MF --old=${oldsvnbasedir}${f}/META-INF/MANIFEST.MF | grep Bundle-Version
 			v=$(cat ${basedir}${f}/META-INF/MANIFEST.MF | grep Bundle-Version | perl -pe "s#Bundle-Version: ##g"); v=${v:0:5}
 			a=${f##*/}  # org.jboss.tools.maven.seam.feature
 			b=${a//./_} # org_jboss_tools_maven_seam_feature
-			if [[ ${!b} ]] && [[ "${!b}" != "$v" ]]; then
-				echo " - ${!b} :: $a"
-				echo " + $v :: $a"
-			elif [[ ! ${!b} ]]; then
-				if [[ $(echo $a | grep test) ]]; then
-					echo "T+ $v :: $a" | grep test
-				else
-					echo "P+ $v :: $a"
-				fi
+
+			echo "diff svn artifact:"
+			svn diff --summarize --new=${newsvnbasedir}${f} --old=${oldsvnbasedir}${f} | perl -pe "s#${oldsvnbasedir}${f}#${a}#g"
+			echo ""
+
+			echo "diff svn manifest:"
+			svn diff --new=${newsvnbasedir}${f}/META-INF/MANIFEST.MF --old=${oldsvnbasedir}${f}/META-INF/MANIFEST.MF | grep Bundle-Version
+			echo ""
+
+			echo "diff rel baseline:"
+			if [[ ${!b} ]]; then
+				echo "- ${!b} :: $a"
 			else
-				echo "== $v :: $a"
+				echo "- ?.?.? :: $a"
 			fi
+			echo "+ $v :: $a"
 		fi
-		if [[ -f ${basedir}${f}/feature.xml ]]; then
-			#svn diff --new=${newsvnbasedir}${f}/feature.xml --old=${oldsvnbasedir}${f}/feature.xml | grep -v "<?xml" | grep -v "version=\"0.0.0\"" | grep "version="
-			dos2unix -q ${basedir}${f}/feature.xml
-			v=$(head -16 ${basedir}${f}/feature.xml | grep -v "<?xml" | grep -v "version=\"0.0.0\"" | grep "version=" | head -1 | tr -d "a-zA-Z=\"> 	"); v=${v%.};
-			a=${f##*/}
-			b=${a//./_} # org_jboss_tools_maven_seam_feature
-			if [[ ${!b} ]] && [[ "${!b}" != "$v" ]]; then
-				echo " - ${!b} :: $a"
-				echo " + $v :: $a"
-			elif [[ ! ${!b} ]]; then
-				if [[ $(echo $a | grep test) ]]; then
-					echo "T+ $v :: $a" | grep test
-				else
-					echo "F+ $v :: $a"
-				fi
-			else
-				echo "== $v :: $a"
-			fi
-		fi
-		#echo ""
 	fi
 done
 
