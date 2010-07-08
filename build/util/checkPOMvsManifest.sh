e7a354f95942a159c0e73d4406d0ad1daf988cd9
Nick Boldt <nboldt@redhat.com>
2010-07-08 13:53:03 +0000

    check for non-SNAPSHOT plugins/features too
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@23315 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/util/checkPOMvsManifest.sh
+++ build/util/checkPOMvsManifest.sh
@@ -12,19 +12,34 @@ for f in $files; do
 	if [[ $d == $f ]]; then #else feature
 		d=${f/feature.xml/}
 		artVersionLine=$(cat $f | egrep "version=\"[0-9]\.[0-9]\.[0-9]\.qualifier" | egrep "[0-9]\.[0-9]\.[0-9]")
-		artVersion=${artVersionLine##*version=\"}
-		artVersion=${artVersion%%.qualifier*}
-		artVersionLine="   "$(cat $f | egrep "version=\"[0-9]\.[0-9]\.[0-9]\.qualifier" | egrep "[0-9]\.[0-9]\.[0-9]")
+		if [[ ! $artVersionLine ]]; then # not a snapshot version
+			artVersionLine=$(cat $f | head -7 | egrep "version=\"[0-9]\.[0-9]\.[0-9].*\".*" | egrep "[0-9]\.[0-9]\.[0-9]" | tail -1)
+			artVersion=${artVersionLine##*version=\"}
+			artVersion=${artVersion%%\"*}
+			#echo "[WARNING] [$altVersion] ${d}feature.xml not .qualifier version."
+		else
+			artVersion=${artVersionLine##*version=\"}
+			artVersion=${artVersion%%.qualifier*}
+		fi
+		artVersionLine="   "$altVersionLine
 	else
 		artVersionLine=$(cat $f | egrep "Bundle-Version: " | egrep "[0-9]\.[0-9]\.[0-9]")
 		artVersion=${artVersionLine##*: }
 		artVersion=${artVersion%%.qualifier}
 		artVersionLine="  "$artVersionLine
 	fi
+
 	if [[ -f $d/pom.xml ]]; then
 		pomVersionLine=$(cat $d/pom.xml | sed "s/\t//" | egrep ".0-SNAPSHOT" | egrep "[0-9]\.[0-9]\.[0-9]")
-		pomVersion=${pomVersionLine#*<version>}
-		pomVersion=${pomVersion%-SNAPSHOT*}
+		if [[ ! $pomVersionLine ]]; then # not a snapshot version
+			pomVersionLine=$(cat $d/pom.xml | sed "s/\t//" | egrep "<version>" | egrep "[0-9]\.[0-9]\.[0-9]" | tail -1)
+			pomVersion=${pomVersionLine%%</version>*}
+			pomVersion=${pomVersion#*<version>}
+			#echo "[WARNING] [$pomVersion] ${d}pom.xml not SNAPSHOT version."
+		else
+			pomVersion=${pomVersionLine%%-SNAPSHOT*}
+			pomVersion=${pomVersion#*<version>}
+		fi
 		pomVersionLine="         "$pomVersionLine
 
 		if [[ $artVersion != $pomVersion ]]; then 
