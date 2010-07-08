b08f6c5ec0dfd7850da8c9f8bc440a56262ba0a6
Nick Boldt <nboldt@redhat.com>
2010-07-08 03:41:29 +0000

    fix manifest/pom versions
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@23300 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ build/util/checkPOMvsManifest.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [[ $1 ]] && [[ -d $1 ]]; then
+	basedir=$1
+else
+	basedir=.
+fi
+
+files=$(find . -maxdepth 5 -type f -name MANIFEST.MF | egrep -v "target|sdk")" "$(find . -maxdepth 4 -type f -name "feature.xml" | egrep -v "target|sdk")
+for f in $files; do
+	d=${f/META-INF\/MANIFEST.MF/}; #assume plugin
+	if [[ $d == $f ]]; then #else feature
+		d=${f/feature.xml/}
+		artVersionLine=$(cat $f | egrep "version=\"[0-9]\.[0-9]\.[0-9]\.qualifier" | egrep "[0-9]\.[0-9]\.[0-9]")
+		artVersion=${artVersionLine##*version=\"}
+		artVersion=${artVersion%%.qualifier*}
+		artVersionLine="   "$(cat $f | egrep "version=\"[0-9]\.[0-9]\.[0-9]\.qualifier" | egrep "[0-9]\.[0-9]\.[0-9]")
+	else
+		artVersionLine=$(cat $f | egrep "Bundle-Version: " | egrep "[0-9]\.[0-9]\.[0-9]")
+		artVersion=${artVersionLine##*: }
+		artVersion=${artVersion%%.qualifier}
+		artVersionLine="  "$artVersionLine
+	fi
+	if [[ -f $d/pom.xml ]]; then
+		pomVersionLine=$(cat $d/pom.xml | sed "s/\t//" | egrep ".0-SNAPSHOT" | egrep "[0-9]\.[0-9]\.[0-9]")
+		pomVersion=${pomVersionLine#*<version>}
+		pomVersion=${pomVersion%-SNAPSHOT*}
+		pomVersionLine="         "$pomVersionLine
+
+		if [[ $artVersion != $pomVersion ]]; then 
+			echo "[ERROR] $d"
+			echo "[ERROR] Artifact version [$artVersion] != pom version [$pomVersion]" | egrep "[0-9]\.[0-9]\.[0-9]"; 
+			echo "[ERROR] $artVersionLine"
+			echo "[ERROR] $pomVersionLine"
+			echo ""
+		else
+			#echo "[INFO] [$artVersion] = [$pomVersion] $d "
+			true
+		fi
+	else
+		echo "[WARNING] $d contains no pom.xml"
+	fi
+done
+
