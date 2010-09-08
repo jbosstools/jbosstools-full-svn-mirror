907542130e8025aa4a7de7f8e9ca4fe4e64a3573
Koen Aers <koen.aers@jboss.com>
2010-09-08 10:05:11 +0000

    initial upload
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@24807 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ bpmn/plugins/org.jboss.tools.bpmn2.process.diagram/src/org/jboss/tools/bpmn2/process/diagram/preferences/DiagramAppearancePreferencePage.java
@@ -0,0 +1,18 @@
+package org.jboss.tools.bpmn2.process.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.AppearancePreferencePage;
+import org.jboss.tools.bpmn2.process.diagram.part.Bpmn2DiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramAppearancePreferencePage extends AppearancePreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramAppearancePreferencePage() {
+		setPreferenceStore(Bpmn2DiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
