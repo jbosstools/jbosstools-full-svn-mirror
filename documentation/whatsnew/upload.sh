8175ca30ddd9a189f50cb5f0024f19fde1940d93
Max Rydahl Andersen <max@hibernate.org>
2010-09-09 13:01:50 +0000

    JBIDE-7035 - reviewed and updated whats N&N
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@24830 a97e2381-89e5-4abb-bab3-167db6db766c

--- documentation/whatsnew/upload.sh
+++ documentation/whatsnew/upload.sh
@@ -1,1 +1,1 @@
-rsync -i -t -e ssh -C --exclude-from '.rsyncexclude' -avz . tools@filemgmt.jboss.org:/docs_htdocs/tools/whatsnew/
+rsync -i -c -e ssh -C --exclude-from '.rsyncexclude' -avz . tools@filemgmt.jboss.org:/docs_htdocs/tools/whatsnew/
