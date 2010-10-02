e945bc8dd657ac65e52e56a1dc8cd5946c02b6c6
Nick Boldt <nboldt@redhat.com>
2010-10-02 06:43:56 +0000

    publish script for generated target platform repo (built by Hudson) echo sizes into log and move temp file
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25431 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/target-platform/publish.sh
+++ build/target-platform/publish.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-# Hudson creates a repo in ${repoPath}
-# So, copy it into other places for access by downstream jobs and others
+# Hudson creates a repo in ${repoPath}; copy it into other places for access by downstream jobs and users
+
+targetFile=e361-wtp322.target
 
 repoPath=/home/hudson/static_build_env/jbds/tools/sources/REPO
-targetFile=e361-wtp322.target
+destinationPath=/home/hudson/static_build_env/jbds/target-platform
 DESTINATION=tools@filemgmt.jboss.org:/downloads_htdocs/tools/updates/target-platform
-LOCALDESTNN=/home/hudson/static_build_env/jbds/target-platform
 
 if [[ -d ${repoPath} ]]; then
+	du -sh ${repoPath}
+
 	# copy/update into central place for reuse by local downstream build jobs
-	rsync -aPrz --delete ${repoPath}/* ${LOCALDESTNN}/${targetFile}/
+	rsync -aPrz --delete ${repoPath}/* ${destinationPath}/${targetFile}/
+	du -sh ${destinationPath}/${targetFile} 
 
 	# upload to http://download.jboss.org/jbossotools/updates/target-platform/latest/ for public use
 	rsync -aPrz --delete --rsh=ssh ${repoPath}/* ${DESTINATION}/latest/ &
 
 	# create zip, then upload to http://download.jboss.org/jbossotools/updates/target-platform/${targetFile}.zip for public use
 	cd ${repoPath} && \
-	zip -q -r9 ../${targetFile}.zip * && \
-	rsync -aPrz --delete --rsh=ssh ${repoPath}/../${targetFile}.zip ${DESTINATION}/ && \
-	rm -f ${repoPath}/../${targetFile}.zip
+	zip -q -r9 /tmp/${targetFile}.zip * && \
+	du -sh /tmp/${targetFile}.zip && \
+	rsync -aPrz --delete --rsh=ssh /tmp/${targetFile}.zip ${DESTINATION}/ && \
+	rm -f /tmp/${targetFile}.zip
 fi
