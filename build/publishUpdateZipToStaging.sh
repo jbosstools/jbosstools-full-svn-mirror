6927185394328a96b109d2c45e68198dc09d5493
Nick Boldt <nboldt@redhat.com>
2010-10-04 18:57:44 +0000

    https://jira.jboss.org/browse/JBDS-1349 new single-serving update site zip publisher, for on demand use (by Hudson)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25465 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publishUpdateZipToStaging.sh
+++ build/publishUpdateZipToStaging.sh
@@ -1,10 +1,11 @@
+# Use this script to publisher a single update site zip via a Hudson job
+
 # get zip name from zip.list.txt
-path=$(wget -q http://download.jboss.org/jbosstools/builds/staging/${JOB_NAME}/logs/zip.list.txt -O - | egrep -- "-Update-" | sed "s#,\\\\##"); 
+path=$(wget -q http://download.jboss.org/jbosstools/builds/staging/${JOB_NAME}/logs/zip.list.txt -O - | egrep -- "-Update-" | head -1 | sed "s#,\\\\##"); 
 
 # get zip to local
 rsync -arzq tools@filemgmt.jboss.org:/downloads_htdocs/tools/builds/staging/${JOB_NAME}/${path} .
 
 # push renamed zip to remote
 cd ${path%/*}; rsync -arzq ${path##*/} tools@filemgmt.jboss.org:/downloads_htdocs/tools/updates/staging/${JOB_NAME}-Update.zip
-
 
