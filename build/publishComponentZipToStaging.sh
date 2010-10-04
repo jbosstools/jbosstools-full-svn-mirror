3a8d164879cf80c14cae462767d578ef9533852e
Nick Boldt <nboldt@redhat.com>
2010-10-04 20:24:33 +0000

    support multiple branches of published component zips
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25471 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publishComponentZipToStaging.sh
+++ build/publishComponentZipToStaging.sh
@@ -3,8 +3,8 @@
 if [[ $1 ]]; then
 	pattern="$1"	
 else
-	echo "Must run from Hudson or manually specify job name"
-	echo "(for offline testing), eg., $0 \"drools|teiid|usage|jbpm\" jbosstools-3.2.0.Beta2.aggregate"
+	echo "Must run from Hudson or manually specify pattern"
+	echo "eg., $0 \"drools|teiid|usage|jbpm\""
 	exit 1
 fi
 
@@ -16,11 +16,14 @@ elif [[ ${JOB_NAME} ]]; then
 	JOBNAMEREDUX=${JOB_NAME/-publish}
 else
 	echo "Must run from Hudson or manually specify job name"
-	echo "(for offline testing), eg., $0 jbosstools-teiid-designer"
+	echo "(for offline testing), eg., $0 \"drools|teiid|usage|jbpm\" jbosstools-3.2.0.Beta2.aggregate"
 	exit 1
 fi
 
 if [[ $DESTINATION == "" ]]; then DESTINATION="tools@filemgmt.jboss.org:/downloads_htdocs/tools"; fi
+
+# eg., extract 3.2.0.Beta2 from jbosstools-3.2.0.Beta2.aggregate or jbosstools-3.2.0.Beta2.aggregate-publish
+BRANCH=${JOBNAMEREDUX/.aggregate}; BRANCH=${BRANCH/jbosstools-}; #echo $BRANCH
 
 # get zip names from zip.list.txt
 paths=$(wget -q http://download.jboss.org/jbosstools/builds/staging/${JOBNAMEREDUX}/logs/zip.list.txt -O - | egrep -- "-Update-" | egrep "${pattern}" | sed "s#,\\\\##"); 
@@ -35,5 +38,5 @@ for path in $paths; do
 	
 	#echo "Publish ${path} as ${targetZip}"
 	# to test locally, may need to use --protocol=29 and -P instead of -q
-	date; rsync -arzq --rsh=ssh ${targetZip} ${DESTINATION}/updates/staging/
+	date; rsync -arzq --rsh=ssh ${targetZip} ${DESTINATION}/updates/staging/$BRANCH/
 done
