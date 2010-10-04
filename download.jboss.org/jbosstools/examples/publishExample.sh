54e8af180832513989c2b0fcd6e24e2b26b46ae4
Nick Boldt <nboldt@redhat.com>
2010-10-04 21:13:51 +0000

    comment out Konqueror launcher
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25474 a97e2381-89e5-4abb-bab3-167db6db766c

--- download.jboss.org/jbosstools/examples/publishExample.sh
+++ download.jboss.org/jbosstools/examples/publishExample.sh
@@ -12,7 +12,7 @@ svn ci -m "$0 $1"
 # Push to sftp://tools@filemgmt.jboss.org/downloads_htdocs/tools/examples/
 scp $filename tools@filemgmt.jboss.org:/downloads_htdocs/tools/examples/
 
-konqueror --profile "JBT jbossqa@filemgmt index-files"
+#konqueror --profile "JBT jbossqa@filemgmt index-files"
 
 echo ""
 echo "Be sure to update index.html if file is new."
