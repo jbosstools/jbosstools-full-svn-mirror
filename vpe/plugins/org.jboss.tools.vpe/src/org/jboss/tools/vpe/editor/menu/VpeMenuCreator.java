fcbcb621044fa8e92247cded3b7f411ac56cd353
Denis Maliarevich <dmaliarevich@exadel.com>
2010-10-05 12:48:20 +0000

    https://jira.jboss.org/browse/JBIDE-7222 , Adding ExternalizeStrings dialog to the VPE context menu
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25489 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/menu/VpeMenuCreator.java
+++ vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/menu/VpeMenuCreator.java
@@ -22,6 +22,7 @@ import org.eclipse.jface.action.Separator;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 import org.eclipse.wst.sse.ui.StructuredTextEditor;
+import org.jboss.tools.jst.jsp.i18n.ExternalizeStringsContributionItem;
 import org.jboss.tools.vpe.VpeDebug;
 import org.jboss.tools.vpe.editor.VpeEditorPart;
 import org.jboss.tools.vpe.editor.mapping.VpeDomMapping;
@@ -87,6 +88,11 @@ public class VpeMenuCreator {
 
 		menuManager.add(new InsertContributionItem(node));
 		addIfEnabled(new StripTagAction(node));
+		/*
+		 * https://jira.jboss.org/browse/JBIDE-7222
+		 * Adding ExternalizeStrings dialog to the VPE context menu
+		 */
+		menuManager.add(new ExternalizeStringsContributionItem());
 		addSeparator();
 		if (topLevelMenu) {
 			addZoomActions();
