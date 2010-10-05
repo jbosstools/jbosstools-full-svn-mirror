7350bc189ec5a594d2ffefe9b9fd461a93b6a8c0
Nick Boldt <nboldt@redhat.com>
2010-10-05 06:09:45 +0000

    switch from parallel to serial publishing; add more detailed du -sh commands
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25480 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/target-platform/publish.sh
+++ build/target-platform/publish.sh
@@ -8,13 +8,15 @@ destinationPath=/home/hudson/static_build_env/jbds/target-platform
 DESTINATION=tools@filemgmt.jboss.org:/downloads_htdocs/tools/updates/target-platform
 
 if [[ -d ${repoPath} ]]; then
-	du -sh ${repoPath}
+	du -sh ${repoPath} ${destinationPath}/${targetFile}
 
 	# copy/update into central place for reuse by local downstream build jobs
-	date; rsync -arzq --delete ${repoPath} ${destinationPath}/${targetFile} &
+	date; rsync -arzq --delete ${repoPath} ${destinationPath}/${targetFile} 
+
+	du -sh ${repoPath} ${destinationPath}/${targetFile}
 
 	# upload to http://download.jboss.org/jbossotools/updates/target-platform/latest/ for public use
-	date; rsync -arzq --delete --rsh=ssh ${repoPath} ${DESTINATION}/latest &
+	date; rsync -arzq --delete --rsh=ssh ${repoPath} ${DESTINATION}/latest 
 
 	# create zip, then upload to http://download.jboss.org/jbossotools/updates/target-platform/${targetFile}.zip for public use
 	cd ${repoPath}
