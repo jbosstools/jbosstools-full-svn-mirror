38a771a15d4818726690a65d2c3aa961ab7d2426
Andre Dietisheim <adietish@redhat.com>
2010-10-21 13:55:20 +0000

    [JBIDE-7371] completed listImages test
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25973 a97e2381-89e5-4abb-bab3-167db6db766c

--- deltacloud/tests/org.jboss.tools.deltacloud.test/src/org/jboss/tools/internal/deltacloud/test/DeltaCloudMockClientIntegrationTest.java
+++ deltacloud/tests/org.jboss.tools.deltacloud.test/src/org/jboss/tools/internal/deltacloud/test/DeltaCloudMockClientIntegrationTest.java
@@ -8,11 +8,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.jboss.tools.deltacloud.core.client.DeltaCloudClient;
 import org.jboss.tools.deltacloud.core.client.DeltaCloudClientException;
+import org.jboss.tools.deltacloud.core.client.Image;
 import org.jboss.tools.internal.deltacloud.test.fakes.ServerFake;
 import org.junit.Before;
 import org.junit.Test;
@@ -78,7 +80,19 @@ public class DeltaCloudMockClientIntegrationTest {
 	}
 
 	@Test
-	public void canListImages() throws DeltaCloudClientException {
-		client.listImages();
+	public void canListMockImages() throws DeltaCloudClientException {
+		List<Image> images = client.listImages();
+		assertEquals(3, images.size());
+		assertImage("img2", "Fedora 10", "fedoraproject", "Fedora 10", "i386", images.get(0));
+		assertImage("img1", "Fedora 10", "fedoraproject", "Fedora 10", "x86_64", images.get(1));
+		assertImage("img3", "JBoss", "mockuser", "JBoss", "i386", images.get(2));
+	}
+
+	private void assertImage(String id, String name, String owner, String description, String architecture, Image image) {
+		assertEquals(id, image.getId());
+		assertEquals(name, image.getName());
+		assertEquals(owner, image.getOwnerId());
+		assertEquals(architecture, image.getArchitecture());
+		assertEquals(description, image.getDescription());
 	}
 }
