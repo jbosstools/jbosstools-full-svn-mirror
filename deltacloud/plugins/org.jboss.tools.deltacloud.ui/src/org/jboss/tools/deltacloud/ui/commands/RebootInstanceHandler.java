fa6dcf629c03f5a539769c8171fd1faebba45dd5
Andre Dietisheim <adietish@redhat.com>
2010-11-04 19:06:31 +0000

    [JBIDE-7499] added dialog to reboot several instances at once
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@26269 a97e2381-89e5-4abb-bab3-167db6db766c

--- deltacloud/plugins/org.jboss.tools.deltacloud.ui/src/org/jboss/tools/deltacloud/ui/commands/RebootInstanceHandler.java
+++ deltacloud/plugins/org.jboss.tools.deltacloud.ui/src/org/jboss/tools/deltacloud/ui/commands/RebootInstanceHandler.java
@@ -10,57 +10,74 @@
  ******************************************************************************/
 package org.jboss.tools.deltacloud.ui.commands;
 
-import org.eclipse.core.commands.AbstractHandler;
+import java.util.List;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.jboss.tools.deltacloud.core.DeltaCloud;
 import org.jboss.tools.deltacloud.core.DeltaCloudInstance;
-import org.jboss.tools.deltacloud.ui.views.CVCloudElement;
 import org.jboss.tools.deltacloud.ui.views.CVInstanceElement;
 import org.jboss.tools.deltacloud.ui.views.CVMessages;
-import org.jboss.tools.deltacloud.ui.views.CloudViewElement;
-import org.jboss.tools.deltacloud.ui.views.PerformInstanceActionThread;
 import org.jboss.tools.internal.deltacloud.ui.utils.UIUtils;
 
 /**
  * @author Andre Dietisheim
  */
-public class RebootInstanceHandler extends AbstractHandler implements IHandler {
+public class RebootInstanceHandler extends AbstractInstanceHandler implements IHandler {
 
 	private final static String REBOOTING_INSTANCE_TITLE = "RebootingInstance.title"; //$NON-NLS-1$
 	private final static String REBOOTING_INSTANCE_MSG = "RebootingInstance.msg"; //$NON-NLS-1$
+
+	private final static String REBOOT_INSTANCE_TITLE = "RebootInstancesDialog.title"; //$NON-NLS-1$
+	private final static String REBOOT_INSTANCE_MSG = "RebootInstancesDialog.msg"; //$NON-NLS-1$
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
 		if (selection instanceof IStructuredSelection) {
-			CVInstanceElement cvInstance = UIUtils.getFirstElement(selection, CVInstanceElement.class);
-			rebootInstance(cvInstance);
+			if (isSingleInstanceSelected(selection)) {
+				CVInstanceElement cvInstance = UIUtils.getFirstElement(selection, CVInstanceElement.class);
+				rebootInstance(cvInstance);
+			} else {
+				rebootWithDialog((IStructuredSelection)selection);
+			}
 		}
 
 		return Status.OK_STATUS;
 	}
 
+	@SuppressWarnings("unchecked")
+	private void rebootWithDialog(IStructuredSelection selection) {
+		CVInstanceElementsSelectionDialog dialog = new CVInstanceElementsSelectionDialog(
+					UIUtils.getActiveShell()
+					, (List<CVInstanceElement>) selection.toList()
+					, CVMessages.getString(REBOOT_INSTANCE_TITLE)
+					, CVMessages.getString(REBOOT_INSTANCE_MSG));
+		if (Dialog.OK == dialog.open()) {
+			rebootInstances(dialog.getResult());
+		}
+	}
+	
+	private void rebootInstances(Object[] cvInstances) {
+		for (int i = 0; i < cvInstances.length; i++) {
+			rebootInstance((CVInstanceElement) cvInstances[i]);
+		}
+	}
+	
 	private void rebootInstance(CVInstanceElement cvInstance) {
 		if (cvInstance != null) {
 			DeltaCloudInstance instance = (DeltaCloudInstance) cvInstance.getElement();
-			CloudViewElement element = cvInstance;
-			while (!(element instanceof CVCloudElement))
-				element = (CloudViewElement) element.getParent();
-			CVCloudElement cvcloud = (CVCloudElement) element;
-			DeltaCloud cloud = (DeltaCloud) cvcloud.getElement();
-			PerformInstanceActionThread t = new PerformInstanceActionThread(cloud, instance,
-					DeltaCloudInstance.REBOOT,
-					CVMessages.getString(REBOOTING_INSTANCE_TITLE),
-					CVMessages.getFormattedString(REBOOTING_INSTANCE_MSG, new String[] { instance.getName() }),
-					DeltaCloudInstance.RUNNING);
-			t.setUser(true);
-			t.schedule();
+			executeInstanceAction(
+					cvInstance
+					, DeltaCloudInstance.REBOOT
+					, DeltaCloudInstance.RUNNING
+					, CVMessages.getString(REBOOTING_INSTANCE_TITLE)
+					, CVMessages.getFormattedString(REBOOTING_INSTANCE_MSG, new String[] { instance.getName() }));
 		}
 	}
 }
