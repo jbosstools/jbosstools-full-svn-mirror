61e8a5cf8150849ea264641c749e84223f424d60
Nick Boldt <nboldt@redhat.com>
2010-11-09 17:02:29 +0000

    add overrides for targetFile, repoPath, destinationPath, and DESTINATION; set defaults for JBDS and JBT
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@26386 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/target-platform/publish.sh
+++ build/target-platform/publish.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 # Hudson creates a repo in ${repoPath}; copy it into other places for access by downstream jobs and users
 
+# defaults for JBoss Tools
 targetFile=e361-wtp322.target
-
 repoPath=/home/hudson/static_build_env/jbds/tools/sources/REPO
 destinationPath=/home/hudson/static_build_env/jbds/target-platform
 DESTINATION=tools@filemgmt.jboss.org:/downloads_htdocs/tools/updates/target-platform
+
+while [ "$#" -gt 0 ]; do
+	case $1 in
+		'-targetFile') targetFile="$2"; shift 2;;
+		'-repoPath') repoPath="$2"; shift 2;;
+		'-destinationPath') destinationPath="$2"; shift 2;;
+		'-DESTINATION') DESTINATION="$2"; shift 2;;
+		'-jbds') 
+		# defaults for JBDS
+		targetFile=jbds400-e361-wtp322.target
+		repoPath=/home/hudson/static_build_env/jbds/tools/sources/JBDS-REPO
+		destinationPath=/home/hudson/static_build_env/jbds/jbds-target-platform
+		DESTINATION=/qa/services/http/binaries/RHDS/updates/jbds-target-platform
+		;;
+	esac
+done
 
 if [[ -d ${repoPath} ]]; then
 	cd ${repoPath}
