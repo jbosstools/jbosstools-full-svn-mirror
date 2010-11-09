9eb83cf760df9a890d4d6cb4f12f0545e0132b13
Nick Boldt <nboldt@redhat.com>
2010-11-09 20:26:18 +0000

    fix du for the first run (if dir not already exist)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@26395 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/target-platform/publish.sh
+++ build/target-platform/publish.sh
@@ -26,7 +26,7 @@ done
 if [[ -d ${repoPath} ]]; then
 	cd ${repoPath}
 
-	du -sh ${repoPath} ${destinationPath}/${targetFile}
+	du -sh ${repoPath}; if [[ -d ${destinationPath}/${targetFile} ]]; then du -sh ${destinationPath}/${targetFile}; fi
 
 	# copy/update into central place for reuse by local downstream build jobs
 	date; rsync -arzqc --delete-after --delete-excluded --rsh=ssh --exclude '.blobstore' * ${destinationPath}/${targetFile}/
