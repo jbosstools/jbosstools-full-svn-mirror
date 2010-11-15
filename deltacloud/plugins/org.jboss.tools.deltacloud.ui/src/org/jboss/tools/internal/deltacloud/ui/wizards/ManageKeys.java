e6b62a5597c4ae5206af2f32ce76c0e6d21a21fb
Andre Dietisheim <adietish@redhat.com>
2010-11-15 16:06:18 +0000

    [JBIDE-7581] cleanup, added copyright header
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@26563 a97e2381-89e5-4abb-bab3-167db6db766c

--- deltacloud/plugins/org.jboss.tools.deltacloud.ui/src/org/jboss/tools/internal/deltacloud/ui/wizards/ManageKeys.java
+++ deltacloud/plugins/org.jboss.tools.deltacloud.ui/src/org/jboss/tools/internal/deltacloud/ui/wizards/ManageKeys.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.internal.deltacloud.ui.wizards;
 
 import org.eclipse.jface.wizard.Wizard;
@@ -9,34 +19,31 @@ public class ManageKeys extends Wizard {
 	private String fileExtension;
 	private ManageKeysPage mainPage;
 	private String keyname;
-	
+
 	public ManageKeys(DeltaCloud cloud, String fileExtension) {
 		this.cloud = cloud;
 		this.fileExtension = fileExtension;
 	}
-	
+
 	public String getKeyName() {
 		return keyname;
 	}
-	
+
 	@Override
 	public void addPages() {
-		// TODO Auto-generated method stub
 		mainPage = new ManageKeysPage(cloud, fileExtension);
 		addPage(mainPage);
 	}
-	
+
 	@Override
 	public boolean canFinish() {
 		return mainPage.isPageComplete();
 	}
-	
+
 	@Override
 	public boolean performFinish() {
 		String currFile = mainPage.getCurrFile();
-		keyname = currFile.substring(0, 
-				currFile.length() - fileExtension.length());
-		// TODO Auto-generated method stub
+		keyname = currFile.substring(0, currFile.length() - fileExtension.length());
 		return true;
 	}
 
