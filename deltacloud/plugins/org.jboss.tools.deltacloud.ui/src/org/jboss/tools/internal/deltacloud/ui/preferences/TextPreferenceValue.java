d14f057c41394b283e91df0783c9eaf83930a375
Andre Dietisheim <adietish@redhat.com>
2010-12-03 08:50:33 +0000

    added copyright header, added author
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@27133 a97e2381-89e5-4abb-bab3-167db6db766c

--- deltacloud/plugins/org.jboss.tools.deltacloud.ui/src/org/jboss/tools/internal/deltacloud/ui/preferences/TextPreferenceValue.java
+++ deltacloud/plugins/org.jboss.tools.deltacloud.ui/src/org/jboss/tools/internal/deltacloud/ui/preferences/TextPreferenceValue.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.internal.deltacloud.ui.preferences;
 
 import org.eclipse.core.runtime.Plugin;
@@ -5,6 +15,9 @@ import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.osgi.service.prefs.BackingStoreException;
 import org.osgi.service.prefs.Preferences;
 
+/**
+ * @author Andre Dietisheim
+ */
 public class TextPreferenceValue {
 
 	private Plugin plugin;
