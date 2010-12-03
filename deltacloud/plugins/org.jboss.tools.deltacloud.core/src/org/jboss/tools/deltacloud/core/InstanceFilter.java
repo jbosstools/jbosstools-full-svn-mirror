ac2c9ffb273864f87b2dfb08accf4e883ee65fdd
Andre Dietisheim <adietish@redhat.com>
2010-12-03 13:27:06 +0000

    [JBIDE-7688] added copyright header and author
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@27143 a97e2381-89e5-4abb-bab3-167db6db766c

--- deltacloud/plugins/org.jboss.tools.deltacloud.core/src/org/jboss/tools/deltacloud/core/InstanceFilter.java
+++ deltacloud/plugins/org.jboss.tools.deltacloud.core/src/org/jboss/tools/deltacloud/core/InstanceFilter.java
@@ -1,7 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.deltacloud.core;
 
 import java.util.regex.PatternSyntaxException;
 
+/**
+ * @author Jeff Johnston
+ */
 public class InstanceFilter implements IInstanceFilter {
 
 	private IFieldMatcher nameRule;
@@ -63,7 +76,6 @@ public class InstanceFilter implements IInstanceFilter {
 	public IFieldMatcher getIdRule() {
 		return idRule;
 	}
-	
 
 	@Override
 	public IFieldMatcher getImageIdRule() {
