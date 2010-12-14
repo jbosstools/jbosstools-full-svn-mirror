43aac17281af6d2ad9532740c5a5654b9fe5db89
Andre Dietisheim <adietish@redhat.com>
2010-12-14 10:49:41 +0000

    [JBIDE-7889] added copyright header and authors
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@27440 a97e2381-89e5-4abb-bab3-167db6db766c

--- deltacloud/plugins/org.jboss.tools.deltacloud.integration/src/org/jboss/tools/deltacloud/integration/wizard/RSEandASWizardPage.java
+++ deltacloud/plugins/org.jboss.tools.deltacloud.integration/src/org/jboss/tools/deltacloud/integration/wizard/RSEandASWizardPage.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat Inc..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Incorporated - initial API and implementation
+ *******************************************************************************/
 package org.jboss.tools.deltacloud.integration.wizard;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -28,6 +38,9 @@ import org.jboss.tools.internal.deltacloud.ui.utils.UIUtils;
 import org.osgi.service.prefs.BackingStoreException;
 import org.osgi.service.prefs.Preferences;
 
+/**
+ * @author Rob Stryker
+ */
 public class RSEandASWizardPage extends WizardPage implements INewInstanceWizardPage {
 
 	private Button createRSE;
