c58ce7bd0b86c47f301c168ee3002d4e0c2cd570
Denis Golovin <dgolovin@exadel.com>
2010-12-30 01:02:16 +0000

    https://issues.jboss.org/browse/JBIDE-8023 JSF component build missing test plugins from JST component workaround to update staging composite repository
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@27816 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/composite/publish.sh
+++ build/composite/publish.sh
@@ -2,4 +2,4 @@
 # Temp workaround to fix missing sites in composite repo
 
 DESTINATION="tools@filemgmt.jboss.org:/downloads_htdocs/tools/builds/staging/_composite_/trunk"
-rsync -arzq --delete ${WORKSPACE}/composite ${DESTINATION}
+rsync -arzq --delete ${WORKSPACE}/composite/ ${DESTINATION}
