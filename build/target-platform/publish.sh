96ae250e8a5745e9a8f27b333d99e22f4243b640
Nick Boldt <nboldt@redhat.com>
2011-03-04 00:33:48 +0000

    target platform is now e362-wtp323, not e361-wtp322
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.2.x@29497 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/target-platform/publish.sh
+++ build/target-platform/publish.sh
@@ -2,7 +2,7 @@
 # Hudson creates a repo in ${repoDir}; copy it into other places for access by downstream jobs and users
 
 # defaults for JBoss Tools
-targetZipFile=e361-wtp322.target
+targetZipFile=e362-wtp323.target
 repoDir=/home/hudson/static_build_env/jbds/tools/sources/REPO
 destinationPath=/home/hudson/static_build_env/jbds/target-platform
 DESTINATION=tools@filemgmt.jboss.org:/downloads_htdocs/tools/updates/target-platform
@@ -21,7 +21,7 @@ while [ "$#" -gt 0 ]; do
 		'-exclude') exclude="$2"; shift 2;;
 		'-jbt_trunk') 
 		# defaults for JBT (trunk)
-		targetZipFile=e361-wtp322.target
+		targetZipFile=e362-wtp323.target
 		repoDir=/home/hudson/static_build_env/jbds/tools/sources/REPO_trunk
 		destinationPath=/home/hudson/static_build_env/jbds/target-platform_trunk
 		DESTINATION=tools@filemgmt.jboss.org:/downloads_htdocs/tools/updates/target-platform_trunk
@@ -30,7 +30,7 @@ while [ "$#" -gt 0 ]; do
 		shift 1;;
 		'-jbt') 
 		# defaults for JBT (stable branch)
-		targetZipFile=e361-wtp322.target
+		targetZipFile=e362-wtp323.target
 		repoDir=/home/hudson/static_build_env/jbds/tools/sources/REPO
 		destinationPath=/home/hudson/static_build_env/jbds/target-platform
 		DESTINATION=tools@filemgmt.jboss.org:/downloads_htdocs/tools/updates/target-platform
@@ -39,7 +39,7 @@ while [ "$#" -gt 0 ]; do
 		shift 1;;
 		'-jbds_trunk') 
 		# defaults for JBDS (trunk)
-		targetZipFile=jbds400-e361-wtp322.target
+		targetZipFile=jbds400-e362-wtp323.target
 		repoDir=/home/hudson/static_build_env/jbds/tools/sources/JBDS-REPO_trunk
 		destinationPath=/home/hudson/static_build_env/jbds/jbds-target-platform_trunk
 		DESTINATION=/qa/services/http/binaries/RHDS/updates/jbds-target-platform_trunk
@@ -48,7 +48,7 @@ while [ "$#" -gt 0 ]; do
 		shift 1;;
 		'-jbds') 
 		# defaults for JBDS (stable branch)
-		targetZipFile=jbds400-e361-wtp322.target
+		targetZipFile=jbds400-e362-wtp323.target
 		repoDir=/home/hudson/static_build_env/jbds/tools/sources/JBDS-REPO
 		destinationPath=/home/hudson/static_build_env/jbds/jbds-target-platform
 		DESTINATION=/qa/services/http/binaries/RHDS/updates/jbds-target-platform
