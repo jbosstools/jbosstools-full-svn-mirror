f2f7e45b865d81f554c361f339f66069f56581b4
Andre Dietisheim <adietish@redhat.com>
2011-03-11 16:35:31 +0000

    [JBIDE-7523] listening to changes in DeltaCloud and refreshing properties view
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@29707 a97e2381-89e5-4abb-bab3-167db6db766c

--- deltacloud/plugins/org.jboss.tools.deltacloud.ui/src/org/jboss/tools/deltacloud/ui/views/cloud/property/CVPropertySheetPage.java
+++ deltacloud/plugins/org.jboss.tools.deltacloud.ui/src/org/jboss/tools/deltacloud/ui/views/cloud/property/CVPropertySheetPage.java
@@ -14,6 +14,7 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.views.properties.PropertySheetPage;
 import org.jboss.tools.deltacloud.core.DeltaCloud;
@@ -31,7 +32,13 @@ public class CVPropertySheetPage extends PropertySheetPage {
 
 		@Override
 		public void propertyChange(PropertyChangeEvent evt) {
-			refresh();
+			Display.getDefault().syncExec(new Runnable() {
+				
+				@Override
+				public void run() {
+					refresh();
+				}
+			});
 		}
 	};
 
