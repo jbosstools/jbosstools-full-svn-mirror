993cc5044c7a7d8a8b9ffb3c918356b5282ff12f
Andre Dietisheim <adietish@redhat.com>
2011-03-12 01:24:52 +0000

    [JBIDE-8562] extracted client tests to their own plugin
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@29723 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ deltacloud/tests/org.jboss.tools.deltacloud.client.test/src/org/jboss/tools/internal/deltacloud/test/DeltaCloudClientTestSuite.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.internal.deltacloud.test;
+
+import org.jboss.tools.internal.deltacloud.test.core.client.APIDomUnmarshallingTest;
+import org.jboss.tools.internal.deltacloud.test.core.client.HardwareProfileDomUnmarshallingTest;
+import org.jboss.tools.internal.deltacloud.test.core.client.ImageDomUnmarshallingTest;
+import org.jboss.tools.internal.deltacloud.test.core.client.InstanceDomUnmarshallingTest;
+import org.jboss.tools.internal.deltacloud.test.core.client.KeyDomUnmarshallingTest;
+import org.jboss.tools.internal.deltacloud.test.core.client.RealmDomUnmarshallingTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Andre Dietisheim
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	KeyDomUnmarshallingTest.class,
+	InstanceDomUnmarshallingTest.class,
+	ImageDomUnmarshallingTest.class,
+	HardwareProfileDomUnmarshallingTest.class,
+	APIDomUnmarshallingTest.class,
+	RealmDomUnmarshallingTest.class})
+public class DeltaCloudClientTestSuite {
+}
---
+++ deltacloud/tests/org.jboss.tools.deltacloud.client.test/src/org/jboss/tools/internal/deltacloud/test/core/client/ImageMockIntegrationTest.java
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.internal.deltacloud.test.core.client;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Collection;
+import java.util.List;
+
+import org.jboss.tools.deltacloud.core.client.DeltaCloudClientException;
+import org.jboss.tools.deltacloud.core.client.DeltaCloudClientImpl;
+import org.jboss.tools.deltacloud.core.client.Image;
+import org.jboss.tools.internal.deltacloud.test.context.MockIntegrationTestContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * An integration test that test various image related operations in DeltaCloudClient
+ * 
+ * @author Andre Dietisheim
+ * 
+ * @see DeltaCloudClientImpl#listImages()
+ * @see DeltaCloudClientImpl#listImages(String)
+ * 
+ */
+public class ImageMockIntegrationTest {
+
+	private MockIntegrationTestContext testSetup;
+
+	@Before
+	public void setUp() throws IOException, DeltaCloudClientException {
+		this.testSetup = new MockIntegrationTestContext();
+		testSetup.setUp();
+	}
+
+	@After
+	public void tearDown() {
+		testSetup.tearDown();
+	}
+
+	@Test(expected = DeltaCloudClientException.class)
+	public void cannotListIfNotAuthenticated() throws MalformedURLException, DeltaCloudClientException {
+		DeltaCloudClientImpl client = new DeltaCloudClientImpl(MockIntegrationTestContext.DELTACLOUD_URL, "badUser", "badPassword");
+		client.listImages();
+	}
+
+	@Test
+	public void assertDefaultImages() throws DeltaCloudClientException {
+		List<Image> images = testSetup.getClient().listImages();
+		assertEquals(3, images.size());
+		assertImage("img1", "Fedora 10", "fedoraproject", "Fedora 10", "x86_64", getImage("img1", images));
+		assertImage("img2", "Fedora 10", "fedoraproject", "Fedora 10", "i386", getImage("img2", images) );
+		assertImage("img3", "JBoss", "mockuser", "JBoss", "i386", getImage("img3", images));
+	}
+
+	private Image getImage(String id, Collection<Image> images) {
+		for (Image image : images) {
+			if (id.equals(image.getId())) {
+				return image;
+			}
+		}
+		fail("image " + id + " was not found");
+		return null;
+	}
+	
+	private void assertImage(String id, String name, String owner, String description, String architecture, Image image) {
+		assertEquals(id, image.getId());
+		assertEquals(name, image.getName());
+		assertEquals(owner, image.getOwnerId());
+		assertEquals(architecture, image.getArchitecture());
+		assertEquals(description, image.getDescription());
+	}
+}
