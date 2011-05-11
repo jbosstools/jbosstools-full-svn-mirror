347c663015045d718ba7bcf932567dfc51de7494
Robert (Bob) Brodt <bbrodt@redhat.com>
2011-05-11 19:03:25 +0000

    https://issues.jboss.org/browse/JBIDE-8861
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31221 a97e2381-89e5-4abb-bab3-167db6db766c

--- bpel/plugins/org.eclipse.bpel.apache.ode.deploy.ui/src/org/eclipse/bpel/apache/ode/deploy/ui/wizards/ODEDeployWizardPage.java
+++ bpel/plugins/org.eclipse.bpel.apache.ode.deploy.ui/src/org/eclipse/bpel/apache/ode/deploy/ui/wizards/ODEDeployWizardPage.java
@@ -43,8 +43,8 @@ public class ODEDeployWizardPage extends WizardPage {
 	 */
 	public ODEDeployWizardPage(ISelection selection) {
 		super("ODEDeployDescriptorWizardPage");
-		setTitle("Apache ODE Deployment Descriptor");
-		setDescription("This wizard creates a new Apache ODE descriptor file (deploy.xml). Note that the file name cannot be changed.");
+		setTitle("BPEL Deployment Descriptor");
+		setDescription("This wizard creates a new BPEL deployment descriptor file (deploy.xml). Note that the file name cannot be changed.");
 		this.selection = selection;
 	}
 
