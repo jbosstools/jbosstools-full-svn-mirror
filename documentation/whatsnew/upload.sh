6a7c311b8b6920c02431a2c137c7a04e17f26699
Max Rydahl Andersen <max@hibernate.org>
2011-05-12 20:39:21 +0000

    fixed upload.sh
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31277 a97e2381-89e5-4abb-bab3-167db6db766c

--- documentation/whatsnew/upload.sh
+++ documentation/whatsnew/upload.sh
@@ -1,2 +1,1 @@
-#test
-rsync -i -I -c -P -e ssh -C --exclude-from '.rsyncexclude' -avz . tools@filemgmt.jboss.org:/docs_htdocs/tools/whatsnew/
+rsync -i -I -c -P -C --protocol=29 --exclude-from '.rsyncexclude' -avz . tools@filemgmt.jboss.org:/docs_htdocs/tools/whatsnew/
