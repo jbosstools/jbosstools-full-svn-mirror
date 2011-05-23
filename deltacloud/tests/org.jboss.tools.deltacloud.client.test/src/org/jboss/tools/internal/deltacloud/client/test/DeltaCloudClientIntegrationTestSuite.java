bf8522942e6ac1c8e5ce18a8c7cd95dca3958733
Andre Dietisheim <adietish@redhat.com>
2011-05-23 07:28:29 +0000

    [JBIDE-8975] corrected package structure in Deltacloud client tests
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31420 a97e2381-89e5-4abb-bab3-167db6db766c

--- deltacloud/tests/org.jboss.tools.deltacloud.client.test/src/org/jboss/tools/internal/deltacloud/client/test/DeltaCloudClientIntegrationTestSuite.java
+++ deltacloud/tests/org.jboss.tools.deltacloud.client.test/src/org/jboss/tools/internal/deltacloud/client/test/DeltaCloudClientIntegrationTestSuite.java
@@ -1,19 +1,27 @@
-/*******************************************************************************
- * Copyright (c) 2010 Red Hat, Inc.
- * Distributed under license by Red Hat, Inc. All rights reserved.
- * This program is made available under the terms of the
- * Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Red Hat, Inc. - initial API and implementation
- ******************************************************************************/
+/*************************************************************************
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ * 
+ *************************************************************************/
 package org.jboss.tools.internal.deltacloud.client.test;
 
-import org.jboss.tools.internal.deltacloud.client.test.core.client.ImageMockIntegrationTest;
-import org.jboss.tools.internal.deltacloud.client.test.core.client.InstanceMockIntegrationTest;
-import org.jboss.tools.internal.deltacloud.client.test.core.client.KeyMockIntegrationTest;
-import org.jboss.tools.internal.deltacloud.client.test.core.client.ServerTypeMockIntegrationTest;
+import org.jboss.tools.internal.deltacloud.client.test.client.ImageMockIntegrationTest;
+import org.jboss.tools.internal.deltacloud.client.test.client.InstanceMockIntegrationTest;
+import org.jboss.tools.internal.deltacloud.client.test.client.KeyMockIntegrationTest;
+import org.jboss.tools.internal.deltacloud.client.test.client.ServerTypeMockIntegrationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
