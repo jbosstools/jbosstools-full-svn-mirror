c309bed1a67079f7f7d04e8341c661e0bcb8af51
Dmitry Geraskov <dgeraskov@exadel.com>
2011-10-14 14:21:07 +0000

    Added license
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/dead/hibernatetools-multiversion2@35661 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.hibernate4_0/src/org/jboss/tools/hibernate4_0/HibernateExtension4_0Plugin.java
+++ plugins/org.jboss.tools.hibernate4_0/src/org/jboss/tools/hibernate4_0/HibernateExtension4_0Plugin.java
@@ -1,9 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.hibernate4_0;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
+ * @author Dmitry Geraskov
  * The activator class controls the plug-in life cycle
  */
 public class HibernateExtension4_0Plugin extends AbstractUIPlugin {
