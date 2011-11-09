f40841b8653d9c0209e5ac33dcacadb121b3e710
Max Rydahl Andersen <max@hibernate.org>
2011-11-09 08:09:59 +0000

    JBIDE-9954 fixed upload.sh to include core explicitliy
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@36242 a97e2381-89e5-4abb-bab3-167db6db766c

--- documentation/whatsnew/upload.sh
+++ documentation/whatsnew/upload.sh
@@ -1,1 +1,1 @@
-rsync -i -I -c -P -C --protocol=29 --exclude-from '.rsyncexclude' -avz . tools@filemgmt.jboss.org:/docs_htdocs/tools/whatsnew/
+rsync -i -I -c -P -C --protocol=29 --exclude-from '.rsyncexclude' --include=core -avz . tools@filemgmt.jboss.org:/docs_htdocs/tools/whatsnew/
