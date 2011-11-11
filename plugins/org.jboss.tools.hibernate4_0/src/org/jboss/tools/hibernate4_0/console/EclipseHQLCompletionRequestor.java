1d93ca5dd3c9a2259ea13aee54834cf67de3ba81
Dmitry Geraskov <dgeraskov@exadel.com>
2011-11-11 12:44:47 +0000

    Move exporters call to extenders plugins
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/dead/hibernatetools-multiversion2@36298 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.hibernate4_0/src/org/jboss/tools/hibernate4_0/console/EclipseHQLCompletionRequestor.java
+++ plugins/org.jboss.tools.hibernate4_0/src/org/jboss/tools/hibernate4_0/console/EclipseHQLCompletionRequestor.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.tools.hibernate4_0;
+package org.jboss.tools.hibernate4_0.console;
 
 import java.util.ArrayList;
 import java.util.List;
