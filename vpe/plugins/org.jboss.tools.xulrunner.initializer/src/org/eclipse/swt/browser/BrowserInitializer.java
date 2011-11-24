cb13c05b39de4cd22953aac26d90e426a7c90384
Yahor Radtsevich <yradtsevich@exadel.com>
2011-11-24 19:43:13 +0000

    https://issues.jboss.org/browse/JBIDE-10185 : Installing JBoss Tools 3.3 into Eclipse causes some problems for the Welcome screen
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@36622 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.xulrunner.initializer/src/org/eclipse/swt/browser/BrowserInitializer.java
+++ vpe/plugins/org.jboss.tools.xulrunner.initializer/src/org/eclipse/swt/browser/BrowserInitializer.java
@@ -1,13 +1,22 @@
 package org.eclipse.swt.browser;
 
+import org.eclipse.core.runtime.Platform;
+
 public class BrowserInitializer {
 
 	private static final String PROPERTY_DEFAULTTYPE = "org.eclipse.swt.browser.DefaultType"; //$NON-NLS-1$
 
 	static {
-		String defaultType = System.getProperty(PROPERTY_DEFAULTTYPE);
-		if (defaultType == null) {
-			System.setProperty(PROPERTY_DEFAULTTYPE, "mozilla"); //$NON-NLS-1$
+		/* Under Linux instantiation of WebKit should be avoided,
+		 * because WebKit and XULRunner running simultaneously
+		 * may cause native errors.
+		 * 
+		 * Also see JBIDE-9144 and JBIDE-10185. 	 */
+		if (Platform.OS_LINUX.equals(Platform.getOS())) {
+			String defaultType = System.getProperty(PROPERTY_DEFAULTTYPE);
+			if (defaultType == null) {
+				System.setProperty(PROPERTY_DEFAULTTYPE, "mozilla"); //$NON-NLS-1$
+			}
 		}
 	}
 }
