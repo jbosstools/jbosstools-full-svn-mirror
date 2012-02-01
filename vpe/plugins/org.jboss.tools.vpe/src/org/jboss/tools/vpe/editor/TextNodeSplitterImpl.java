bb4f9817dfc18715a7c87f0a0235c6a0db255487
Denis Golovin <dgolovin@exadel.com>
2012-02-01 02:27:33 +0000

    removed redundant if statement
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38348 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/TextNodeSplitterImpl.java
+++ vpe/plugins/org.jboss.tools.vpe/src/org/jboss/tools/vpe/editor/TextNodeSplitterImpl.java
@@ -84,8 +84,7 @@ public class TextNodeSplitterImpl implements ITextNodeSplitter  {
 	}
 	
 	private boolean isSpacing(String str){
-		if("".endsWith(str.trim())) return true; //$NON-NLS-1$
-		else return false;
+		return "".endsWith(str.trim());
 	}
 	
 	private int getIndex(Node parentNode, Node child) {
