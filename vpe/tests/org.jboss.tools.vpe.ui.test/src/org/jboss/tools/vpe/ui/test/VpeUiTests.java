807db5f6d6c382ce7909b726596e4ea22fff5107
Denis Maliarevich <dmaliarevich@exadel.com>
2012-03-01 11:45:36 +0000

    https://issues.jboss.org/browse/JBIDE-8701 - facets.jsp page was changed, junit should work correctly.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39223 a97e2381-89e5-4abb-bab3-167db6db766c

--- vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/VpeUiTests.java
+++ vpe/tests/org.jboss.tools.vpe.ui.test/src/org/jboss/tools/vpe/ui/test/VpeUiTests.java
@@ -38,7 +38,7 @@ public class VpeUiTests {
 //		suite.addTestSuite(VpePopupMenuTest.class);
 		suite.addTestSuite(VpeEditAnyDialogTest.class);
 		suite.addTestSuite(MultipleSelectionTest.class);
-//		suite.addTestSuite(ScrollingTest_Jbide8701.class);
+		suite.addTestSuite(ScrollingTest_Jbide8701.class);
 		suite.addTestSuite(ToggleClassCastTest_Jbide9790.class);
 		return new VpeTestSetup(suite);
 	}
