57a47f83741dfa26ba703f579152e1dcece1b51e
Mickael Istria <mickael.istria@gmail.com>
2012-04-02 16:11:21 +0000

    JBIDE-11370 Integrated tests in GWT
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39967 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ gwt/tests/org.jboss.tools.gwt.tests/src/org/jboss/tools/gwt/tests/AllTests.java
@@ -0,0 +1,13 @@
+package org.jboss.tools.gwt.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(JUnit4.class)
+@SuiteClasses({
+	GWTTest.class
+})
+public class AllTests {
+
+}
---
+++ gwt/tests/org.jboss.tools.gwt.tests/src/org/jboss/tools/gwt/tests/GWTTest.java
@@ -0,0 +1,13 @@
+package org.jboss.tools.gwt.tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GWTTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
