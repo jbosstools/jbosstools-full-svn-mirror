fab6b549b013470958f21423a0ff76ede508c604
Denis Golovin <dgolovin@exadel.com>
2012-04-05 00:35:08 +0000

    added simple GWT test for plugin activation
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@40043 a97e2381-89e5-4abb-bab3-167db6db766c

--- gwt/tests/org.jboss.tools.gwt.tests/src/org/jboss/tools/gwt/tests/GWTTest.java
+++ gwt/tests/org.jboss.tools.gwt.tests/src/org/jboss/tools/gwt/tests/GWTTest.java
@@ -1,13 +1,18 @@
 package org.jboss.tools.gwt.tests;
 
+import org.eclipse.core.runtime.Platform;
 import org.junit.Test;
+import org.osgi.framework.Bundle;
+
 import static org.junit.Assert.*;
 
 public class GWTTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGwtPlugisArePresentAndActivated() {
+		Bundle bundle = Platform.getBundle("org.jboss.tools.gwt.core");
+		assertNotNull(bundle);
+		bundle = Platform.getBundle("org.jboss.tools.gwt.ui");
+		assertNotNull(bundle);
 	}
-
 }
