1b5ca25eab77e01d40641ae1584f3c22d46b891c
Nick Boldt <nboldt@redhat.com>
2012-06-07 16:09:50 +0000

    support Jenkins better
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.3.x@41781 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publish.sh
+++ build/publish.sh
@@ -3,7 +3,8 @@ script=$0
 if [[ ${script%%publish.sh} != $script ]]; then
 	script=${script/publish.sh/publish\/publish.sh}
 else
-	script=$1
+	script=`cat $0 | grep ".sh"`
 	script=${script/publish.sh/publish\/publish.sh}
+	script=${script/. /}
 fi
 . ${script}
