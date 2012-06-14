51b4cd5b2d3af856968851ff8415d6e771914177
Nick Boldt <nboldt@redhat.com>
2012-06-14 21:29:40 +0000

    update diff script to exclude soa stuff and include hibernate better
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41992 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/util/checkCompiledArtifactVersions.sh
+++ build/util/checkCompiledArtifactVersions.sh
@@ -15,17 +15,20 @@ else
 	basedir=.
 fi
 
+# point at an update site to produce list of versions
+
 versionlist=/tmp/versionlist; rm -fr $versionlist
 
-for d in tools.common tools.flow jbpm jmx archives .as bpel smooks freemarker profiler portlet modeshape xulrunner jst vpe jsf drools esb tptp ws cdi struts hibernatetools seam examples birt maven; do 
+for d in tools.common jmx archives .as freemarker profiler portlet xulrunner jst vpe jsf tptp ws cdi struts hibernate seam examples birt maven; do 
 	#echo " == $d =="
-	for a in $(find $basedir -maxdepth 2 -name "*${d}*"); do
+	for a in $(find $basedir -maxdepth 3 -name "*${d}*" | sort); do
 		b=$a; b=${b##*/}; b=${b%.*}; b=${b%.jar};
 		a=${b%%_*}; a=${a//./_} # org_jboss_tools_maven_seam_feature
 		v=${b#*_}; v=${v:0:5}
-		echo "${a}=${v}" | tee -a $versionlist
+		echo "${a}=${v}" >> $versionlist
+		#echo "${a}=${v} ($b)"
 	done
 	#echo " -- $d --"
 done
 echo ""
-echo "Baseline artifact version information stored in ${versionlist}."
+echo "Baseline artifact version information stored in ${versionlist}"
