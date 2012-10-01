06ffd47ec209079c4b876f7713af40ec1e89ba21
Nick Boldt <nboldt@redhat.com>
2012-10-01 18:01:21 +0000

    use new commandline args when calling jbosstools-cleanup.sh (JBIDE-12662)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@44186 a97e2381-89e5-4abb-bab3-167db6db766c

--- build/publish/publish.sh
+++ build/publish/publish.sh
@@ -482,7 +482,7 @@ if [[ ${JOB_NAME/.aggregate} != ${JOB_NAME} ]]; then
 	# regenerate http://download.jboss.org/jbosstools/builds/nightly/*/*/composite*.xml files for up to 5 builds, cleaning anything older than 5 days old
 	wget http://anonsvn.jboss.org/repos/jbosstools/trunk/build/util/cleanup/jbosstools-cleanup.sh --no-check-certificate
 	chmod +x jbosstools-cleanup.sh
-	./jbosstools-cleanup.sh 5 5
+	./jbosstools-cleanup.sh --keep 5 --age-to-delete 5
 	rm -f jbosstools-cleanup.sh
 fi
 
